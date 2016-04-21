27bd7af1253702107313f564c3c42b7a0bfefb5e
ialonso <kaian@irontec.com>
2016-04-21 15:27:49 +0200

    scheme: restore KamUsersAddress models & mappers

---
+++ library/IvozProvider/Mapper/Sql/DbTable/KamUsersAddress.php
@@ -0,0 +1,175 @@
+<?php
+
+/**
+ * Application Model DbTables
+ *
+ * @package IvozProvider\Mapper\Sql\DbTable
+ * @subpackage DbTable
+ * @author Luis Felipe Garcia
+ * @copyright ZF model generator
+ * @license http://framework.zend.com/license/new-bsd     New BSD License
+ */
+
+/**
+ * Table definition for kam_users_address
+ *
+ * @package IvozProvider\Mapper\Sql\DbTable
+ * @subpackage DbTable
+ * @author Luis Felipe Garcia
+ */
+
+namespace IvozProvider\Mapper\Sql\DbTable;
+class KamUsersAddress extends TableAbstract
+{
+    /**
+     * $_name - name of database table
+     *
+     * @var string
+     */
+    protected $_name = 'kam_users_address';
+
+    /**
+     * $_id - this is the primary key name
+     *
+     * @var int
+     */
+    protected $_id = 'id';
+
+    protected $_rowClass = 'IvozProvider\\Model\\KamUsersAddress';
+    protected $_rowMapperClass = 'IvozProvider\\Mapper\\Sql\\KamUsersAddress';
+
+    protected $_sequence = true; // int
+    protected $_referenceMap = array(
+        'KamUsersAddressIbfk1' => array(
+            'columns' => 'peerServerId',
+            'refTableClass' => 'IvozProvider\\Mapper\\Sql\\DbTable\\PeerServers',
+            'refColumns' => 'id'
+        )
+    );
+    
+    protected $_metadata = array (
+	  'id' => 
+	  array (
+	    'SCHEMA_NAME' => NULL,
+	    'TABLE_NAME' => 'kam_users_address',
+	    'COLUMN_NAME' => 'id',
+	    'COLUMN_POSITION' => 1,
+	    'DATA_TYPE' => 'int',
+	    'DEFAULT' => NULL,
+	    'NULLABLE' => false,
+	    'LENGTH' => NULL,
+	    'SCALE' => NULL,
+	    'PRECISION' => NULL,
+	    'UNSIGNED' => true,
+	    'PRIMARY' => true,
+	    'PRIMARY_POSITION' => 1,
+	    'IDENTITY' => true,
+	  ),
+	  'grp' => 
+	  array (
+	    'SCHEMA_NAME' => NULL,
+	    'TABLE_NAME' => 'kam_users_address',
+	    'COLUMN_NAME' => 'grp',
+	    'COLUMN_POSITION' => 2,
+	    'DATA_TYPE' => 'int',
+	    'DEFAULT' => '1',
+	    'NULLABLE' => false,
+	    'LENGTH' => NULL,
+	    'SCALE' => NULL,
+	    'PRECISION' => NULL,
+	    'UNSIGNED' => true,
+	    'PRIMARY' => false,
+	    'PRIMARY_POSITION' => NULL,
+	    'IDENTITY' => false,
+	  ),
+	  'ip_addr' => 
+	  array (
+	    'SCHEMA_NAME' => NULL,
+	    'TABLE_NAME' => 'kam_users_address',
+	    'COLUMN_NAME' => 'ip_addr',
+	    'COLUMN_POSITION' => 3,
+	    'DATA_TYPE' => 'varchar',
+	    'DEFAULT' => NULL,
+	    'NULLABLE' => false,
+	    'LENGTH' => '50',
+	    'SCALE' => NULL,
+	    'PRECISION' => NULL,
+	    'UNSIGNED' => NULL,
+	    'PRIMARY' => false,
+	    'PRIMARY_POSITION' => NULL,
+	    'IDENTITY' => false,
+	  ),
+	  'mask' => 
+	  array (
+	    'SCHEMA_NAME' => NULL,
+	    'TABLE_NAME' => 'kam_users_address',
+	    'COLUMN_NAME' => 'mask',
+	    'COLUMN_POSITION' => 4,
+	    'DATA_TYPE' => 'int',
+	    'DEFAULT' => '32',
+	    'NULLABLE' => false,
+	    'LENGTH' => NULL,
+	    'SCALE' => NULL,
+	    'PRECISION' => NULL,
+	    'UNSIGNED' => NULL,
+	    'PRIMARY' => false,
+	    'PRIMARY_POSITION' => NULL,
+	    'IDENTITY' => false,
+	  ),
+	  'port' => 
+	  array (
+	    'SCHEMA_NAME' => NULL,
+	    'TABLE_NAME' => 'kam_users_address',
+	    'COLUMN_NAME' => 'port',
+	    'COLUMN_POSITION' => 5,
+	    'DATA_TYPE' => 'smallint',
+	    'DEFAULT' => '0',
+	    'NULLABLE' => false,
+	    'LENGTH' => NULL,
+	    'SCALE' => NULL,
+	    'PRECISION' => NULL,
+	    'UNSIGNED' => true,
+	    'PRIMARY' => false,
+	    'PRIMARY_POSITION' => NULL,
+	    'IDENTITY' => false,
+	  ),
+	  'tag' => 
+	  array (
+	    'SCHEMA_NAME' => NULL,
+	    'TABLE_NAME' => 'kam_users_address',
+	    'COLUMN_NAME' => 'tag',
+	    'COLUMN_POSITION' => 6,
+	    'DATA_TYPE' => 'varchar',
+	    'DEFAULT' => NULL,
+	    'NULLABLE' => true,
+	    'LENGTH' => '64',
+	    'SCALE' => NULL,
+	    'PRECISION' => NULL,
+	    'UNSIGNED' => NULL,
+	    'PRIMARY' => false,
+	    'PRIMARY_POSITION' => NULL,
+	    'IDENTITY' => false,
+	  ),
+	  'peerServerId' => 
+	  array (
+	    'SCHEMA_NAME' => NULL,
+	    'TABLE_NAME' => 'kam_users_address',
+	    'COLUMN_NAME' => 'peerServerId',
+	    'COLUMN_POSITION' => 7,
+	    'DATA_TYPE' => 'int',
+	    'DEFAULT' => NULL,
+	    'NULLABLE' => true,
+	    'LENGTH' => NULL,
+	    'SCALE' => NULL,
+	    'PRECISION' => NULL,
+	    'UNSIGNED' => true,
+	    'PRIMARY' => false,
+	    'PRIMARY_POSITION' => NULL,
+	    'IDENTITY' => false,
+	  ),
+	);
+
+
+
+
+}
