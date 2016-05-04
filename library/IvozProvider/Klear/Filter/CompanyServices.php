e2e094387bb4b5dc3d7becddfb5a5a30c3acd3be
ialonso <kaian@irontec.com>
2016-05-04 12:06:48 +0200

    mappers: Delete CompanyService when BrandService is deleted

--- library/IvozProvider/Klear/Filter/CompanyServices.php
+++ library/IvozProvider/Klear/Filter/CompanyServices.php
@@ -1,47 +1,27 @@
 <?php
-class IvozProvider_Klear_Filter_BrandServices implements KlearMatrix_Model_Field_Select_Filter_Interface
+class IvozProvider_Klear_Filter_CompanyServices implements KlearMatrix_Model_Field_Select_Filter_Interface
 {
     protected $_condition = array();
 
     public function setRouteDispatcher(KlearMatrix_Model_RouteDispatcher $routeDispatcher)
     {
-        // Get Action
-        $currentAction = $routeDispatcher->getActionName();
-
-        // Get Controller
-        $currentController = $routeDispatcher->getControllerName();
-
-        // Get ModelName and your Controller
-        $currentItemName = $routeDispatcher->getCurrentItemName();
-
-        $unfilteredScreens = array(
-                "brandServicesList_screen"
-        );
-
-        if (in_array($currentItemName, $unfilteredScreens)) {
-            return true;
-        }
-
-        // Get Current Emulated brand
         $auth = Zend_Auth::getInstance();
-
         if (!$auth->hasIdentity()) {
             throw new Klear_Exception_Default("No brand emulated");
         }
-
+        
         $loggedUser = $auth->getIdentity();
         $currentBrandyId = $loggedUser->brandId;
-
+        
         $brandServiceMapper = new \IvozProvider\Mapper\Sql\BrandServices();
         $brandServices = $brandServiceMapper->fetchList("`brandId` = " . $currentBrandyId);
-
+        
         $servicesIds = array();
         foreach ($brandServices as $brandService) {
             array_push($servicesIds, $brandService->getServiceId());
         }
-
         if (count($servicesIds)) {
-            $this->_condition[] = "`id` NOT IN (" . implode(',', $servicesIds) . ")";
+            $this->_condition[] = "`id` IN (" . implode(',', $servicesIds) . ")";
         } else {
             $this->_condition[] = "`id` IS NULL";  // Hackish
         }
--- library/IvozProvider/Mapper/Sql/BrandServices.php
+++ library/IvozProvider/Mapper/Sql/BrandServices.php
@@ -20,5 +20,21 @@
 namespace IvozProvider\Mapper\Sql;
 class BrandServices extends Raw\BrandServices
 {
+    public function delete(\IvozProvider\Model\Raw\ModelAbstract $model)
+    {
+        $companiesMapper = new \IvozProvider\Mapper\Sql\Companies;
+        $companies =  $companiesMapper->fetchAll();
 
+        // Foreach Company in Service Brand
+        foreach ($companies as $company) {
+            // Find Company Services with the same Service
+            $companyServices = $company->getCompanyServices("serviceId = ". $model->getServiceId());
+            foreach ($companyServices as $companyService) {
+                // Delete custom company service code
+                $companyService->delete();
+            }
+        }
+        // Delete current model
+        return parent::delete($model);
+    }
 }
