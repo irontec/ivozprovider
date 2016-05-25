844c5c62199431e567548a17b3bf7092028a0661
ialonso <kaian@irontec.com>
2016-05-25 16:45:49 +0200

    userweb: Restore basic portal usage

--- portals/application/Bootstrap.php
+++ portals/application/Bootstrap.php
@@ -6,8 +6,8 @@ class Bootstrap extends Zend_Application_Bootstrap_Bootstrap
     public function _initRouter()
     {
         $front = Zend_Controller_Front::getInstance();
-        $restRoute = new Zend_Rest_Route($front, array(), array('rest'));
-        $front->getRouter()->addRoute('rest', $restRoute);
+        $restRoute = new Zend_Rest_Route($front, array(), array('userweb'));
+        $front->getRouter()->addRoute('userweb', $restRoute);
     }
 
 }
