f2cf6075bc97d0f3713431ccfdf7061a8e5723d5
kaian <kaian@irontec.com>
2016-05-26 13:23:13 +0200

    api: Add Zend Route for REST API

--- portals/application/Bootstrap.php
+++ portals/application/Bootstrap.php
@@ -6,8 +6,10 @@ class Bootstrap extends Zend_Application_Bootstrap_Bootstrap
     public function _initRouter()
     {
         $front = Zend_Controller_Front::getInstance();
-        $restRoute = new Zend_Rest_Route($front, array(), array('userweb'));
-        $front->getRouter()->addRoute('userweb', $restRoute);
+        $restRoute = new Zend_Rest_Route($front, array(), array('rest'));
+        $front->getRouter()->addRoute('rest', $restRoute);
+        $userwebRoute = new Zend_Rest_Route($front, array(), array('userweb'));
+        $front->getRouter()->addRoute('userweb', $userwebRoute);
     }
 
 }
