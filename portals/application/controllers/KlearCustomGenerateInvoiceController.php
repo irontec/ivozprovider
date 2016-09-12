a267f1345352d882aa6c1e123805b12e86b624d3
Victor Vargas <victor@irontec.com>
2016-09-12 12:34:57 +0200

    portals: Changes on CSS and controller

--- portals/application/controllers/KlearCustomGenerateInvoiceController.php
+++ portals/application/controllers/KlearCustomGenerateInvoiceController.php
@@ -45,29 +45,112 @@ class KlearCustomGenerateInvoiceController extends Zend_Controller_Action
 
     protected function _confirmDialog ()
     {
-        $title = $this->_helper->translate("Generate invoice");
-        $message = $this->_helper->translate("Are you sure you want to generate the invoice?");
-        $okButton = $this->_helper->translate("OK");
-        $closeButton = $this->_helper->translate("Close");
+        $_haveError = $this->_searchError();
 
-        $data = array(
-            'title' => $title,
-            'message' => $message,
-            'buttons' => array(
-                $closeButton => array(
-                    "recall" => false,
-                    "reloadParent" => false
-                ),
-                $okButton => array(
-                    "recall" => true,
-                    "reloadParent" => false,
-                    "params" => array(
-                        "generate" => true
+        if ($_haveError) {
+            $data = $_haveError;
+        } else {
+            $title = $this->_helper->translate("Generate invoice");
+            $message = $this->_helper->translate("Are you sure you want to generate the invoice?");
+            $okButton = $this->_helper->translate("OK");
+            $closeButton = $this->_helper->translate("Close");
+
+            $data = array(
+                'title' => $title,
+                'message' => $message,
+                'buttons' => array(
+                    $closeButton => array(
+                        "recall" => false,
+                        "reloadParent" => false
+                    ),
+                    $okButton => array(
+                        "recall" => true,
+                        "reloadParent" => false,
+                        "params" => array(
+                            "generate" => true
+                        )
                     )
                 )
-            )
-        );
+            );
+        }
+
         $this->_dispatch($data);
+    }
+
+    protected function _searchError ()
+    {
+        $pks = $this->getRequest()->getParam("pk");
+        if (!is_array($pks)) {
+            $pks = array($pks);
+        }
+
+        $invoicesMapper = new \IvozProvider\Mapper\Sql\Invoices();
+        foreach ($pks as $pk) {
+            $invoice = $invoicesMapper->find($pk);
+
+            $invoiceTz = $invoice->getCompany()->getDefaultTimezone()->getTz();
+            $inDate = $invoice->getInDate(true);
+            $inDate->setTimezone($invoiceTz);
+            $outDate = $invoice->getOutDate(true);
+            $outDate->setTimezone($invoiceTz);
+            $outDate->addDay(1)->subSecond(1);
+
+            $now = new \Zend_Date();
+            $now->setTimezone($invoiceTz);
+            $inDateIsInFuture = $invoice->getInDate(true)->getDate()->compare($now->getDate()) >= 0;
+            $outDateIsInFuture = $invoice->getOutDate(true)->getDate()->compare($now->getDate()) >= 0;
+
+            $closeButton = $this->_helper->translate("Close");
+
+            if ($inDateIsInFuture || $outDateIsInFuture) {
+                $title = 'Error 50006';
+                $message = $this->_helper->translate("Cannot invoice calls from today in the future.");
+
+                $data = array(
+                    'title' => $title,
+                    'message' => $message,
+                    'buttons' => array(
+                        $closeButton => array(
+                            "recall" => false,
+                            "reloadParent" => false
+                        )
+                    )
+                );
+
+                return $data;
+            }
+
+
+            $callMapper = new \IvozProvider\Mapper\Sql\KamAccCdrs();
+
+            $wheres = array(
+                "companyId = '".$invoice->getCompanyId()."'",
+                "brandId = '".$invoice->getBrandId()."'",
+                "start_time_utc < '".$inDate->toString('yyyy-MM-dd HH:mm:ss')."'",
+                "(invoiceId is null OR invoiceId = '".$invoice->getPrimaryKey()."')"
+            );
+
+            $unbilledCalls = $callMapper->fetchTarificableList($wheres);
+            if (!empty($unbilledCalls)) {
+                $title = 'Error 50002';
+                $message = $this->_helper->translate("There are unbilled calls before in date.");
+
+                $data = array(
+                    'title' => $title,
+                    'message' => $message,
+                    'buttons' => array(
+                        $closeButton => array(
+                            "recall" => false,
+                            "reloadParent" => false
+                        )
+                    )
+                );
+
+                return $data;
+            }
+        }
+
+        return false;
     }
 
     protected function _generateDialog ()
@@ -81,6 +164,7 @@ class KlearCustomGenerateInvoiceController extends Zend_Controller_Action
         foreach ($pks as $pk) {
             $invoice = $invoicesMapper->find($pk);
             $invoice->setStatus("waiting")->save();
+
             $invoicerJob = new \IvozProvider\Gearmand\Jobs\Invoicer();
             $invoicerJob->setPk($pk)->send();
         }
