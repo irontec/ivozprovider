4b7f5490f24433d7f2aaf8f900dfb67d1fc7ae52
Victor Vargas <victor@irontec.com>
2016-09-14 09:45:42 +0200

    portals: changes to global path for invoicer.dev.sh and tarificator.dev.sh

--- portals/application/bin/invoicer.dev.sh
+++ portals/application/bin/invoicer.dev.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-/usr/bin/php /home/luis/workspace/ivozprovider/portals/application/bin/cli.php -a default/worker/invoicer -e development
+/usr/bin/php ./cli.php -a default/worker/invoicer -e development
