db899aecc97815e2315aa40d51cd19ab35b82933
ccruz <carlos@irontec.com>
2017-01-02 13:48:00 +0100

    portals: Fix brandOperator name collision

--- library/IvozProvider/Klear/Auth/BrandOperators/Mapper.php
+++ library/IvozProvider/Klear/Auth/BrandOperators/Mapper.php
@@ -10,6 +10,22 @@ class Mapper extends \IvozProvider\Klear\Auth\Mapper
         $this->_mapper = new BrandOperators();
     }
 
+    public function findByLogin($login)
+    {
+        $brandOperators = $this->_mapper->fetchList(array('username=? and active=1',array($login)));
+
+        foreach ($brandOperators as $brandOperator) {
+            if ($brandOperator->getBrandId() === $this->_brand->getId()) {
+                $user = new \IvozProvider\Klear\Auth\User();
+                $this->_poblateUser($user, $brandOperator);
+                $this->_populateCustomPerms($user, $brandOperator);
+                return $user;
+            }
+        }
+
+        return null;
+    }
+
     protected function _populateCustomPerms(\IvozProvider\Klear\Auth\User $user, $operator)
     {
         $user->isBrandOperator = true;
