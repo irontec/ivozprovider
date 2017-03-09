0474f90ddd62150e43bafcfe3000f1ddfef10aca
kaian <kaian@irontec.com>
2017-03-09 18:36:49 +0100

    portals: Users without voicemail are no longer displayed in route to voicemail #21

---
+++ library/IvozProvider/Klear/Filter/Voicemail.php
@@ -0,0 +1,14 @@
+<?php
+class IvozProvider_Klear_Filter_Voicemail extends IvozProvider_Klear_Filter_Company
+{
+    protected $_condition = array();
+
+    public function setRouteDispatcher(KlearMatrix_Model_RouteDispatcher $routeDispatcher)
+    {
+        // Add parent filters
+        parent::setRouteDispatcher($routeDispatcher);
+        // Only show users with voicemail enabled
+        $this->_condition[] = "`voicemailEnabled` = 1";
+        return true;
+    }
+}
