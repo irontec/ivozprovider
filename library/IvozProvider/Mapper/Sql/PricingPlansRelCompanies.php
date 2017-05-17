17ec0c6f01c3fbea46822f2e4f70019bc5e54c74
Mikel Madariaga <mikel@irontec.com>
2017-05-17 17:19:28 +0200

    mappers: fixed pricingPlan-company metric issues

--- library/IvozProvider/Mapper/Sql/PricingPlansRelCompanies.php
+++ library/IvozProvider/Mapper/Sql/PricingPlansRelCompanies.php
@@ -20,22 +20,4 @@
 namespace IvozProvider\Mapper\Sql;
 class PricingPlansRelCompanies extends Raw\PricingPlansRelCompanies
 {
-
-    protected function _save(\IvozProvider\Model\Raw\PricingPlansRelCompanies $model,
-            $recursive = false, $useTransaction = true, $transactionTag = null, $forceInsert = false
-    )
-    {
-        if (is_null($model->getPrimaryKey())) {
-            $companyId = $model->getCompanyId();
-            $where = "companyId = '".$companyId."'";
-            $companyPlans = $this->fetchList($where, "metric desc");
-            if (!empty($companyPlans)) {
-                $maxMetric = $companyPlans[0]->getMetric();
-                $model->setMetric($maxMetric+10);
-            }
-        }
-
-        return parent::_save($model, $recursive, $useTransaction, $transactionTag, $forceInsert);
-    }
-
 }
