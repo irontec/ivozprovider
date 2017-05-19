163e036e71e32e59207b79d2a3c70e6b4223b2ce
Mikel Madariaga <mmadariaga@irontec.com>
2017-05-19 13:06:27 +0200

    provisioning: sanitize templates on save
    Improved terminal model detection.
    It doesn't trust url path anymore.
    Subpaths in specificUrlPattern will now be ignored.

--- library/IvozProvider/Model/TerminalModels.php
+++ library/IvozProvider/Model/TerminalModels.php
@@ -27,4 +27,35 @@ class TerminalModels extends Raw\TerminalModels
     public function init()
     {
     }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function setGenericTemplate($data)
+    {
+        return parent::setGenericTemplate(
+            $this->_sanitizeTemplate($data)
+        );
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function setSpecificTemplate($data)
+    {
+        return parent::setSpecificTemplate(
+            $this->_sanitizeTemplate($data)
+        );
+    }
+
+    /**
+     * @param $template string
+     * @return string
+     *
+     * Ensures that template lines don't end with a php close tag: ?>
+     */
+    protected function _sanitizeTemplate($template)
+    {
+        return preg_replace('/\?>\r\n/', "?> \r\n", $template);
+    }
 }
