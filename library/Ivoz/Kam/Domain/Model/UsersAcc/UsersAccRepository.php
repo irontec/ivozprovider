a02aaf15094da04d94c15f6b06bc17b62f2624eb
ccruz <carlos@irontec.com>
2017-11-03 18:22:13 +0100

    kam: Disable DB insert per transaction

--- library/Ivoz/Kam/Domain/Model/UsersAcc/UsersAccRepository.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace Ivoz\Kam\Domain\Model\UsersAcc;
-
-use Doctrine\Common\Persistence\ObjectRepository;
-use Doctrine\Common\Collections\Selectable;
-
-interface UsersAccRepository extends ObjectRepository, Selectable {}
-
