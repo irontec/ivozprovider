bc9652be819d45809a6db5e5a797bafb700b4224
ccruz <carlos@irontec.com>
2017-11-03 16:52:44 +0100

    core: Convert ast_queue_member into a view

--- library/Ivoz/Ast/Domain/Model/QueueMember/QueueMemberRepository.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace Ivoz\Ast\Domain\Model\QueueMember;
-
-use Doctrine\Common\Persistence\ObjectRepository;
-use Doctrine\Common\Collections\Selectable;
-
-interface QueueMemberRepository extends ObjectRepository, Selectable {}
-
