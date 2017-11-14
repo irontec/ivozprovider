646772c9a9aa00bb964b9e47e98da01c6abca6e0
Mikel Madariaga <mikel@irontec.com>
2017-11-14 12:20:59 +0100

    core: brandUrl assembler

---
+++ library/Ivoz/Provider/Application/Service/BrandUrl/BrandUrlAssembler.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Ivoz\Provider\Application\Service\BrandUrl;
+
+use Ivoz\Core\Application\DataTransferObjectInterface;
+use Ivoz\Core\Application\Service\StoragePathResolverCollection;
+use Ivoz\Core\Domain\Model\EntityInterface;
+use Ivoz\Core\Application\Service\Assembler\CustomEntityAssemblerInterface;
+use Ivoz\Provider\Domain\Model\BrandUrl\BrandUrlInterface;
+use Assert\Assertion;
+use Ivoz\Core\Application\Service\Traits\FileContainerEntityAssemblerTrait;
+
+
+class BrandUrlAssembler implements CustomEntityAssemblerInterface
+{
+    use FileContainerEntityAssemblerTrait;
+
+    public function __construct(
+        StoragePathResolverCollection $storagePathResolver
+    ) {
+        $this->storagePathResolver = $storagePathResolver;
+    }
+
+    /**
+     * @param DataTransferObjectInterface $dto
+     * @param EntityInterface $entity
+     */
+    public function fromDTO(DataTransferObjectInterface $dto, EntityInterface $entity)
+    {
+        Assertion::isInstanceOf($entity, BrandUrlInterface::class);
+        $entity->updateFromDTO($dto);
+        $this->handleEntityFiles($entity, $dto);
+    }
+}
--- library/Ivoz/Provider/Domain/Model/BrandUrl/BrandUrl.php
+++ library/Ivoz/Provider/Domain/Model/BrandUrl/BrandUrl.php
@@ -2,18 +2,31 @@
 
 namespace Ivoz\Provider\Domain\Model\BrandUrl;
 
+use Ivoz\Core\Domain\Model\TempFileContainnerTrait;
+use Ivoz\Core\Domain\Service\FileContainerInterface;
+
 /**
  * BrandUrl
  */
-class BrandUrl extends BrandUrlAbstract implements BrandUrlInterface
+class BrandUrl extends BrandUrlAbstract implements BrandUrlInterface, FileContainerInterface
 {
     use BrandUrlTrait;
+    use TempFileContainnerTrait;
 
     public function getChangeSet()
     {
         return parent::getChangeSet();
     }
 
+    /**
+     * @return array
+     */
+    public function getFileObjects()
+    {
+        return [
+            'Logo'
+        ];
+    }
 
     /**
      * Get id
