61ff4ae1cec9271f87d11d1bc8d2a80d753b1d7c
kaian <kaian@irontec.com>
2017-11-21 19:49:38 +0100

    core: remove unused AstMusicOnHold entities

--- library/Ivoz/Ast/Domain/Model/Musiconhold/MusiconholdRepository.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace Ivoz\Ast\Domain\Model\Musiconhold;
-
-use Doctrine\Common\Persistence\ObjectRepository;
-use Doctrine\Common\Collections\Selectable;
-
-interface MusiconholdRepository extends ObjectRepository, Selectable {}
-
