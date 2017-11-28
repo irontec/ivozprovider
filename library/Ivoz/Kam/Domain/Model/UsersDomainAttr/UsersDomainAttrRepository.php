ca51fbf799ef91a6a3f17c3fa4479e73a1e58f78
ccruz <carlos@irontec.com>
2017-11-28 16:55:56 +0100

    core: KamUserDomainAttrs is a view now
    With this view, any domain handled by KamUsers gets corresponding brandId as soon
    as possible.

--- library/Ivoz/Kam/Domain/Model/UsersDomainAttr/UsersDomainAttrRepository.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace Ivoz\Kam\Domain\Model\UsersDomainAttr;
-
-use Doctrine\Common\Persistence\ObjectRepository;
-use Doctrine\Common\Collections\Selectable;
-
-interface UsersDomainAttrRepository extends ObjectRepository, Selectable {}
-
