1e6eda6b94955fadd5d15e819e612dc1bf1bf5e4
ccruz <carlos@irontec.com>
2017-11-30 17:40:17 +0100

    scheme+kamusers: New table for KamUsers stats
    kam_acc_cdrs had cdrs from proxies. This commit changes this.
    
    KamUsers now saves its cdrs into a separate table is identical to kam_acc_cdrs
    without unnecesary fields. It only add one/four field(s):
    
    - owner: saved which user (friend or retailAccount) is behind each stats. It is
    an initial letter (u for users, f for friends and r for retailAccounts) and its id.
    
    There is a MySQL trigger that gets that id and sets in corresponding field (userId,
    friendId, retailClientId).
    
    NOTE: MySQL trigger is necessary as Kamailio acc only saves dlg_vars (that only
    store strings). This means that it cannot save only userId and leave remaining values
    (friendId, retailAccountId) to NULL. It would save '' that makes MySQL not to save
    the row as it is not a valid value for an int field.

---
+++ library/Ivoz/Kam/Domain/Model/UsersCdr/UsersCdrTrait.php
@@ -0,0 +1,92 @@
+<?php
+
+namespace Ivoz\Kam\Domain\Model\UsersCdr;
+
+use Ivoz\Core\Application\DataTransferObjectInterface;
+
+/**
+ * UsersCdrTrait
+ * @codeCoverageIgnore
+ */
+trait UsersCdrTrait
+{
+    /**
+     * @var integer
+     */
+    protected $id;
+
+
+    /**
+     * Constructor
+     */
+    public function __construct()
+    {
+        parent::__construct(...func_get_args());
+
+    }
+
+    /**
+     * @return UsersCdrDTO
+     */
+    public static function createDTO()
+    {
+        return new UsersCdrDTO();
+    }
+
+    /**
+     * Factory method
+     * @param DataTransferObjectInterface $dto
+     * @return self
+     */
+    public static function fromDTO(DataTransferObjectInterface $dto)
+    {
+        /**
+         * @var $dto UsersCdrDTO
+         */
+        $self = parent::fromDTO($dto);
+
+        if ($dto->getId()) {
+            $self->id = $dto->getId();
+            $self->initChangelog();
+        }
+
+        return $self;
+    }
+
+    /**
+     * @param DataTransferObjectInterface $dto
+     * @return self
+     */
+    public function updateFromDTO(DataTransferObjectInterface $dto)
+    {
+        /**
+         * @var $dto UsersCdrDTO
+         */
+        parent::updateFromDTO($dto);
+
+        return $this;
+    }
+
+    /**
+     * @return UsersCdrDTO
+     */
+    public function toDTO()
+    {
+        $dto = parent::toDTO();
+        return $dto
+            ->setId($this->getId());
+    }
+
+    /**
+     * @return array
+     */
+    protected function __toArray()
+    {
+        return parent::__toArray() + [
+            'id' => self::getId()
+        ];
+    }
+
+
+}
+
