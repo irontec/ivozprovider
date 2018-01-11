8d59d334e0470ca9db0d9f683b449efdc8309bc9
Mikel Madariaga <mikel@irontec.com>
2018-01-11 17:33:53 +0100

    web/rest: updated rest API to use api-platformn

--- library/bin/cache_clear.sh
+++ library/bin/cache_clear.sh
@@ -2,5 +2,6 @@
 
 php ../web/rest/bin/console cache:clear -e prod
 php ../web/rest/bin/console cache:clear -e dev --no-warmup
+php ../web/rest/bin/console api:swagger:export > web/swagger.json
 php ../scheme/bin/console cache:clear -e prod
 php ../scheme/bin/console cache:clear -e dev --no-warmup
