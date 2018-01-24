953f5201f7905560c2d927c342f1d2752d1d1d1a
Kaian <kaian@irontec.com>
2018-01-24 13:26:42 +0100

    core: update Endpoint/Voicemail when Extension number changes
    Ast/PsEndpoint and Ast/Voicemail were only being updated when
    the User changed the extension. This two tables contain the extension
    number, so must be updated if the Extension itself changes, not only
    its relation with the user.
    
    This change only applies to those extension that are used as screen
    extension for the user, not all the extension that route to user.

---
+++ library/Ivoz/Ast/Domain/Service/PsEndpoint/UpdateByExtension.php
@@ -0,0 +1,82 @@
+<?php
+
+namespace Ivoz\Ast\Domain\Service\PsEndpoint;
+
+use Ivoz\Ast\Domain\Model\PsEndpoint\PsEndpointInterface;
+use Ivoz\Core\Domain\Service\EntityPersisterInterface;
+use Ivoz\Provider\Domain\Model\Extension\ExtensionInterface;
+use Ivoz\Provider\Domain\Service\Extension\ExtensionLifecycleEventHandlerInterface;
+
+/**
+ * Class UpdateByExtension
+ * @package Ivoz\Ast\Domain\Service\PsEndpoint
+ * @lifecycle pre_persist
+ */
+class UpdateByExtension implements ExtensionLifecycleEventHandlerInterface
+{
+    /**
+     * @var EntityPersisterInterface
+     */
+    protected $entityPersister;
+
+    public function __construct(
+        EntityPersisterInterface $entityPersister
+    ) {
+        $this->entityPersister = $entityPersister;
+    }
+
+    /**
+     * @param ExtensionInterface $entity
+     * @param $isNew
+     */
+    public function execute(ExtensionInterface $entity, $isNew)
+    {
+        // Ignore non-user extensions
+        $user = $entity->getUser();
+        if (!$user) {
+            return;
+        }
+
+        // Only apply to user's with extension
+        $extension = $user->getExtension();
+        if (!$extension) {
+            return;
+        }
+
+        // Only apply if the extension changed is user's screen extension
+        if ($entity->getId() != $extension->getId()) {
+            return;
+        }
+
+        // Ignore non-terminal users
+        $terminal = $user->getTerminal();
+        if (!$terminal) {
+            return;
+        }
+
+        // Update terminal endpoint
+        /** @var PsEndpointInterface $endpoint */
+        $endpoint = $terminal->getAstPsEndpoint();
+        if (!$endpoint) {
+            return;
+        }
+
+        $callerId = sprintf(
+            '%s <%s>',
+            $user->getFullName(),
+            $user->getExtensionNumber()
+        );
+
+        // Set new callerid with updated extension number
+        $endpoint->setCallerid($callerId);
+
+        // Update endpoint voicemail mailbox@context
+        if ($user->getVoicemailEnabled()) {
+            $endpoint->setMailboxes($user->getVoiceMail());
+        }
+
+        $this
+            ->entityPersister
+            ->persist($endpoint);
+    }
+}
