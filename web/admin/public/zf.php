e97a65508f9b5efb019bdbddd59b9624c0dc4c16
Mikel Madariaga <mikel@irontec.com>
2018-02-13 18:35:02 +0100

    web/admin: remove old vendor reference

--- web/admin/public/zf.php
+++ web/admin/public/zf.php
@@ -15,8 +15,6 @@ set_include_path(implode(PATH_SEPARATOR, array(
     get_include_path(),
 )));
 
-require_once realpath(APPLICATION_PATH . '/../vendor/autoload.php');
-
 /** Zend_Application */
 require_once 'Zend/Application.php';
 
