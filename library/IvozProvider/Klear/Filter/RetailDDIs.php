9f2fa5bbb544a70141fde37bbab43b0a26414e93
ccruz <carlos@irontec.com>
2018-02-23 14:21:57 +0100

    portals: Fix deletion of multiple retailAccounts

--- library/IvozProvider/Klear/Filter/RetailDDIs.php
+++ library/IvozProvider/Klear/Filter/RetailDDIs.php
@@ -5,6 +5,19 @@ class IvozProvider_Klear_Filter_RetailDDIs implements KlearMatrix_Model_Field_Se
 
     public function setRouteDispatcher(KlearMatrix_Model_RouteDispatcher $routeDispatcher)
     {
+        // Get ModelName and your Controller
+        $currentItemName = $routeDispatcher->getCurrentItemName();
+
+        $unfilteredScreens = array(
+            "retailAccountsList_screen",
+            "retailAccountsDel_dialog",
+            "callForwardSettingsDel_dialog",
+        );
+
+        if (in_array($currentItemName, $unfilteredScreens)) {
+            return true;
+        }
+
         // Get current pk
         $pk = $routeDispatcher->getParam("pk", false);
         $this->_condition[] = "`retailAccountId` = '" . $pk . "'";
