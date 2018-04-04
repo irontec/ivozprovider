b4419639efc11ee2b031ccc01168f65442a4199e
ccruz <carlos@irontec.com>
2018-04-04 13:38:45 +0200

    library: add agi cache clear to helper script

--- library/bin/cache_clear.sh
+++ library/bin/cache_clear.sh
@@ -11,6 +11,11 @@ pushd /opt/irontec/ivozprovider/web/rest
     php bin/console api:swagger:export > web/swagger.json
 popd
 
+pushd /opt/irontec/ivozprovider/asterisk/agi
+    php bin/console cache:clear -e prod
+    php bin/console cache:clear -e dev --no-warmup
+popd
+
 pushd /opt/irontec/ivozprovider/microservices/recordings
     php bin/console cache:clear -e prod
     php bin/console cache:clear -e dev --no-warmup
