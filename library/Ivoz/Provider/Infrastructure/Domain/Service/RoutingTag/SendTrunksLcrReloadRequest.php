9c5c3f51664caec16e4eceb4c2158522a742241c
Kaian <kaian@irontec.com>
2018-05-25 11:24:05 +0200

    scheme: add new table RoutingTag and relation fields

---
+++ library/Ivoz/Provider/Infrastructure/Domain/Service/RoutingTag/SendTrunksLcrReloadRequest.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Ivoz\Provider\Infrastructure\Domain\Service\RoutingTag;
+
+use Ivoz\Core\Infrastructure\Domain\Service\XmlRpc\XmlRpcTrunksRequest;
+use Ivoz\Provider\Domain\Model\RoutingTag\RoutingTagInterface;
+use Ivoz\Provider\Domain\Service\RoutingTag\RoutingTagLifecycleEventHandlerInterface;
+
+class SendTrunksLcrReloadRequest implements RoutingTagLifecycleEventHandlerInterface
+{
+    protected $trunksLcrReload;
+
+    public function __construct(
+        XmlRpcTrunksRequest $trunksLcrReload
+    ) {
+        $this->trunksLcrReload = $trunksLcrReload;
+    }
+
+    public static function getSubscribedEvents()
+    {
+        return [
+            self::EVENT_ON_COMMIT => 10
+        ];
+    }
+
+    public function execute(RoutingTagInterface $entity, $isNew)
+    {
+        $this->trunksLcrReload->send();
+    }
+}
