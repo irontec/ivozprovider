a0fb26617ea66265749f0a6c6fae31b6e57e31d1
Mikel Madariaga <mikel@irontec.com>
2018-06-06 12:31:16 +0200

    tests: Added RoutingPattern RoutingTag LifeCycle tests

---
+++ library/DataFixtures/ORM/ProviderRoutingTag.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace DataFixtures\ORM;
+
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Common\DataFixtures\DependentFixtureInterface;
+use Doctrine\Common\Persistence\ObjectManager;
+use Doctrine\ORM\Mapping\ClassMetadata;
+use Ivoz\Provider\Domain\Model\RoutingTag\RoutingTag;
+
+class ProviderRoutingTag extends Fixture implements DependentFixtureInterface
+{
+    use \DataFixtures\FixtureHelperTrait;
+
+    /**
+     * {@inheritDoc}
+     */
+    public function load(ObjectManager $manager)
+    {
+        $this->disableLifecycleEvents($manager);
+        $manager
+            ->getClassMetadata(RoutingTag::class)
+            ->setIdGeneratorType(ClassMetadata::GENERATOR_TYPE_NONE);
+
+        $item1 = $this->createEntityInstanceWithPublicMethods(RoutingTag::class);
+        $item1->setName("TagName");
+        $item1->setTag('aTag');
+        $item1->setBrand(
+            $this->getReference('_reference_ProviderBrand1')
+        );
+        $this->addReference('_reference_ProviderRoutingTag1', $item1);
+        $this->sanitizeEntityValues($item1);
+        $manager->persist($item1);
+
+        $manager->flush();
+    }
+
+    public function getDependencies()
+    {
+        return array(
+            ProviderBrand::class
+        );
+    }
+}
---
+++ web/rest/tests/Provider/RoutingTag/RoutingTagLifeCycleTest.php
@@ -0,0 +1,89 @@
+<?php
+
+namespace Tests\Provider\RoutingTag;
+
+use Ivoz\Kam\Domain\Model\TrunksLcrRule\TrunksLcrRule;
+use Ivoz\Provider\Domain\Model\OutgoingRouting\OutgoingRouting;
+use Ivoz\Provider\Domain\Model\RoutingTag\RoutingTag;
+use Ivoz\Provider\Domain\Model\RoutingTag\RoutingTagDto;
+use Symfony\Bundle\FrameworkBundle\Test\KernelTestCase;
+use Tests\DbIntegrationTestHelperTrait;
+
+class RoutingTagLifeCycleTest extends KernelTestCase
+{
+    use DbIntegrationTestHelperTrait;
+
+    /**
+     * @return OutgoingRouting
+     */
+    protected function getRoutingTagDto()
+    {
+        $routingTagDto = new RoutingTagDto();
+        $routingTagDto
+            ->setName('1')
+            ->setTag('2')
+            ->setBrandId(1);
+
+        return $routingTagDto;
+    }
+
+    /**
+     * @return OutgoingRouting
+     */
+    protected function addRoutingTag()
+    {
+        return $this
+            ->entityPersister
+            ->persistDto($this->getRoutingTagDto(), null, true);
+    }
+
+    /**
+     * @test
+     */
+    public function it_persists_routing_tags()
+    {
+        $routingTagRepository = $this->em
+            ->getRepository(RoutingTag::class);
+
+        $fixtureRoutingTag = $routingTagRepository->findAll();
+        $this->assertCount(1, $fixtureRoutingTag);
+
+        $this->addRoutingTag();
+
+        $routingTags = $routingTagRepository->findAll();
+        $this->assertCount(
+            count($fixtureRoutingTag) + 1,
+            $routingTags
+        );
+    }
+
+    /**
+     * @test
+     */
+    public function updated_tag_updates_trunks_lcr_rule()
+    {
+        $outgoingRoutingRepository = $this->em
+            ->getRepository(OutgoingRouting::class);
+
+        /** @var OutgoingRouting $outgoingRouting */
+        $outgoingRouting = $outgoingRoutingRepository->findOneBy(
+            ['routingTag' => 1]
+        );
+
+        $routingTag = $outgoingRouting->getRoutingTag();
+        $routingTagDto = $this->entityTools->entityToDto($routingTag);
+
+        $routingTagDto->setTag('Something');
+        $this->entityTools->persistDto(
+            $routingTagDto,
+            $routingTag,
+            true
+        );
+
+        $lcrRules = $this->getChangelogByClass(
+            TrunksLcrRule::class
+        );
+
+        $this->assertGreaterThan(0, count($lcrRules));
+    }
+}
