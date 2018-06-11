c7d71acca795b9ee52b28b83d0ba504e73bb9a52
Mikel Madariaga <mikel@irontec.com>
2018-06-11 15:19:30 +0200

    core: clean up extensions properly on user delete

--- library/Ivoz/Provider/Domain/Service/Extension/UpdateByUser.php
+++ library/Ivoz/Provider/Domain/Service/Extension/UpdateByUser.php
@@ -11,29 +11,49 @@ use Ivoz\Provider\Domain\Service\User\UserLifecycleEventHandlerInterface;
  */
 class UpdateByUser implements UserLifecycleEventHandlerInterface
 {
+    const POST_PERSIST_PRIORITY = 20;
+    const POST_REMOVE_PRIORITY = 10;
+
     public function __construct() {}
 
     public static function getSubscribedEvents()
     {
         return [
-            self::EVENT_POST_PERSIST => 20
+            self::EVENT_POST_PERSIST => self::POST_PERSIST_PRIORITY,
+            self::EVENT_POST_REMOVE => self::POST_REMOVE_PRIORITY,
         ];
     }
 
-    public function execute(UserInterface $entity, $isNew)
+    public function execute(UserInterface $user, $isNew)
     {
-        $hasChangedExtension = $entity->hasChanged("extensionId");
+        $removed = ($user->hasChanged('id') && !$user->getId());
+        if ($removed) {
+            $this->cleanUpExtension($user);
+            return;
+        }
+
+        $hasChangedExtension = $user->hasChanged("extensionId");
         if (!$hasChangedExtension) {
             return;
         }
 
         // If extension has changed, update extension user
 
-        $extension = $entity->getExtension();
+        $extension = $user->getExtension();
         if ($extension) {
             $extension
                 ->setRouteType('user')
-                ->setUser($entity);
+                ->setUser($user);
+        }
+    }
+
+    private function cleanUpExtension(UserInterface $entity)
+    {
+        $extension = $entity->getExtension();
+        if ($extension) {
+            $extension
+                ->setRouteType(null)
+                ->setUser(null);
         }
     }
 }
