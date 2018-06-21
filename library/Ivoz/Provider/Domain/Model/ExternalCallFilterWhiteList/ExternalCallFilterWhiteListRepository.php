754bc60ea72a3586871400f4bf9409d316a9a15b
Mikel Madariaga <mikel@irontec.com>
2018-06-21 09:54:59 +0200

    core: autogenerated repositories

---
+++ library/Ivoz/Provider/Domain/Model/ExternalCallFilterWhiteList/ExternalCallFilterWhiteListRepository.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Ivoz\Provider\Domain\Model\ExternalCallFilterWhiteList;
+
+use Doctrine\Common\Persistence\ObjectRepository;
+use Doctrine\Common\Collections\Selectable;
+
+interface ExternalCallFilterWhiteListRepository extends  ObjectRepository, Selectable
+{
+
+}
+
+
