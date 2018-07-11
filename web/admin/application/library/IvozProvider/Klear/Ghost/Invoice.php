67dacb82ccc8dd0abbec2f50b4bffabdc71347a9
Kaian <kaian@irontec.com>
2018-07-11 16:32:02 +0200

    web/admin: ghost field for Invoices status column

---
+++ web/admin/application/library/IvozProvider/Klear/Ghost/Invoice.php
@@ -0,0 +1,38 @@
+<?php
+
+
+use Ivoz\Provider\Domain\Model\Invoice\InvoiceDto;
+
+class IvozProvider_Klear_Ghost_Invoice extends KlearMatrix_Model_Field_Ghost_Abstract
+{
+    /**
+     * @param InvoiceDto $invoice
+     * @return mixed|string
+     * @throws Exception
+     */
+    public function getStatus(InvoiceDto $invoice)
+    {
+        // Status translated to web language
+        $status = Klear_Model_Gettext::gettextCheck(
+            sprintf("_('%s')",
+                    ucfirst($invoice->getStatus())
+            )
+        );
+
+        // Status Message translated to web language (if possible)
+        $statusMsg = Klear_Model_Gettext::gettextCheck(
+            sprintf("_('%s')",
+                ucfirst($invoice->getStatusMsg())
+            )
+        );
+
+        if ($invoice->getStatus() === 'error') {
+            return sprintf("%s <span class='ui-silk inline ui-silk-exclamation' title='%s'></span>",
+                $status,
+                $statusMsg
+            );
+        }
+
+        return $status;
+    }
+}
