6155205918614c9cf0acb3fe8676180c6298a1a2
Kaian <kaian@irontec.com>
2018-07-16 11:57:42 +0200

    web/admin: update screens for new Retail client type

--- web/admin/application/library/IvozProvider/Klear/Auth/User.php
+++ web/admin/application/library/IvozProvider/Klear/Auth/User.php
@@ -76,6 +76,7 @@ class User extends \Klear_Model_UserAdvanced
         $this->companyVPBX = $company->getType() === Company::VPBX;
         $this->companyResidential = $company->getType() === Company::RESIDENTIAL;
         $this->companyWholesale = $company->getType() === Company::WHOLESALE;
+        $this->companyRetail = $company->getType() === Company::RETAIL;
     }
 
     public function getCompany()
--- web/admin/application/library/IvozProvider/Klear/Filter/Features.php
+++ web/admin/application/library/IvozProvider/Klear/Filter/Features.php
@@ -38,6 +38,7 @@ class IvozProvider_Klear_Filter_Features implements KlearMatrix_Model_Field_Sele
             if ($featureId == Feature::INVOICES) continue;
             if ($featureId == Feature::RESIDENTIAL) continue;
             if ($featureId == Feature::WHOLESALE) continue;
+            if ($featureId == Feature::RETAIL) continue;
             $featureIds[] = $featureId;
         }
 
