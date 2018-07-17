53680cb85887c96b3cbd845013a88da93358543e
Kaian <kaian@irontec.com>
2018-07-17 17:10:18 +0200

    core: implement lifecycle to load kam_trunks_address rows

---
+++ library/Ivoz/Kam/Domain/Service/TrunksAddress/UpdateByDdiProviderAddress.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace Ivoz\Kam\Domain\Service\TrunksAddress;
+
+use Ivoz\Core\Application\Service\EntityTools;
+use Ivoz\Kam\Domain\Model\TrunksAddress\TrunksAddressDto;
+use Ivoz\Provider\Domain\Model\DdiProviderAddress\DdiProviderAddressInterface;
+use Ivoz\Provider\Domain\Service\DdiProviderAddress\DdiProviderAddressLifecycleEventHandlerInterface;
+
+/**
+ * Class UpdateByDdiProviderAddress
+ * @package Ivoz\Kam\Domain\Service\TrunksLcrGateway
+ */
+class UpdateByDdiProviderAddress implements DdiProviderAddressLifecycleEventHandlerInterface
+{
+    CONST POST_PERSIST_PRIORITY = self::PRIORITY_NORMAL;
+
+    /**
+     * @var EntityTools
+     */
+    protected $entityTools;
+
+    public function __construct(
+        EntityTools $entityTools
+    ) {
+        $this->entityTools = $entityTools;
+    }
+
+    public static function getSubscribedEvents()
+    {
+        return [
+            self::EVENT_POST_PERSIST => self::POST_PERSIST_PRIORITY
+        ];
+    }
+
+    public function execute(DdiProviderAddressInterface $ddiProviderAddress, $isNew)
+    {
+        $trunksAddress = $ddiProviderAddress->getTrunksAddress();
+
+        $trunksAddressDto = ($trunksAddress)
+            ? $this->entityTools->entityToDto($trunksAddress)
+            : new TrunksAddressDto();
+
+        // Update/Create Trunks Address for this DDI Provider Address
+        $trunksAddressDto
+            ->setIpAddr($ddiProviderAddress->getIp())
+            ->setGrp($ddiProviderAddress->getDdiProvider()->getId())
+            ->setDdiProviderAddressId($ddiProviderAddress->getId());
+
+        $trunksAddress = $this->entityTools->persistDto(
+            $trunksAddressDto,
+            $trunksAddress,
+            true
+        );
+
+        $ddiProviderAddress->setTrunksAddress($trunksAddress);
+    }
+}
---
+++ library/Ivoz/Provider/Domain/Service/DdiProviderAddress/DdiProviderAddressLifecycleEventHandlerInterface.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace Ivoz\Provider\Domain\Service\DdiProviderAddress;
+
+use Ivoz\Core\Domain\Service\LifecycleEventHandlerInterface;
+use Ivoz\Provider\Domain\Model\DdiProviderAddress\DdiProviderAddressInterface;
+
+interface DdiProviderAddressLifecycleEventHandlerInterface extends LifecycleEventHandlerInterface
+{
+    public function execute(DdiProviderAddressInterface $entity, $isNew);
+}
