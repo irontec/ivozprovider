cc47bd9daf4f0ea4b16ef68c8409915eba570bb8
Kaian <kaian@irontec.com>
2018-07-17 17:48:06 +0200

    core: lifecycle for reloading proxy on kam_trunks_address changes

---
+++ library/Ivoz/Kam/Domain/Service/TrunksAddress/TrunksAddressLifecycleEventHandlerInterface.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace Ivoz\Kam\Domain\Service\TrunksAddress;
+
+use Ivoz\Core\Domain\Service\LifecycleEventHandlerInterface;
+use Ivoz\Kam\Domain\Model\TrunksAddress\TrunksAddressInterface;
+
+interface TrunksAddressLifecycleEventHandlerInterface extends LifecycleEventHandlerInterface
+{
+    public function execute(TrunksAddressInterface $entity);
+}
