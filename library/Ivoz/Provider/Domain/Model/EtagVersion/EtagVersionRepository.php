e2bb7a20da3a33e0cf42437ae6cbca63461f9162
Kaian <kaian@irontec.com>
2018-07-20 17:08:05 +0200

    scheme: remove unused table ETagVersions

--- library/Ivoz/Provider/Domain/Model/EtagVersion/EtagVersionRepository.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace Ivoz\Provider\Domain\Model\EtagVersion;
-
-use Doctrine\Common\Persistence\ObjectRepository;
-use Doctrine\Common\Collections\Selectable;
-
-interface EtagVersionRepository extends ObjectRepository, Selectable {}
-
