27c24ff4d9d26d14f6069107df0878b6fa11870e
Mikel Madariaga <mikel@irontec.com>
2018-07-27 10:47:57 +0200

    tests: minor e2e error prevention enhancements

--- web/admin/e2e/tests/steps/edit.js
+++ web/admin/e2e/tests/steps/edit.js
@@ -1,34 +1,32 @@
 const {client} = require('nightwatch-cucumber');
-const {defineSupportCode} = require('cucumber');
+const {Given, Then, When} = require('cucumber');
 var edit = client.page.edit();
 
-defineSupportCode(({Given, Then, When}) => {
-
-  When(/^I fill out the form with "([^"]*)" data fixture$/, (fixture) => {
-      return edit.fillOutFormByFixture(fixture);
-  });
-
-  When(/^I compare the form data with "([^"]*)" data fixture$/, (fixture) => {
-      return edit.compareFormDataWithFixture(fixture);
-  });
-
-  When(/^I click on save button$/, () => {
-    return edit.save();
-  });
-
-  Then(/^I can see confirmation dialog$/, () => {
-    return edit.assertConfirmationDialog();
-  });
-
-  Then(/^I can see confirmation dialog within "([^"]*)" seconds or lower$/, (timeoutSeconds) => {
-    return edit.assertConfirmationDialog(timeoutSeconds);
-  });
-
-  When(/^I click on close dialog button$/, () => {
-    return edit.closeDialog();
-  });
-
-  When(/^I click on close button$/, () => {
-    return edit.close();
-  });
+When(/^I fill out the form with "([^"]*)" data fixture$/, (fixture) => {
+    return edit.fillOutFormByFixture(fixture);
 });
+
+When(/^I compare the form data with "([^"]*)" data fixture$/, (fixture) => {
+    return edit.compareFormDataWithFixture(fixture);
+});
+
+When(/^I click on save button$/, () => {
+  return edit.save();
+});
+
+Then(/^I can see confirmation dialog$/, () => {
+  return edit.assertConfirmationDialog();
+});
+
+Then(/^I can see confirmation dialog within "([^"]*)" seconds or lower$/, (timeoutSeconds) => {
+  return edit.assertConfirmationDialog(timeoutSeconds);
+});
+
+When(/^I click on close dialog button$/, () => {
+  return edit.closeDialog();
+});
+
+When(/^I click on close button$/, () => {
+  return edit.close();
+});
+
