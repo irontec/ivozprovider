c2940f446099f2b536741660476c03c5cdd9a857
Mikel Madariaga <mikel@irontec.com>
2018-08-21 13:18:28 +0200

    core: updated api path

--- web/admin/public/e2e.php
+++ web/admin/public/e2e.php
@@ -2,8 +2,8 @@
 require_once 'Zend/Registry.php';
 
 /** @var \Composer\Autoload\ClassLoader $loader */
-$loader = require __DIR__.'/../../rest/app/autoload.php';
-include_once __DIR__.'/../../rest/var/bootstrap.php.cache';
+$loader = require __DIR__.'/../../rest/platform/app/autoload.php';
+include_once __DIR__.'/../../rest/platform/var/bootstrap.php.cache';
 
 $kernel = new AppKernel('test_e2e', false);
 $kernel->boot();
