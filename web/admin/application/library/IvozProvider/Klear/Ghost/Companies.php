fd13956d2fbf2471ed3627ad5ad25a2f78d5fdf9
Kaian <kaian@irontec.com>
2018-09-10 14:46:41 +0200

    web/admin: Carrier/Company balances changed to ghost fields

--- web/admin/application/library/IvozProvider/Klear/Ghost/Companies.php
+++ web/admin/application/library/IvozProvider/Klear/Ghost/Companies.php
@@ -1,14 +1,27 @@
 <?php
 
-use Ivoz\Core\Application\Service\DataGateway;
 use Ivoz\Provider\Domain\Model\Brand\Brand;
 use Ivoz\Provider\Domain\Model\Brand\BrandDto;
 use Ivoz\Provider\Domain\Model\Company\Company;
 use Ivoz\Provider\Domain\Model\Company\CompanyDto;
+use Ivoz\Provider\Domain\Service\Company\CompanyBalanceServiceInterface;
 use IvozProvider\Utils\SizeFormatter;
 
 class IvozProvider_Klear_Ghost_Companies extends KlearMatrix_Model_Field_Ghost_Abstract
 {
+    /**
+     * @var CompanyBalanceServiceInterface
+     */
+    private $fetchCompanyBalance;
+
+    public function __construct()
+    {
+        $serviceContainer = \Zend_Registry::get('container');
+        $this->fetchCompanyBalance = $serviceContainer->get(
+            CompanyBalanceServiceInterface::class
+        );
+    }
+
     /**
      * @param CompanyDto $model
      * @return string
@@ -27,5 +40,17 @@ class IvozProvider_Klear_Ghost_Companies extends KlearMatrix_Model_Field_Ghost_A
             default:
                 return $model->getType();
         }
+    }
+
+    /**
+     * @param CompanyDto $companyDto
+     * @return string
+     */
+    public function getBalance(CompanyDto $companyDto)
+    {
+        return $this->fetchCompanyBalance->getBalance(
+            $companyDto->getBrandId(),
+            $companyDto->getId()
+        );
     }
 }
