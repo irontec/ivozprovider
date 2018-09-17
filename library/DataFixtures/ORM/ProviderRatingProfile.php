233c7d7c85a458ff3eeb8b10561119b525454a55
Kaian <kaian@irontec.com>
2018-09-17 15:47:14 +0200

    tests: update ORM tests with new RatingPlanGroup entity

--- library/DataFixtures/ORM/ProviderRatingProfile.php
+++ library/DataFixtures/ORM/ProviderRatingProfile.php
@@ -26,7 +26,7 @@ class ProviderRatingProfile extends Fixture implements DependentFixtureInterface
         $item1 = $this->createEntityInstanceWithPublicMethods(RatingProfile::class);
 
         $item1->setCompany($this->getReference('_reference_ProviderCompany1'));
-        $item1->setRatingPlan($this->getReference('_reference_ProviderRatingPlan1'));
+        $item1->setRatingPlanGroup($this->getReference('_reference_ProviderRatingPlanGroup1'));
         $item1->setActivationTime(new \DateTime('2018-02-02 20:20:20'));
         $this->addReference('_reference_ProviderRatingProfile1', $item1);
         $this->sanitizeEntityValues($item1);
@@ -36,7 +36,7 @@ class ProviderRatingProfile extends Fixture implements DependentFixtureInterface
         $item2 = $this->createEntityInstanceWithPublicMethods(RatingProfile::class);
 
         $item2->setCompany($this->getReference('_reference_ProviderCompany1'));
-        $item2->setRatingPlan($this->getReference('_reference_ProviderRatingPlan2'));
+        $item2->setRatingPlanGroup($this->getReference('_reference_ProviderRatingPlanGroup2'));
         $item2->setActivationTime(new \DateTime('2018-02-02 20:20:20'));
         $item2->setRoutingTag($this->getReference('_reference_ProviderRoutingTag1'));
         $this->addReference('_reference_ProviderRatingProfile2', $item2);
@@ -50,7 +50,7 @@ class ProviderRatingProfile extends Fixture implements DependentFixtureInterface
     {
         return array(
             ProviderCompany::class,
-            ProviderRatingPlans::class,
+            ProviderRatingPlanGroups::class,
             ProviderRoutingTag::class
         );
     }
