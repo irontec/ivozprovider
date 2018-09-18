431819108e1fd6d8f2d9c53cc29ec529315e83f3
Mikel Madariaga <mikel@irontec.com>
2018-09-18 12:47:28 +0200

    web/rest: added RatingPlanPrices endpoint

---
+++ web/rest/brand/src/Controller/My/RatingPlanPricesAction.php
@@ -0,0 +1,136 @@
+<?php
+
+namespace Controller\My;
+
+use ApiPlatform\Core\Bridge\Doctrine\Orm\Util\QueryNameGenerator;
+use ApiPlatform\Core\Exception\ResourceClassNotFoundException;
+use Doctrine\ORM\QueryBuilder;
+use Ivoz\Api\Doctrine\Orm\Extension\CollectionExtensionList;
+use Ivoz\Provider\Domain\Model\RatingPlanGroup\RatingPlanGroup;
+use Ivoz\Provider\Domain\Model\RatingPlanGroup\RatingPlanGroupRepository;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\RequestStack;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Security\Core\Authentication\Token\Storage\TokenStorage;
+use Symfony\Component\Security\Core\Authentication\Token\TokenInterface;
+
+class RatingPlanPricesAction
+{
+    /**
+     * @var TokenStorage
+     */
+    protected $tokenStorage;
+
+    /**
+     * @var RatingPlanGroupRepository
+     */
+    protected $ratingPlanGroupRepository;
+
+    /**
+     * @var CollectionExtensionList
+     */
+    protected $collectionExtensions;
+
+    /**
+     * @var RequestStack
+     */
+    protected $requestStack;
+
+    public function __construct (
+        TokenStorage $tokenStorage,
+        RatingPlanGroupRepository $ratingPlanGroupRepository,
+        CollectionExtensionList $collectionExtensions,
+        RequestStack $requestStack
+    ) {
+        $this->tokenStorage = $tokenStorage;
+        $this->ratingPlanGroupRepository = $ratingPlanGroupRepository;
+        $this->collectionExtensions = $collectionExtensions;
+        $this->requestStack = $requestStack;
+    }
+
+    public function __invoke()
+    {
+        /** @var Request $request */
+        $request = $this->requestStack->getCurrentRequest();
+
+        /** @var TokenInterface $token */
+        $token =  $this->tokenStorage->getToken();
+
+        if (!$token || !$token->getUser()) {
+            throw new ResourceClassNotFoundException('User not found');
+        }
+
+        $collectionExtensions = $this->collectionExtensions;
+        $queryModifier = function (QueryBuilder $qb) use ($collectionExtensions, $request) {
+
+            $queryArguments = $request->query->all();
+            unset($queryArguments['id']);
+
+            foreach ($queryArguments as $name => $value) {
+
+                /** @see https://stackoverflow.com/questions/45060606/doctrine-substring-field-with-alias-in-where-clause **/
+                $qb
+                    ->andHaving("$name = :$name")
+                    ->setParameter($name, $value);
+            }
+
+            $queryNameGenerator = new QueryNameGenerator();
+            foreach ($collectionExtensions->get() as $extension) {
+
+                $entityClass = RatingPlanGroup::class;
+                $operationName = 'get_my_rating_plan_prices';
+
+                $extension->applyToCollection(
+                    $qb,
+                    $queryNameGenerator,
+                    $entityClass,
+                    $operationName
+                );
+            }
+        };
+
+        $page = $request->query->has('_page');
+        if ($page) {
+            throw new \DomainException('_page querystring arguments is not supported here');
+        }
+        $itemsPerPage = $request->query->has('_itemsPerPage');
+        if ($itemsPerPage) {
+            throw new \DomainException('_itemsPerPage querystring arguments is not supported here');
+        }
+
+        $ratingPlanGroupId = $request
+            ->query
+            ->get('id');
+
+        $generator = $this
+            ->ratingPlanGroupRepository
+            ->getAllRatesByRatingPlanId(
+                $ratingPlanGroupId,
+                10000,
+                $queryModifier
+            );
+
+        $payload = '';
+
+        foreach ($generator as $batch) {
+            foreach ($batch as $item) {
+                $payload .= $this->array2csv($item) . "\n";
+            }
+        }
+
+        return new Response($payload);
+    }
+
+    private function array2csv(array $fields) : string
+    {
+        unset($fields['weight']);
+
+        $f = fopen('php://memory', 'r+');
+        if (fputcsv($f, $fields) === false) {
+            return false;
+        }
+        rewind($f);
+        $csv_line = stream_get_contents($f);
+        return rtrim($csv_line);
+    }
+}
---
+++ web/rest/brand/src/Model/RatingPlanPrices.php
@@ -0,0 +1,251 @@
+<?php
+
+namespace Model;
+
+use Ivoz\Api\Core\Annotation\AttributeDefinition;
+
+/**
+ * Class RatingPlanPrices
+ * @package Model
+ * @codeCoverageIgnore
+ */
+class RatingPlanPrices
+{
+    /**
+     * @var string
+     * @AttributeDefinition(type="string")
+     */
+    protected $ratingPlan;
+
+    /**
+     * @var string
+     * @AttributeDefinition(type="string")
+     */
+    protected $name;
+
+    /**
+     * @var string
+     * @AttributeDefinition(type="string")
+     */
+    protected $prefix;
+
+    /**
+     * @var float
+     * @AttributeDefinition(type="float")
+     */
+    protected $connectFee;
+
+    /**
+     * @var float
+     * @AttributeDefinition(type="float")
+     */
+    protected $cost;
+
+    /**
+     * @var string
+     * @AttributeDefinition(type="string")
+     */
+    protected $rateIncrement;
+
+    /**
+     * @var string
+     * @AttributeDefinition(type="string")
+     */
+    protected $groupIntervalStart;
+
+    /**
+     * @var string
+     * @AttributeDefinition(type="string")
+     */
+    protected $timeIn;
+
+    /**
+     * @var string
+     * @AttributeDefinition(type="string")
+     */
+    protected $days;
+
+    public function __construct()
+    {
+
+    }
+
+    public static function fromArray(array $data)
+    {
+        $instance = new self();
+        $instance
+            ->setRatingPlan($data['ratingPlan'])
+            ->setName($data['name'])
+            ->setPrefix($data['prefix'])
+            ->setConnectFee($data['connectFee'])
+            ->setCost($data['cost'])
+            ->setRateIncrement($data['rateIncrement'])
+            ->setGroupIntervalStart($data['groupIntervalStart'])
+            ->setTimeIn($data['timeIn'])
+            ->setDays($data['days']);
+
+        return $instance;
+    }
+
+    /**
+     * @return string
+     */
+    public function getRatingPlan(): string
+    {
+        return $this->ratingPlan;
+    }
+
+    /**
+     * @param string $ratingPlan
+     * @return static
+     */
+    public function setRatingPlan(string $ratingPlan): self
+    {
+        $this->ratingPlan = $ratingPlan;
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function getName(): string
+    {
+        return $this->name;
+    }
+
+    /**
+     * @param string $name
+     * @return static
+     */
+    public function setName(string $name): self
+    {
+        $this->name = $name;
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function getPrefix(): string
+    {
+        return $this->prefix;
+    }
+
+    /**
+     * @param string $prefix
+     * @return static
+     */
+    public function setPrefix(string $prefix): self
+    {
+        $this->prefix = $prefix;
+        return $this;
+    }
+
+    /**
+     * @return float
+     */
+    public function getConnectFee(): float
+    {
+        return $this->connectFee;
+    }
+
+    /**
+     * @param float $connectFee
+     * @return static
+     */
+    public function setConnectFee(float $connectFee): self
+    {
+        $this->connectFee = $connectFee;
+        return $this;
+    }
+
+    /**
+     * @return float
+     */
+    public function getCost(): float
+    {
+        return $this->cost;
+    }
+
+    /**
+     * @param float $cost
+     * @return static
+     */
+    public function setCost(float $cost): self
+    {
+        $this->cost = $cost;
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function getRateIncrement(): string
+    {
+        return $this->rateIncrement;
+    }
+
+    /**
+     * @param string $rateIncrement
+     * @return static
+     */
+    public function setRateIncrement(string $rateIncrement): self
+    {
+        $this->rateIncrement = $rateIncrement;
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function getGroupIntervalStart(): string
+    {
+        return $this->groupIntervalStart;
+    }
+
+    /**
+     * @param string $groupIntervalStart
+     * @return static
+     */
+    public function setGroupIntervalStart(string $groupIntervalStart): self
+    {
+        $this->groupIntervalStart = $groupIntervalStart;
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function getTimeIn(): string
+    {
+        return $this->timeIn;
+    }
+
+    /**
+     * @param string $timeIn
+     * @return static
+     */
+    public function setTimeIn(string $timeIn): self
+    {
+        $this->timeIn = $timeIn;
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function getDays(): string
+    {
+        return $this->days;
+    }
+
+    /**
+     * @param string $days
+     * @return static
+     */
+    public function setDays(string $days): self
+    {
+        $this->days = $days;
+        return $this;
+    }
+}
