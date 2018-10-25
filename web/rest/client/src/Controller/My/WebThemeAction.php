95b57aa1f699acb664de64806754bea5217b51c6
Kaian <kaian@irontec.com>
2018-10-25 13:21:01 +0200

    web/rest: properly encode logo URL for user web theme

--- web/rest/client/src/Controller/My/WebThemeAction.php
+++ web/rest/client/src/Controller/My/WebThemeAction.php
@@ -60,7 +60,7 @@ class WebThemeAction
             . '/fso/brandUrl/'
             . $brandUrlDto->getId()
             . '-'
-            . $brandUrlDto->getLogoBaseName();
+            . urlencode($brandUrlDto->getLogoBaseName());
 
         return new WebTheme(
             $brandUrlDto->getName(),
