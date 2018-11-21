4d90b0fc91bde92fb0f8e26da90aa3bdb33a5bfd
ccruz <carlos@irontec.com>
2018-11-21 14:06:05 +0100

    scheme: change saycid default value to yes

---
+++ scheme/app/DoctrineMigrations/Version20181121130028.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace Application\Migrations;
+
+use Doctrine\DBAL\Migrations\AbstractMigration;
+use Doctrine\DBAL\Schema\Schema;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+class Version20181121130028 extends AbstractMigration
+{
+    /**
+     * @param Schema $schema
+     */
+    public function up(Schema $schema)
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('ALTER TABLE ast_voicemail CHANGE saycid saycid VARCHAR(255) DEFAULT \'yes\'');
+        $this->addSql('UPDATE ast_voicemail SET saycid="yes"');
+    }
+
+    /**
+     * @param Schema $schema
+     */
+    public function down(Schema $schema)
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('UPDATE ast_voicemail SET saycid=NULL');
+        $this->addSql('ALTER TABLE ast_voicemail CHANGE saycid saycid VARCHAR(255) DEFAULT NULL');
+    }
+}
