irontec/ivozprovider

commit 633be0f4661658983303f17b33a6cb68d40391ed

core: refactored services

library/Ivoz/Provider/Domain/Service/BillableCall/CreateOrUpdateDtoByTrunksCdr.php
@@ -1,0 +1,85 @@
+<?php
+
+namespace Ivoz\Provider\Domain\Service\BillableCall;
+
+use Ivoz\Core\Application\Service\EntityTools;
+use Ivoz\Kam\Domain\Model\TrunksCdr\TrunksCdrDto;
+use Ivoz\Kam\Domain\Model\TrunksCdr\TrunksCdrInterface;
+use Ivoz\Provider\Domain\Model\BillableCall\BillableCall;
+use Ivoz\Provider\Domain\Model\BillableCall\BillableCallDto;
+use Ivoz\Provider\Domain\Model\BillableCall\BillableCallInterface;
+use Ivoz\Provider\Domain\Model\Carrier\CarrierInterface;
+
+class CreateOrUpdateDtoByTrunksCdr
+{
+    /**
+     * @var EntityTools
+     */
+    protected $entityTools;
+
+    public function __construct(
+        EntityTools $entityTools
+    ) {
+        $this->entityTools = $entityTools;
+    }
+
+    /**
+     * @param TrunksCdrInterface $trunksCdr
+     * @param TrunksCdrDto $trunksCdrDto
+     *
+     * @return BillableCallDto
+     */
+    public function execute(
+        TrunksCdrInterface $trunksCdr,
+        BillableCallInterface $billableCall = null
+    ) {
+        $billableCallDto = $billableCall
+            ? $this->entityTools->entityToDto($billableCall)
+            : BillableCall::createDto();
+
+        /**
+         * @var CarrierInterface $carrier
+         */
+        $carrier = $trunksCdr->getCarrier();
+
+        $carrierName = $carrier
+            ? $carrier->getName()
+            : '';
+
+        /**
+         * @var TrunksCdrDto $trunksCdrDto
+         */
+        $trunksCdrDto = $this->entityTools->entityToDto(
+            $trunksCdr
+        );
+
+        $caller = $trunksCdrDto->getDiversion()
+            ? $trunksCdrDto->getDiversion()
+            : $trunksCdrDto->getCaller();
+
+        $billableCallDto
+            ->setTrunksCdrId(
+                $trunksCdrDto->getId()
+            )->setBrandId(
+                $trunksCdrDto->getBrandId()
+            )->setCompanyId(
+                $trunksCdrDto->getCompanyId()
+            )->setCarrierId(
+                $trunksCdrDto->getCarrierId()
+            )->setCarrierName(
+                $carrierName
+            )->setCallid(
+                $trunksCdrDto->getCallid()
+            )->setCaller(
+                $caller
+            )->setCallee(
+                $trunksCdrDto->getCallee()
+            )->setStartTime(
+                $trunksCdrDto->getStartTime()
+            )->setDuration(
+                $trunksCdrDto->getDuration()
+            );
+
+        return $billableCallDto;
+    }
+}

library/Ivoz/Provider/Domain/Service/BillableCall/MigrateFromTrunksCdr.php
@@ -2,25 +2,13 @@
 
 namespace Ivoz\Provider\Domain\Service\BillableCall;
 
-use Ivoz\Cgr\Domain\Model\TpCdr\TpCdrInterface;
-use Ivoz\Cgr\Domain\Model\TpCdr\TpCdrRepository;
-use Ivoz\Cgr\Domain\Model\TpDestination\TpDestinationInterface;
-use Ivoz\Cgr\Domain\Model\TpDestination\TpDestinationRepository;
-use Ivoz\Cgr\Domain\Model\TpRatingPlan\TpRatingPlanInterface;
-use Ivoz\Cgr\Domain\Model\TpRatingPlan\TpRatingPlanRepository;
 use Ivoz\Core\Application\Service\EntityTools;
 use Ivoz\Kam\Domain\Model\TrunksCdr\TrunksCdrDto;
 use Ivoz\Kam\Domain\Model\TrunksCdr\TrunksCdrInterface;
 use Ivoz\Kam\Domain\Model\TrunksCdr\TrunksCdrRepository;
-use Ivoz\Provider\Domain\Model\BillableCall\BillableCall;
-use Ivoz\Provider\Domain\Model\BillableCall\BillableCallDto;
 use Ivoz\Provider\Domain\Model\BillableCall\BillableCallInterface;
 use Ivoz\Provider\Domain\Model\BillableCall\BillableCallRepository;
-use Ivoz\Provider\Domain\Model\Carrier\CarrierInterface;
-use Ivoz\Provider\Domain\Model\Company\Company;
-use Ivoz\Provider\Domain\Model\Destination\DestinationInterface;
-use Ivoz\Provider\Domain\Model\RatingPlan\RatingPlanInterface;
-use Ivoz\Provider\Domain\Model\RatingPlan\RatingPlanRepository;
+
 use Psr\Log\LoggerInterface;
 
 class MigrateFromTrunksCdr
@@ -33,24 +21,19 @@ class MigrateFromTrunksCdr
     protected $trunksCdrRepository;
 
     /**
-     * @var TpCdrRepository
-     */
-    protected $tpCdrRepository;
-
-    /**
-     * @var TpRatingPlanRepository
-     */
-    protected $tpRatingPlanRepository;
-
-    /**
-     * @var TpDestinationRepository
-     */
-    protected $tpDestinationRepository;
-
-    /**
      * @var BillableCallRepository
      */
     protected $billableCallRepository;
+
+    /**
+     * @var CreateOrUpdateDtoByTrunksCdr
+     */
+    protected $createOrUpdateBillableCallByTrunksCdr;
+
+    /**
+     * @var UpdateDtoByTpCdr
+     */
+    protected $updateBillableCallByTpCdr;
 
     /**
      * @var EntityTools
@@ -64,18 +47,16 @@ class MigrateFromTrunksCdr
 
     public function __construct(
         TrunksCdrRepository  $trunksCdrRepository,
-        TpCdrRepository $tpCdrRepository,
-        TpRatingPlanRepository $tpRatingPlanRepository,
-        TpDestinationRepository $tpDestinationRepository,
         BillableCallRepository $billableCallRepository,
+        CreateOrUpdateDtoByTrunksCdr $createOrUpdateBillableCallByTrunksCdr,
+        UpdateDtoByTpCdr $updateBillableCallByTpCdr,
         EntityTools $entityTools,
         LoggerInterface $logger
     ) {
         $this->trunksCdrRepository = $trunksCdrRepository;
-        $this->tpCdrRepository = $tpCdrRepository;
-        $this->tpRatingPlanRepository = $tpRatingPlanRepository;
-        $this->tpDestinationRepository = $tpDestinationRepository;
         $this->billableCallRepository = $billableCallRepository;
+        $this->createOrUpdateBillableCallByTrunksCdr = $createOrUpdateBillableCallByTrunksCdr;
+        $this->updateBillableCallByTpCdr = $updateBillableCallByTpCdr;
         $this->entityTools = $entityTools;
         $this->logger = $logger;
     }
@@ -118,12 +99,14 @@ class MigrateFromTrunksCdr
             $trunksCdr->getId()
         );
 
-        $billableCallDto = $this->createOrUpdateBillableCallByTrunksCdr(
-            $trunksCdr,
-            $billableCall
-        );
+        $billableCallDto = $this
+            ->createOrUpdateBillableCallByTrunksCdr
+            ->execute(
+                $trunksCdr,
+                $billableCall
+            );
 
-        $this->updateBillableCallByTpCdr(
+        $this->updateBillableCallByTpCdr->execute(
             $billableCallDto,
             $trunksCdr->getCgrid(),
             ucfirst($trunksCdr->getBrand()->getLanguageCode())
@@ -147,188 +130,5 @@ class MigrateFromTrunksCdr
             $trunksCdr,
             false
         );
-    }
-
-    /**
-     * @param TrunksCdrInterface $trunksCdr
-     * @param TrunksCdrDto $trunksCdrDto
-     *
-     * @return BillableCallDto
-     */
-    private function createOrUpdateBillableCallByTrunksCdr(
-        TrunksCdrInterface $trunksCdr,
-        BillableCallInterface $billableCall = null
-    ) {
-        $billableCallDto = $billableCall
-            ? $this->entityTools->entityToDto($billableCall)
-            : BillableCall::createDto();
-
-        /**
-         * @var CarrierInterface $carrier
-         */
-        $carrier = $trunksCdr->getCarrier();
-
-        $carrierName = $carrier
-            ? $carrier->getName()
-            : '';
-
-        /**
-         * @var TrunksCdrDto $trunksCdrDto
-         */
-        $trunksCdrDto = $this->entityTools->entityToDto(
-            $trunksCdr
-        );
-
-        $caller = $trunksCdrDto->getDiversion()
-            ? $trunksCdrDto->getDiversion()
-            : $trunksCdrDto->getCaller();
-
-        $billableCallDto
-            ->setTrunksCdrId(
-                $trunksCdrDto->getId()
-            )->setBrandId(
-                $trunksCdrDto->getBrandId()
-            )->setCompanyId(
-                $trunksCdrDto->getCompanyId()
-            )->setCarrierId(
-                $trunksCdrDto->getCarrierId()
-            )->setCarrierName(
-                $carrierName
-            )->setCallid(
-                $trunksCdrDto->getCallid()
-            )->setCaller(
-                $caller
-            )->setCallee(
-                $trunksCdrDto->getCallee()
-            )->setStartTime(
-                $trunksCdrDto->getStartTime()
-            )->setDuration(
-                $trunksCdrDto->getDuration()
-            );
-
-        if ($trunksCdrDto->getRetailAccountId()) {
-            $billableCallDto
-                ->setEndpointType('RetailAccount')
-                ->setEndpointId($trunksCdrDto->getRetailAccountId());
-        }
-
-        return $billableCallDto;
-    }
-
-    /**
-     * @param BillableCallDto $billableCallDto
-     * @param string $cgrid | null
-     * @param string $languageCode
-     * @return BillableCallDto
-     */
-    private function updateBillableCallByTpCdr(BillableCallDto $billableCallDto, string $cgrid = null, string $languageCode)
-    {
-        if (!$cgrid) {
-            return $billableCallDto;
-        }
-
-        /**
-         * @var TpCdrInterface $defaultRunTpCdr
-         */
-        $defaultRunTpCdr = $this->tpCdrRepository->getDefaultRunByCgrid(
-            $cgrid
-        );
-
-        if (!$defaultRunTpCdr) {
-            return $billableCallDto;
-        }
-
-        if (!$defaultRunTpCdr->getCostDetailsFirstTimespan()) {
-            return $billableCallDto;
-        }
-
-        $callee = $defaultRunTpCdr->getDestination()
-            ? $defaultRunTpCdr->getDestination()
-            : $billableCallDto->getCallee();
-
-        /**
-         * @var TpRatingPlanInterface $tpRatingPlan
-         */
-        $tpRatingPlan = $this->tpRatingPlanRepository->findOneByTag(
-            $defaultRunTpCdr->getRatingPlanTag()
-        );
-
-        $ratingPlan = $tpRatingPlan->getRatingPlan();
-        $ratingPlanGroup = $ratingPlan->getRatingPlanGroup();
-
-        $ratingPlanGroupId = $ratingPlanGroup
-            ? $ratingPlanGroup->getId()
-            : null;
-
-        $brandLangGetter = 'get' . $languageCode;
-        $ratingPlanGroupName = $ratingPlanGroup
-            ? $ratingPlanGroup->getName()->{$brandLangGetter}()
-            : '';
-
-        /** @var TpDestinationInterface $tpDestination */
-        $tpDestination = $this->tpDestinationRepository->findOneByTag(
-            $defaultRunTpCdr->getMatchedDestinationTag()
-        );
-        /** @var DestinationInterface $destination */
-        $destination = $tpDestination
-            ? $tpDestination->getDestination()
-            : null;
-
-        $destinationId = $destination
-            ? $destination->getId()
-            : null;
-
-        $brandLangGetter = 'get' . $languageCode;
-        $destinationName = $destination
-            ? $destination->getName()->{$brandLangGetter}()
-            : '';
-
-        $startTime = $defaultRunTpCdr->getStartTime()
-            ? $defaultRunTpCdr->getStartTime()
-            : $billableCallDto->getStartTime();
-
-        $duration = $defaultRunTpCdr->getDuration()
-            ? $defaultRunTpCdr->getDuration()
-            : $billableCallDto->getDuration();
-
-        $billableCallDto
-            ->setStartTime(
-                $startTime
-            )
-            ->setDuration(
-                $duration
-            )
-            ->setCallee(
-                $callee
-            )
-            ->setDestinationId(
-                $destinationId
-            )
-            ->setDestinationName(
-                $destinationName
-            )
-            ->setRatingPlanGroupId(
-                $ratingPlanGroupId
-            )
-            ->setRatingPlanName(
-                $ratingPlanGroupName
-            )->setPrice(
-                $defaultRunTpCdr->getCost()
-            );
-
-        /**
-         * @var TpCdrInterface $carrierRunTpCdr
-         */
-        $carrierRunTpCdr = $this->tpCdrRepository->getCarrierRunByCgrid(
-            $cgrid
-        );
-
-        if ($carrierRunTpCdr) {
-            $billableCallDto->setCost(
-                $carrierRunTpCdr->getCost()
-            );
-        }
-
-        return $billableCallDto;
     }
 }

library/Ivoz/Provider/Domain/Service/BillableCall/UpdateDtoByTpCdr.php
@@ -1,0 +1,157 @@
+<?php
+
+namespace Ivoz\Provider\Domain\Service\BillableCall;
+
+use Ivoz\Cgr\Domain\Model\TpCdr\TpCdrInterface;
+use Ivoz\Cgr\Domain\Model\TpCdr\TpCdrRepository;
+use Ivoz\Cgr\Domain\Model\TpDestination\TpDestinationInterface;
+use Ivoz\Cgr\Domain\Model\TpDestination\TpDestinationRepository;
+use Ivoz\Cgr\Domain\Model\TpRatingPlan\TpRatingPlanInterface;
+use Ivoz\Cgr\Domain\Model\TpRatingPlan\TpRatingPlanRepository;
+use Ivoz\Provider\Domain\Model\BillableCall\BillableCallDto;
+use Ivoz\Provider\Domain\Model\Destination\DestinationInterface;
+
+class UpdateDtoByTpCdr
+{
+    /**
+     * @var TpCdrRepository
+     */
+    protected $tpCdrRepository;
+
+    /**
+     * @var TpRatingPlanRepository
+     */
+    protected $tpRatingPlanRepository;
+
+    /**
+     * @var TpDestinationRepository
+     */
+    protected $tpDestinationRepository;
+
+    public function __construct(
+        TpCdrRepository $tpCdrRepository,
+        TpRatingPlanRepository $tpRatingPlanRepository,
+        TpDestinationRepository $tpDestinationRepository
+    ) {
+        $this->tpCdrRepository = $tpCdrRepository;
+        $this->tpRatingPlanRepository = $tpRatingPlanRepository;
+        $this->tpDestinationRepository = $tpDestinationRepository;
+    }
+
+    /**
+     * @param BillableCallDto $billableCallDto
+     * @param string $cgrid | null
+     * @param string $languageCode
+     * @return BillableCallDto
+     */
+    public function execute(BillableCallDto $billableCallDto, string $cgrid = null, string $languageCode)
+    {
+        if (!$cgrid) {
+            return $billableCallDto;
+        }
+
+        /**
+         * @var TpCdrInterface $defaultRunTpCdr
+         */
+        $defaultRunTpCdr = $this->tpCdrRepository->getDefaultRunByCgrid(
+            $cgrid
+        );
+
+        if (!$defaultRunTpCdr) {
+            return $billableCallDto;
+        }
+
+        if (!$defaultRunTpCdr->getCostDetailsFirstTimespan()) {
+            return $billableCallDto;
+        }
+
+        $callee = $defaultRunTpCdr->getDestination()
+            ? $defaultRunTpCdr->getDestination()
+            : $billableCallDto->getCallee();
+
+        /**
+         * @var TpRatingPlanInterface $tpRatingPlan
+         */
+        $tpRatingPlan = $this->tpRatingPlanRepository->findOneByTag(
+            $defaultRunTpCdr->getRatingPlanTag()
+        );
+
+        $ratingPlan = $tpRatingPlan->getRatingPlan();
+        $ratingPlanGroup = $ratingPlan->getRatingPlanGroup();
+
+        $ratingPlanGroupId = $ratingPlanGroup
+            ? $ratingPlanGroup->getId()
+            : null;
+
+        $brandLangGetter = 'get' . $languageCode;
+        $ratingPlanGroupName = $ratingPlanGroup
+            ? $ratingPlanGroup->getName()->{$brandLangGetter}()
+            : '';
+
+        /** @var TpDestinationInterface $tpDestination */
+        $tpDestination = $this->tpDestinationRepository->findOneByTag(
+            $defaultRunTpCdr->getMatchedDestinationTag()
+        );
+        /** @var DestinationInterface $destination */
+        $destination = $tpDestination
+            ? $tpDestination->getDestination()
+            : null;
+
+        $destinationId = $destination
+            ? $destination->getId()
+            : null;
+
+        $brandLangGetter = 'get' . $languageCode;
+        $destinationName = $destination
+            ? $destination->getName()->{$brandLangGetter}()
+            : '';
+
+        $startTime = $defaultRunTpCdr->getStartTime()
+            ? $defaultRunTpCdr->getStartTime()
+            : $billableCallDto->getStartTime();
+
+        $duration = $defaultRunTpCdr->getDuration()
+            ? $defaultRunTpCdr->getDuration()
+            : $billableCallDto->getDuration();
+
+        $billableCallDto
+            ->setStartTime(
+                $startTime
+            )
+            ->setDuration(
+                $duration
+            )
+            ->setCallee(
+                $callee
+            )
+            ->setDestinationId(
+                $destinationId
+            )
+            ->setDestinationName(
+                $destinationName
+            )
+            ->setRatingPlanGroupId(
+                $ratingPlanGroupId
+            )
+            ->setRatingPlanName(
+                $ratingPlanGroupName
+            )->setPrice(
+                $defaultRunTpCdr->getCost()
+            );
+
+        /**
+         * @var TpCdrInterface $carrierRunTpCdr
+         */
+        $carrierRunTpCdr = $this->tpCdrRepository->getCarrierRunByCgrid(
+            $cgrid
+        );
+
+        if ($carrierRunTpCdr) {
+            $billableCallDto->setCost(
+                $carrierRunTpCdr->getCost()
+            );
+        }
+
+        return $billableCallDto;
+    }
+}

library/Ivoz/Provider/Domain/Service/CallCsvReport/CreateByScheduler.php
@@ -26,7 +26,6 @@ class CreateByScheduler
         LoggerInterface $logger
     ) {
         $this->entityTools = $entityTools;
-        $this->restClient = $restClient;
         $this->logger = $logger;
     }
 
@@ -36,22 +35,17 @@ class CreateByScheduler
      */
     public function execute(CallCsvSchedulerInterface $scheduler)
     {
-
         $report = null;
 
         try {
-            $report = $this->createCallCsvReport($scheduler);
             $this->updateLastExecutionDate($scheduler);
+            $this->createCallCsvReport($scheduler);
         } catch (\Exception $e) {
             $name = $scheduler->getName();
             $this->logger->error(
-                "Call CSV scheduler #${$name} has failed: "
+                "Call CSV scheduler #${name} has failed: "
                 . $e->getMessage()
             );
-
-            if ($report && $report->getId()) {
-                $this->updateLastExecutionDate($scheduler);
-            }
 
             throw $e;
         }

library/Ivoz/Provider/Domain/Service/CallCsvReport/EmailSender.php
@@ -56,7 +56,7 @@ class EmailSender implements CallCsvReportLifecycleEventHandlerInterface
 
     public function execute(CallCsvReportInterface $callCsvReport, $isNew)
     {
-        if (!$callCsvReport->hasChanged('id')) {
+        if (!$callCsvReport->isNew()) {
             return;
         }
 

library/Ivoz/Provider/Domain/Service/Carrier/AbstractBalanceOperation.php
@@ -78,7 +78,7 @@ abstract class AbstractBalanceOperation
      */
     protected function handleResponse($amount, array $response, CarrierInterface $carrier)
     {
-        if ($response['error']) {
+        if (!empty($response['error'])) {
             $this->lastError = $response['error'];
             $this->logger->error('Could not modify balance: ' . $response['error']);
         }

commit 98e75baf2b9fde69e3c30f68ab7eca7e510bad60

core: recover endpointType and endpointId set logic
Fix regression introduced in 633be0f46

library/Ivoz/Provider/Domain/Service/BillableCall/CreateOrUpdateDtoByTrunksCdr.php
@@ -80,6 +80,12 @@ class CreateOrUpdateDtoByTrunksCdr
                 $trunksCdrDto->getDuration()
             );
 
+        if ($trunksCdrDto->getRetailAccountId()) {
+            $billableCallDto
+                ->setEndpointType('RetailAccount')
+                ->setEndpointId($trunksCdrDto->getRetailAccountId());
+        }
+
         return $billableCallDto;
     }
 }