19140022fcd3b1abccfedcbb8fea7491118aea2b
ccruz <carlos@irontec.com>
2018-12-21 12:47:15 +0100

    scheme: change activation_time type
    CGRateS does not handle datetime fields as expected.

---
+++ scheme/app/DoctrineMigrations/Version20181221113443.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Application\Migrations;
+
+use Doctrine\DBAL\Migrations\AbstractMigration;
+use Doctrine\DBAL\Schema\Schema;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+class Version20181221113443 extends AbstractMigration
+{
+    /**
+     * @param Schema $schema
+     */
+    public function up(Schema $schema)
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql("ALTER TABLE tp_rating_profiles CHANGE activation_time activation_time VARCHAR(32) DEFAULT '1970-01-01 00:00:00' NOT NULL");
+    }
+
+    /**
+     * @param Schema $schema
+     */
+    public function down(Schema $schema)
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql("ALTER TABLE tp_rating_profiles CHANGE activation_time activation_time DATETIME DEFAULT CURRENT_TIMESTAMP NOT NULL COMMENT '(DC2Type:datetime)'");
+    }
+}
