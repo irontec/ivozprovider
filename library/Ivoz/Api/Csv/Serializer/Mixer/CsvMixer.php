d9333742f678b97f34021ca54d167997fc18a9c1
mmadariaga <mikel@irontec.com>
2019-02-05 16:57:16 +0100

    web/rest: iterate during data serialization on unpaginated pages

---
+++ library/Ivoz/Api/Csv/Serializer/Mixer/CsvMixer.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace Ivoz\Api\Csv\Serializer\Mixer;
+
+use Ivoz\Api\Core\Serializer\Mixer\MixerInterface;
+
+class CsvMixer implements MixerInterface
+{
+    const FORMAT = 'csv';
+
+    public function __construct()
+    {
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function supportsFormat($format = null)
+    {
+        return static::FORMAT === $format;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function mix(array $segments)
+    {
+        for ($i=0; $i < count($segments); $i++) {
+            // Remove column names from every segment but the first one
+            $content = stream_get_contents($segments[$i]);
+
+            $segments[$i] = $i > 0
+                ? $this->removeFirstLine($content)
+                : $content;
+        }
+
+        return implode(
+            '',
+            $segments
+        );
+    }
+
+    private function removeFirstLine($content)
+    {
+        return substr($content, strpos($content, "\n")+1);
+    }
+}
