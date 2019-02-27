d763b5cfe6d48920225969af48fa67d41086836f
mmadariaga <mikel@irontec.com>
2019-02-27 11:16:00 +0100

    web/rest: add outgoing routing into brand API

--- library/Ivoz/Provider/Domain/Model/OutgoingRouting/OutgoingRoutingDto.php
+++ library/Ivoz/Provider/Domain/Model/OutgoingRouting/OutgoingRoutingDto.php
@@ -4,5 +4,20 @@ namespace Ivoz\Provider\Domain\Model\OutgoingRouting;
 
 class OutgoingRoutingDto extends OutgoingRoutingDtoAbstract
 {
+    public static function getPropertyMap(string $context = '')
+    {
+        if ($context === self::CONTEXT_COLLECTION) {
+            return [
+                'id' => 'id',
+                'type' => 'type',
+                'priority' => 'priority',
+                'weight' => 'weight',
+                'routingMode' => 'routingMode',
+                'companyId' => 'company',
+                'routingTagId' => 'routingTag',
+            ];
+        }
 
+        return parent::getPropertyMap($context);
+    }
 }
