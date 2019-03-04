8d8f0b9c8e812b89d6a7f409387f44ed52e8b204
Kaian <kaian@irontec.com>
2019-03-04 19:24:13 +0100

    core: update property maps on new brand API entities

--- library/Ivoz/Provider/Domain/Model/DestinationRate/DestinationRateDto.php
+++ library/Ivoz/Provider/Domain/Model/DestinationRate/DestinationRateDto.php
@@ -4,5 +4,23 @@ namespace Ivoz\Provider\Domain\Model\DestinationRate;
 
 class DestinationRateDto extends DestinationRateDtoAbstract
 {
+    /**
+     * @inheritdoc
+     */
+    public static function getPropertyMap(string $context = '')
+    {
+        if ($context === self::CONTEXT_COLLECTION) {
+            return [
+                'cost' => 'cost',
+                'connectFee' => 'connectFee',
+                'rateIncrement' => 'rateIncrement',
+                'groupIntervalStart' => 'groupIntervalStart',
+                'id' => 'id',
+                'destinationRateGroupId' => 'destinationRateGroup',
+                'destinationId' => 'destination'
+            ];
+        }
 
+        return parent::getPropertyMap(...func_get_args());
+    }
 }
