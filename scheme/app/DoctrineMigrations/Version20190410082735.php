5f972e92985bcf290d29df243c70ea2b446de7fb
mmadariaga <mikel@irontec.com>
2019-04-10 10:46:50 +0200

    scheme: added direction field into CallCsvSchedulers

--- scheme/app/DoctrineMigrations/Version20190410082735.php
+++ scheme/app/DoctrineMigrations/Version20190410082735.php
@@ -19,6 +19,10 @@ class Version20190410082735 extends AbstractMigration
         $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
 
         $this->addSql('ALTER TABLE BillableCalls ADD direction VARCHAR(255) DEFAULT \'outbound\' COMMENT \'[enum:inbound|outbound]\'');
+        $this->addSql('ALTER TABLE CallCsvSchedulers ADD callDirection VARCHAR(255) DEFAULT \'outbound\' COMMENT \'[enum:inbound|outbound]\'');
+
+        // Do not migrate inbound call from the past
+        $this->addSql('UPDATE kam_trunks_cdrs set parsed = 1 WHERE parsed = 0 and direction = \'inbound\'');
     }
 
     /**
@@ -29,6 +33,7 @@ class Version20190410082735 extends AbstractMigration
         // this down() migration is auto-generated, please modify it to your needs
         $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
 
+        $this->addSql('ALTER TABLE CallCsvSchedulers DROP callDirection');
         $this->addSql('ALTER TABLE BillableCalls DROP direction');
     }
 }
