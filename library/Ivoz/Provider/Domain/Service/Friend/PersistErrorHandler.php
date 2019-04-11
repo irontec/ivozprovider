05a85cff3cb94a8e3685d1ad0040bb702b23e236
mmadariaga <mikel@irontec.com>
2019-04-11 13:23:46 +0200

    core: improved duplicated friend priority error handler

---
+++ library/Ivoz/Provider/Domain/Service/Friend/PersistErrorHandler.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace Ivoz\Provider\Domain\Service\Friend;
+
+use Doctrine\DBAL\Exception\UniqueConstraintViolationException;
+use Ivoz\Core\Domain\Service\PersistErrorHandlerInterface;
+use Doctrine\DBAL\Driver\PDOException;
+
+class PersistErrorHandler implements PersistErrorHandlerInterface
+{
+    /*
+     * Mysql error code list:
+     * https://dev.mysql.com/doc/refman/5.5/en/error-messages-server.html
+     */
+    const MYSQL_ERROR_DUPLICATE_ENTRY = 1062;
+    const UNIQUE_PRIORITY_CONSTRAINT_NAME = 'companyPrio';
+
+    public function __construct()
+    {
+    }
+
+    public function handle(\Exception $exception)
+    {
+        if (!$exception instanceof UniqueConstraintViolationException) {
+            return;
+        }
+
+        $pdoException = $exception->getPrevious();
+        if (!$pdoException instanceof PDOException) {
+            return;
+        }
+
+        $isDuplicatedEmailError =
+            $pdoException->getErrorCode() === self::MYSQL_ERROR_DUPLICATE_ENTRY
+            && strpos($exception->getMessage(), self::UNIQUE_PRIORITY_CONSTRAINT_NAME);
+
+        if ($isDuplicatedEmailError) {
+            throw new \DomainException('Duplicated priority', 2202, $exception);
+        }
+    }
+}
