04fff8ab6b885c9b157a19dec45fcd5ba66eb0f0
Kaian <kaian@irontec.com>
2019-04-30 16:32:23 +0200

    pkg: version bump to 2.11.0

--- doc/sphinx/conf.py
+++ doc/sphinx/conf.py
@@ -73,7 +73,7 @@ author = 'Irontec'
 # built documents.
 #
 # The short X.Y version.
-version = "2.10"
+version = "2.11"
 # The full version, including alpha/beta/rc tags.
 release = 'Artemis'
 
@@ -276,7 +276,7 @@ latex_elements = {
 # (source start file, target name, title,
 #  author, documentclass [howto, manual, or own class]).
 latex_documents = [
-    (master_doc, 'IvozProvider.tex', 'IvozProvider 2.10 Documentation',
+    (master_doc, 'IvozProvider.tex', 'IvozProvider 2.11 Documentation',
      'Irontec', 'manual'),
 ]
 
