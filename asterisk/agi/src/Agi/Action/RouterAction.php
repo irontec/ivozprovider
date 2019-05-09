b1dbf9db185597c815062122bc95caa929543fa1
ccruz <carlos@irontec.com>
2019-05-09 16:58:28 +0200

    agis: fix route type friend destination

--- asterisk/agi/src/Agi/Action/RouterAction.php
+++ asterisk/agi/src/Agi/Action/RouterAction.php
@@ -465,7 +465,7 @@ class RouterAction
 
         $this->friendCallAction
             ->setFriend($friend)
-            ->setDestination($this->routeFriend)
+            ->setDestination($this->routeFriendDestination)
             ->process();
     }
 
