321a7117081d859a7cbebbb304cffaf6d2e5435d
mmadariaga <mikel@irontec.com>
2019-05-15 13:07:10 +0200

    core: abstract dto genertor, add rol argument to getPropertyMap

--- library/Ivoz/Core/Application/DataTransferObjectInterface.php
+++ library/Ivoz/Core/Application/DataTransferObjectInterface.php
@@ -22,17 +22,17 @@ interface DataTransferObjectInterface
     /**
      * @return array
      */
-    public function normalize(string $context);
+    public function normalize(string $context, string $role = '');
 
     /**
      * @return void
      */
-    public function denormalize(array $data, string $context);
+    public function denormalize(array $data, string $context, string $role = '');
 
     /**
      * @return array
      */
-    public static function getPropertyMap(string $context = '');
+    public static function getPropertyMap(string $context = '', string $role = null);
 
     /**
      * @return array
