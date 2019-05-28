510c4c56569e78ae175e4a62d5438f58cb3c3d32
mmadariaga <mikel@irontec.com>
2019-05-28 11:24:47 +0200

    tests: added brand API access control tests

---
+++ web/rest/brand/tests/DataAccessControl/Provider/BalanceNotificationTest.php
@@ -0,0 +1,114 @@
+<?php
+
+namespace Tests\DataAccessControl\Provider;
+
+use Ivoz\Api\Core\Security\DataAccessControlParser;
+use Ivoz\Provider\Domain\Model\BalanceNotification\BalanceNotification;
+use Symfony\Bundle\FrameworkBundle\Test\KernelTestCase;
+
+class BalanceNotificationTest extends KernelTestCase
+{
+    use \Ivoz\Tests\AccessControlTestHelperTrait;
+
+    protected function getResourceClass()
+    {
+        return BalanceNotification::class;
+    }
+
+    protected function getAdminCriteria(): array
+    {
+        return ['username' => 'test_brand_admin'];
+    }
+
+    /**
+     * @test
+     */
+    function it_has_read_access_control()
+    {
+        $accessControl = $this
+            ->dataAccessControlParser
+            ->get(
+                DataAccessControlParser::READ_ACCESS_CONTROL_ATTRIBUTE
+            );
+
+        $this->assertEquals(
+            $accessControl,
+            [
+                [
+                    'or' => [
+                        [
+                            'company',
+                            'in',
+                            'companyRepository.getSupervisedCompanyIdsByAdmin(user)'
+                        ],
+                        [
+                            'carrier',
+                            'in',
+                            'CarrierRepository([["brand","eq","user.getBrand().getId()"]])'
+                        ]
+                    ]
+                ]
+            ]
+        );
+    }
+
+    /**
+     * @test
+     */
+    function it_has_write_access_control()
+    {
+        $accessControl = $this
+            ->dataAccessControlParser
+            ->get(
+                DataAccessControlParser::WRITE_ACCESS_CONTROL_ATTRIBUTE
+            );
+
+        $this->assertEquals(
+            $accessControl,
+            [
+                [
+                    'or' => [
+                        [
+                            'company',
+                            'in',
+                            'companyRepository.getSupervisedCompanyIdsByAdmin(user)'
+                        ],
+                        [
+                            'company',
+                            'isNull',
+                            null
+                        ]
+                    ]
+                ],
+                [
+                    'or' => [
+                        [
+                            'notificationTemplate',
+                            'in',
+                            'NotificationTemplateRepository([["brand","eq","user.getBrand().getId()"]])'
+                        ],
+                        [
+                            'notificationTemplate',
+                            'isNull',
+                            null
+                        ]
+                    ]
+                ],
+                [
+                    'or' => [
+                        [
+                            'carrier',
+                            'in',
+                            'CarrierRepository([["brand","eq","user.getBrand().getId()"]])'
+                        ],
+                        [
+                            'carrier',
+                            'isNull',
+                            null
+                        ]
+                    ]
+                ]
+            ]
+        );
+    }
+}
