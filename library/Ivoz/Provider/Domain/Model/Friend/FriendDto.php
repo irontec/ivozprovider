9df02289cf1764fb0c1e5e98ca36d8db90a9f2ac
mmadariaga <mikel@irontec.com>
2019-06-10 16:37:34 +0200

    web/rest: filtered friends by API

--- library/Ivoz/Provider/Domain/Model/Friend/FriendDto.php
+++ library/Ivoz/Provider/Domain/Model/Friend/FriendDto.php
@@ -9,7 +9,7 @@ class FriendDto extends FriendDtoAbstract
      * @inheritdoc
      * @codeCoverageIgnore
      */
-    public static function getPropertyMap(string $context = '', string $rol = null)
+    public static function getPropertyMap(string $context = '', string $role = null)
     {
         if ($context === self::CONTEXT_COLLECTION) {
             return [
@@ -18,6 +18,87 @@ class FriendDto extends FriendDtoAbstract
             ];
         }
 
-        return parent::getPropertyMap(...func_get_args());
+        $response = parent::getPropertyMap(...func_get_args());
+
+        if ($role === 'ROLE_BRAND_ADMIN') {
+            return self::filterFieldsForBrandAdmin($response);
+        }
+
+        if ($role === 'ROLE_COMPANY_ADMIN') {
+            return self::filterFieldsForCompanyAdmin($response);
+        }
+
+        return $response;
+    }
+
+    private static function filterFieldsForBrandAdmin(array $response): array
+    {
+        $allowedFields = [
+            'name',
+            'description',
+            'transport',
+            'ip',
+            'port',
+            'authNeeded',
+            'password',
+            'priority',
+            'allow',
+            'fromDomain',
+            'directConnectivity',
+            'ddiIn',
+            't38Passthrough',
+            'id',
+            'companyId',
+            'transformationRuleSetId',
+            'callAclId',
+            'outgoingDdiId',
+            'languageId',
+            'interCompanyId'
+        ];
+
+        $response = array_filter(
+            $response,
+            function ($key) use ($allowedFields) {
+                return in_array($key, $allowedFields, true);
+            },
+            ARRAY_FILTER_USE_KEY
+        );
+
+        return $response;
+    }
+
+    private static function filterFieldsForCompanyAdmin(array $response): array
+    {
+        $allowedFields = [
+            'name',
+            'description',
+            'transport',
+            'ip',
+            'port',
+            'authNeeded',
+            'password',
+            'priority',
+            'allow',
+            'fromDomain',
+            'directConnectivity',
+            'ddiIn',
+            't38Passthrough',
+            'id',
+            'companyId',
+            'transformationRuleSetId',
+            'callAclId',
+            'outgoingDdiId',
+            'languageId',
+        ];
+
+        $response = array_filter(
+            $response,
+            function ($key) use ($allowedFields) {
+                return in_array($key, $allowedFields, true);
+            },
+            ARRAY_FILTER_USE_KEY
+        );
+
+        return $response;
     }
 }
