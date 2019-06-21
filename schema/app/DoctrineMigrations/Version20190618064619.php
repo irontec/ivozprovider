e62a5be8c3ec5c333d6472eded2e4714c29bae62
ccruz <carlos@irontec.com>
2019-06-21 17:42:24 +0200

    schema: disable foreign key checks before migration

--- schema/app/DoctrineMigrations/Version20190618064619.php
+++ schema/app/DoctrineMigrations/Version20190618064619.php
@@ -18,8 +18,10 @@ class Version20190618064619 extends AbstractMigration
         // this up() migration is auto-generated, please modify it to your needs
         $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
 
+        $this->addSql('SET FOREIGN_KEY_CHECKS = 0');
         $this->addSql('ALTER TABLE QueueMembers CHANGE queueId queueId INT UNSIGNED NOT NULL, CHANGE userId userId INT UNSIGNED NOT NULL');
         $this->addSql('ALTER TABLE CallForwardSettings CHANGE targetType targetType VARCHAR(25) DEFAULT NULL COMMENT \'[enum:number|extension|voicemail]\'');
+        $this->addSql('SET FOREIGN_KEY_CHECKS = 1');
     }
 
     /**
