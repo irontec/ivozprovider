6d0e35b549a3d4c17ee666f8763b78f382a24663
mmadariaga <mikel@irontec.com>
2019-07-29 16:50:50 +0200

    tests: added some cdr model and service unit test

---
+++ library/spec/Ivoz/Cgr/Domain/Service/TpRatingProfile/UpdateByRatingProfileSpec.php
@@ -0,0 +1,278 @@
+<?php
+
+namespace spec\Ivoz\Cgr\Domain\Service\TpRatingProfile;
+
+use Doctrine\Common\Collections\ArrayCollection;
+use Ivoz\Cgr\Domain\Model\TpRatingProfile\TpRatingProfileDto;
+use Ivoz\Cgr\Domain\Model\TpRatingProfile\TpRatingProfileInterface;
+use Ivoz\Cgr\Domain\Service\TpRatingProfile\UpdateByRatingProfile;
+use Ivoz\Core\Application\Service\EntityTools;
+use Ivoz\Provider\Domain\Model\Brand\BrandInterface;
+use Ivoz\Provider\Domain\Model\Carrier\CarrierInterface;
+use Ivoz\Provider\Domain\Model\Company\CompanyInterface;
+use Ivoz\Provider\Domain\Model\RatingPlanGroup\RatingPlanGroupInterface;
+use Ivoz\Provider\Domain\Model\RatingProfile\RatingProfileInterface;
+use Ivoz\Provider\Domain\Model\RoutingTag\RoutingTagInterface;
+use PhpSpec\ObjectBehavior;
+use Prophecy\Argument;
+use spec\HelperTrait;
+
+class UpdateByRatingProfileSpec extends ObjectBehavior
+{
+    use HelperTrait;
+
+    protected $entityTools;
+    protected $ratingProfile;
+    protected $tpRatingProfile;
+    protected $tpRatingProfileDto;
+    protected $company;
+    protected $carrier;
+    protected $brand;
+    protected $ratingPlanGroup;
+    protected $routingTag;
+
+    public function let()
+    {
+        $this->entityTools = $this->getTestDouble(
+            EntityTools::class
+        );
+
+        $this->beConstructedWith(
+            $this->entityTools
+        );
+    }
+
+    protected function prepareExecution()
+    {
+        $this->ratingProfile = $this->getTestDouble(
+            RatingProfileInterface::class,
+            false
+        );
+
+        $this->tpRatingProfile = $this->getTestDouble(
+            TpRatingProfileInterface::class,
+            false
+        );
+
+        $this->tpRatingProfileDto = $this->getTestDouble(
+            TpRatingProfileDto::class,
+            false
+        );
+
+        $this->company = $this->getTestDouble(
+            CompanyInterface::class,
+            false
+        );
+
+        $this->carrier = $this->getTestDouble(
+            CarrierInterface::class
+        );
+
+        $this->brand = $this->getTestDouble(
+            BrandInterface::class,
+            false
+        );
+
+        $this->ratingPlanGroup = $this->getTestDouble(
+            RatingPlanGroupInterface::class,
+            false
+        );
+
+        $this->routingTag = $this->getTestDouble(
+            RoutingTagInterface::class,
+            false
+        );
+
+        $activationTime = new \DateTime(
+            '2019-01-02 23:00:00',
+            new \DateTimeZone('UTC')
+        );
+
+        $this->getterProphecy(
+            $this->ratingProfile,
+            [
+                'getCgrRatingProfile' => $this->tpRatingProfile,
+                'getCompany' => $this->company,
+                'getCarrier' => $this->carrier,
+                'getRatingPlanGroup' => $this->ratingPlanGroup,
+                'getRoutingTag' => $this->routingTag,
+                'getActivationTime' => $activationTime,
+                'getId' => 1
+            ],
+            false
+        );
+
+        $this->setterProphecy(
+            $this->ratingProfile,
+            [
+                'replaceTpRatingProfiles' => function () {
+                    return [
+                        [Argument::type(ArrayCollection::class)],
+                        $this->ratingProfile
+                    ];
+                }
+            ],
+            false
+        );
+
+        $this->getterProphecy(
+            $this->company,
+            [
+                'getBrand' => $this->brand,
+                'getCgrSubject' => 'c1'
+            ],
+            false
+        );
+
+        $this->getterProphecy(
+            $this->carrier,
+            [
+                'getBrand' => $this->brand,
+                'getCgrSubject' => 'cr1'
+            ],
+            false
+        );
+
+        $this->getterProphecy(
+            $this->brand,
+            [
+                'getCgrTenant' => 'b1'
+            ],
+            false
+        );
+
+        $this->getterProphecy(
+            $this->ratingPlanGroup,
+            [
+                'getCgrTag' => 'b1rp1'
+            ],
+            false
+        );
+
+        $this->getterProphecy(
+            $this->routingTag,
+            [
+                'getCgrSubject' => 'rt1'
+            ],
+            false
+        );
+
+        $this->getterProphecy(
+            $this->tpRatingProfileDto,
+            [
+                'getSubject' => ''
+            ],
+            false
+        );
+
+        $this->fluentSetterProphecy(
+            $this->tpRatingProfileDto,
+            [
+                'setTpid' => Argument::any(),
+                'setActivationTime' => Argument::any(),
+                'setTenant' => Argument::any(),
+                'setRatingPlanTag' => Argument::any(),
+                'setRatingProfileId' => Argument::any(),
+                'setSubject' => Argument::any(),
+                'setCdrStatQueueIds' => Argument::any(),
+                'setSubject' => Argument::any()
+            ],
+            false
+        );
+
+        $this
+            ->entityTools
+            ->entityToDto(
+                $this->tpRatingProfile
+            )
+            ->willReturn(
+                $this->tpRatingProfileDto
+            );
+
+        $this
+            ->entityTools
+            ->persistDto(
+                $this->tpRatingProfileDto,
+                $this->tpRatingProfile,
+                true
+            )
+            ->willReturn(
+                $this->tpRatingProfile
+            );
+    }
+
+    function it_is_initializable()
+    {
+        $this->shouldHaveType(UpdateByRatingProfile::class);
+    }
+
+    function it_persists_tp_rating_profile()
+    {
+        $this->prepareExecution();
+
+        $this
+            ->entityTools
+            ->persistDto(
+                $this->tpRatingProfileDto,
+                $this->tpRatingProfile,
+                true
+            )
+            ->willReturn(
+                $this->tpRatingProfile
+            )
+            ->shouldBeCalled();
+
+        $this->execute($this->ratingProfile);
+    }
+
+    function it_updates_TpRatingProfile()
+    {
+        $this->prepareExecution();
+
+        $this
+            ->ratingProfile
+            ->replaceTpRatingProfiles(
+                new ArrayCollection([$this->tpRatingProfile])
+            )
+            ->shouldbeCalled();
+
+        $this
+            ->execute($this->ratingProfile);
+    }
+
+    function it_updates_rating_profile()
+    {
+        $this->prepareExecution();
+
+        $this
+            ->entityTools
+            ->persist($this->ratingProfile)
+            ->shouldBeCalled();
+
+        $this
+            ->execute($this->ratingProfile);
+    }
+
+    function it_uses_carier_on_empty_company()
+    {
+        $this->prepareExecution();
+
+        $this->ratingProfile
+            ->getCompany()
+            ->willReturn(null)
+            ->shouldBeCalled();
+
+        $this->getterProphecy(
+            $this->carrier,
+            [
+                'getBrand' => $this->brand,
+                'getCgrSubject' => 'something',
+                'getCgrSubject' => 'something',
+            ],
+            true
+        );
+
+        $this
+            ->execute($this->ratingProfile);
+    }
+}
---
+++ library/spec/Ivoz/Cgr/Domain/Service/TpTiming/DeletedByRatingPlanSpec.php
@@ -0,0 +1,73 @@
+<?php
+
+namespace spec\Ivoz\Cgr\Domain\Service\TpTiming;
+
+use Ivoz\Cgr\Domain\Model\TpTiming\TpTimingInterface;
+use Ivoz\Cgr\Domain\Service\TpTiming\DeletedByRatingPlan;
+use Ivoz\Core\Application\Service\EntityTools;
+use Ivoz\Provider\Domain\Model\RatingPlan\RatingPlan;
+use Ivoz\Provider\Domain\Model\RatingPlan\RatingPlanInterface;
+use Ivoz\Provider\Domain\Model\RatingPlanGroup\RatingPlanGroupInterface;
+use PhpSpec\ObjectBehavior;
+use Prophecy\Argument;
+use spec\HelperTrait;
+
+class DeletedByRatingPlanSpec extends ObjectBehavior
+{
+    use HelperTrait;
+
+    protected $entityTools;
+    protected $ratingPlan;
+    protected $tpTiming;
+    protected $ratingPlanGroup;
+
+    public function let()
+    {
+        $this->entityTools = $this->getTestDouble(
+            EntityTools::class,
+            true
+        );
+
+        $this->beConstructedWith(
+            $this->entityTools
+        );
+    }
+
+    protected function prepareExecution()
+    {
+        $this->ratingPlan = $this->getTestDouble(
+            RatingPlanInterface::class,
+            false
+        );
+
+        $this->tpTiming = $this->getTestDouble(
+            TpTimingInterface::class,
+            false
+        );
+
+        $this->getterProphecy(
+            $this->ratingPlan,
+            [
+                'getTpTiming' => $this->tpTiming,
+                'getTimingType' => RatingPlan::TIMING_TYPE_ALWAYS
+            ]
+        );
+    }
+
+    function it_is_initializable()
+    {
+        $this->shouldHaveType(DeletedByRatingPlan::class);
+    }
+
+    function it_removes_tp_timins_on_always_timings()
+    {
+        $this->prepareExecution();
+
+        $this
+            ->entityTools
+            ->remove($this->tpTiming)
+            ->shouldBeCalled();
+
+        $this->execute($this->ratingPlan);
+    }
+}
