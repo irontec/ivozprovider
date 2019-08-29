68869c37d68658090db3079a6c99b9d93782d2dc
Kaian <kaian@irontec.com>
2019-08-29 13:54:16 +0200

    tests: update spec tests with new holiday checks

--- library/spec/Ivoz/Provider/Domain/Model/Calendar/CalendarSpec.php
+++ library/spec/Ivoz/Provider/Domain/Model/Calendar/CalendarSpec.php
@@ -2,6 +2,7 @@
 
 namespace spec\Ivoz\Provider\Domain\Model\Calendar;
 
+use Doctrine\Common\Collections\Criteria;
 use Ivoz\Provider\Domain\Model\Calendar\Calendar;
 use Ivoz\Provider\Domain\Model\Calendar\CalendarDto;
 use Ivoz\Provider\Domain\Model\HolidayDate\HolidayDateInterface;
@@ -41,10 +42,6 @@ class CalendarSpec extends ObjectBehavior
             ->setCalendar(Argument::type(Calendar::class))
             ->willReturn();
 
-        $this
-            ->holidayDate
-            ->checkEventOnTime(Argument::any())
-            ->willReturn(true);
 
         $this->holidayDate2 = $holidayDate2;
         $this->holidayDate2
@@ -54,11 +51,6 @@ class CalendarSpec extends ObjectBehavior
         $this->holidayDate2
             ->setCalendar(Argument::type(Calendar::class))
             ->willReturn();
-
-        $this
-            ->holidayDate2
-            ->checkEventOnTime(Argument::any())
-            ->willReturn(true);
 
         $holidayDates = [
             $holidayDate->getWrappedObject(),
@@ -86,21 +78,21 @@ class CalendarSpec extends ObjectBehavior
 
     function it_tells_whether_given_day_is_a_holiday()
     {
-        $this->getterProphecy(
-            $this->holidayDate,
-            [
-                'getEventDate' => (new \DateTime('2019-01-01'))
-            ],
-            true
+
+        $holidayDates = $this->getTestDouble(
+            ArrayCollection::class
         );
 
-        $this->getterProphecy(
-            $this->holidayDate2,
-            [
-                'getEventDate' => (new \DateTime('2019-01-02'))
-            ],
-            true
+        $this->hydrate(
+            $this->getWrappedObject(),
+            ['holidayDates' => $holidayDates->reveal()]
         );
+
+        $holidayDates
+            ->matching(
+                Argument::type(\Doctrine\Common\Collections\Criteria::class)
+            )
+            ->willReturn(new ArrayCollection([true]));
 
         $this
             ->isHolidayDate(new \DateTime('2019-01-01'))
@@ -110,50 +102,15 @@ class CalendarSpec extends ObjectBehavior
             ->isHolidayDate(new \DateTime('2019-01-02'))
             ->shouldReturn(true);
 
+
+        $holidayDates
+            ->matching(
+                Argument::type(\Doctrine\Common\Collections\Criteria::class)
+            )
+            ->willReturn(new ArrayCollection([]));
+
         $this
             ->isHolidayDate(new \DateTime('2019-01-03'))
             ->shouldReturn(false);
-    }
-
-    function it_makes_timezone_conversions_on_isHolidayDate()
-    {
-        // UTC#2018-12-31 23:00:00
-        $eventDate = new \DateTime(
-            '2019-01-01 00:00:00',
-            new \DateTimeZone('Europe/Madrid')
-        );
-        $this->getterProphecy(
-            $this->holidayDate,
-            [
-                'getEventDate' => $eventDate
-            ],
-            true
-        );
-
-        // UTC#2019-02-01 15:00:00
-        $eventDate2 = new \DateTime(
-            '2019-02-02 00:00:00',
-            new \DateTimeZone('Asia/Tokyo')
-        );
-        $this->getterProphecy(
-            $this->holidayDate2,
-            [
-                'getEventDate' => $eventDate2
-            ],
-            true
-        );
-
-        $utc = new \DateTimeZone('UTC');
-        $this
-            ->isHolidayDate(new \DateTime('2018-12-31 23:00:00', $utc))
-            ->shouldReturn(true);
-
-        $this
-            ->isHolidayDate(new \DateTime('2019-01-01 23:00:00', $utc))
-            ->shouldReturn(false);
-
-        $this
-            ->isHolidayDate(new \DateTime('2019-02-01 20:00:00', $utc))
-            ->shouldReturn(true);
     }
 }
