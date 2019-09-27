126c50e417ed07e10d791fa9eef4575e4c63f738
mmadariaga <mikel@irontec.com>
2019-09-27 13:00:28 +0200

    core: split Invoice\CreateByScheduler

---
+++ library/Ivoz/Provider/Domain/Service/InvoiceScheduler/UpdateLastExecutionDate.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace Ivoz\Provider\Domain\Service\InvoiceScheduler;
+
+use Ivoz\Core\Application\Service\EntityTools;
+use Ivoz\Provider\Domain\Model\InvoiceScheduler\InvoiceSchedulerDto;
+use Ivoz\Provider\Domain\Model\InvoiceScheduler\InvoiceSchedulerInterface;
+
+class UpdateLastExecutionDate
+{
+    private $entityTools;
+
+    public function __construct(
+        EntityTools $entityTools
+    ) {
+        $this->entityTools = $entityTools;
+    }
+
+    /**
+     * @param InvoiceSchedulerInterface $scheduler
+     * @return void
+     */
+    public function execute(
+        InvoiceSchedulerInterface $scheduler,
+        $dispatchImmediately = true
+    ) {
+        /** @var InvoiceSchedulerDto $invoiceSchedulerDto */
+        $invoiceSchedulerDto = $this
+            ->entityTools
+            ->entityToDto($scheduler);
+
+        $invoiceSchedulerDto
+            ->setLastExecution(
+                new \DateTime(
+                    null,
+                    new \DateTimeZone('UTC')
+                )
+            )
+            ->setLastExecutionError('');
+
+        $this->entityTools->persistDto(
+            $invoiceSchedulerDto,
+            $scheduler,
+            $dispatchImmediately
+        );
+    }
+}
