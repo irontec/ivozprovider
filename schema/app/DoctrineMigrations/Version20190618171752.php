cb3a91e323064e2771401c67ba1f57281847f2be
Kaian <kaian@irontec.com>
2019-10-31 13:09:30 +0100

    scheme: delete duplicates before trunks cdr unique callid index

--- schema/app/DoctrineMigrations/Version20190618171752.php
+++ schema/app/DoctrineMigrations/Version20190618171752.php
@@ -18,6 +18,7 @@ class Version20190618171752 extends AbstractMigration
         // this up() migration is auto-generated, please modify it to your needs
         $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
 
+        $this->addSql('DELETE t1 FROM kam_trunks_cdrs t1 INNER JOIN kam_trunks_cdrs t2 WHERE t1.id > t2.id AND t1.callid=t2.callid AND t1.direction=t2.direction');
         $this->addSql('CREATE UNIQUE INDEX trunksCdr_callid_direction ON kam_trunks_cdrs (callid, direction)');
     }
 
