952e14dc6108744200c057ce50eeca8802cd56d7
mmadariaga <mikel@irontec.com>
2019-11-27 16:56:00 +0100

    web/rest: do not join tables on zero depth responses (collections usually)

--- library/Ivoz/Api/Doctrine/Orm/Extension/UnpaginatedResultGeneratorExtension.php
+++ library/Ivoz/Api/Doctrine/Orm/Extension/UnpaginatedResultGeneratorExtension.php
@@ -2,25 +2,18 @@
 
 namespace Ivoz\Api\Doctrine\Orm\Extension;
 
-use ApiPlatform\Core\Bridge\Doctrine\Orm\AbstractPaginator;
 use ApiPlatform\Core\Bridge\Doctrine\Orm\Extension\ContextAwareQueryResultCollectionExtensionInterface;
-use ApiPlatform\Core\Bridge\Doctrine\Orm\Paginator;
-use ApiPlatform\Core\Bridge\Doctrine\Orm\Util\QueryChecker;
 use ApiPlatform\Core\Bridge\Doctrine\Orm\Util\QueryNameGeneratorInterface;
-use ApiPlatform\Core\Exception\InvalidArgumentException;
 use ApiPlatform\Core\Metadata\Resource\Factory\ResourceMetadataFactoryInterface;
 use ApiPlatform\Core\Metadata\Resource\ResourceMetadata;
-use Doctrine\Common\Persistence\ManagerRegistry;
 use Doctrine\ORM\EntityManager;
 use Doctrine\ORM\QueryBuilder;
-use Doctrine\ORM\Tools\Pagination\Paginator as DoctrineOrmPaginator;
-use Psr\Log\LoggerInterface;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\RequestStack;
 
 final class UnpaginatedResultGeneratorExtension implements ContextAwareQueryResultCollectionExtensionInterface
 {
-    const BATCH_SIZE = 1000;
+    const BATCH_SIZE = 4000;
 
     private $requestStack;
     private $resourceMetadataFactory;
@@ -114,6 +107,7 @@ final class UnpaginatedResultGeneratorExtension implements ContextAwareQueryResu
 
             $query = $queryBuilder->getQuery();
             $results = $query->getResult();
+
             $continue = count($results) === self::BATCH_SIZE;
             $currentPage++;
 
