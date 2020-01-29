09973de0d904017fe40f7554020e49c6f3057c50
mmadariaga <mikel@irontec.com>
2020-01-29 17:15:52 +0100

    web/admin: updated year on the footer

--- web/admin/application/library/IvozProvider/Klear/Dynamic/Config/Base.php
+++ web/admin/application/library/IvozProvider/Klear/Dynamic/Config/Base.php
@@ -28,7 +28,7 @@ abstract class Base extends \Klear_Model_Settings_Dynamic_Abstract
 
     protected $_title = '';
     protected $_subTitle = '';
-    protected $_year = '2019';
+    protected $_year = '2020';
     protected $_logo = "images/palmera90.png";
 
     protected $_sessionName = 'BrandOperatorSession';
