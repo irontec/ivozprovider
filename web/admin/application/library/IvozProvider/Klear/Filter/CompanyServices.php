irontec/ivozprovider

commit d877740cac34944518c24305bcc4d0f51aa533e9

web/admin: update Company available services listbox

web/admin/application/library/IvozProvider/Klear/Filter/CompanyServices.php
@@ -4,6 +4,7 @@ use Ivoz\Provider\Domain\Model\BrandService\BrandService;
 use Ivoz\Provider\Domain\Model\BrandService\BrandServiceDto;
 use Ivoz\Provider\Domain\Model\CompanyService\CompanyService;
 use Ivoz\Provider\Domain\Model\CompanyService\CompanyServiceDto;
+use Ivoz\Provider\Domain\Model\Service\Service;
 
 /**
  * Class IvozProvider_Klear_Filter_CompanyServices
@@ -49,7 +50,10 @@ class IvozProvider_Klear_Filter_CompanyServices implements KlearMatrix_Model_Fie
 
         $brandServicesIds = array();
         foreach ($brandServices as $brandService) {
-            array_push($brandServicesIds, $brandService->getServiceId());
+            $serviceIden = $brandService->getService()->getIden();
+            if (in_array($serviceIden, Service::VPBX_AVAILABLE_SERVICES, true)) {
+                array_push($brandServicesIds, $brandService->getServiceId());
+            }
         }
 
         if (count($brandServicesIds)) {

commit c3a7ac919fd8108a670c689d9ae91bea08e4bf8f

web/admin: fix vpbx service selector filter
Broken since d877740

web/admin/application/library/IvozProvider/Klear/Filter/CompanyServices.php
@@ -5,6 +5,7 @@ use Ivoz\Provider\Domain\Model\BrandService\BrandServiceDto;
 use Ivoz\Provider\Domain\Model\CompanyService\CompanyService;
 use Ivoz\Provider\Domain\Model\CompanyService\CompanyServiceDto;
 use Ivoz\Provider\Domain\Model\Service\Service;
+use Ivoz\Provider\Domain\Model\Service\ServiceDto;
 
 /**
  * Class IvozProvider_Klear_Filter_CompanyServices
@@ -50,7 +51,14 @@ class IvozProvider_Klear_Filter_CompanyServices implements KlearMatrix_Model_Fie
 
         $brandServicesIds = array();
         foreach ($brandServices as $brandService) {
-            $serviceIden = $brandService->getService()->getIden();
+
+            /** @var ServiceDto $service */
+            $service = $dataGateway->findOneBy(
+                Service::class,
+                ["Service.id = " . $brandService->getService()->getId()]
+            );
+            $serviceIden = $service->getIden();
+
             if (in_array($serviceIden, Service::VPBX_AVAILABLE_SERVICES, true)) {
                 array_push($brandServicesIds, $brandService->getServiceId());
             }