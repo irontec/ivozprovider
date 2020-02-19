85e73a327956e8c31ae88348f1cd5537a7a5c1bd
ccruz <carlos@irontec.com>
2020-02-19 17:52:39 +0100

    agis: t38passthrough retail accounts need no routing through AS anymore

--- asterisk/agi/src/Agi/Action/DdiAction.php
+++ asterisk/agi/src/Agi/Action/DdiAction.php
@@ -153,7 +153,6 @@ class DdiAction
             ->setRouteFriendDestination($ddi->getFriendValue())
             ->setRouteQueue($ddi->getQueue())
             ->setRouteResidential($ddi->getResidentialDevice())
-            ->setRouteRetail($ddi->getRetailAccount())
             ->setRouteConditional($ddi->getConditionalRoute())
             ->route();
     }
