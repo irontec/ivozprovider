359607c01fb2cfb02b6dfcd75d9f7b34003c3d9c
Mikel Madariaga <mikel@irontec.com>
2020-02-20 13:16:45 +0100

    schema: fixed merge issues

--- schema/DoctrineMigrations/Version20200210145817.php
+++ schema/DoctrineMigrations/Version20200210145817.php
@@ -39,6 +39,12 @@ class Version20200210145817 extends LoggableMigration
         $this->addSql(
             'UPDATE `PublicEntities` SET name_it = name_en, name_ca = name_es'
         );
+
+        $this->addSql(
+            'INSERT INTO AdministratorRelPublicEntities (administratorId, publicEntityId, `create`, `read`, `update`, `delete`) '
+            . 'SELECT A.id, P.id, 1, 1, 1, 1 FROM Administrators A INNER JOIN PublicEntities P '
+            . 'WHERE A.brandId IS NOT NULL AND A.companyId IS NULL AND P.brand = 1'
+        );
     }
 
     /**
@@ -56,12 +62,6 @@ class Version20200210145817 extends LoggableMigration
 
         $this->addSql(
             'UPDATE `PublicEntities` SET `brand` = 0'
-        );
-
-        $this->addSql(
-            'INSERT INTO AdministratorRelPublicEntities (administratorId, publicEntityId, `create`, `read`, `update`, `delete`) '
-            . 'SELECT A.id, P.id, 1, 1, 1, 1 FROM Administrators A INNER JOIN PublicEntities P '
-            . 'WHERE A.brandId IS NOT NULL AND A.companyId IS NULL AND P.brand = 1'
         );
     }
 
