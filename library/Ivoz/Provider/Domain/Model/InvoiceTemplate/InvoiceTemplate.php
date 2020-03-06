450ec6c2f09f3156213faeadb34a30d73448d225
mmadariaga <mikel@irontec.com>
2020-03-06 12:47:55 +0100

    core: ensure InvoiceTemplate does not switch type global/brand

--- library/Ivoz/Provider/Domain/Model/InvoiceTemplate/InvoiceTemplate.php
+++ library/Ivoz/Provider/Domain/Model/InvoiceTemplate/InvoiceTemplate.php
@@ -27,6 +27,20 @@ class InvoiceTemplate extends InvoiceTemplateAbstract implements InvoiceTemplate
         return $this->id;
     }
 
+    protected function sanitizeValues()
+    {
+        $notNew = !$this->isNew();
+        $brandHasChanged = $this->hasChanged('brandId');
+
+        if ($notNew && $brandHasChanged) {
+            $errorMsg = $this->getBrand()
+                ? 'Unable to convert a global invoice template into a brand invoice template'
+                : 'Unable to convert a brand invoice template into a global invoice template';
+
+            throw new \DomainException($errorMsg);
+        }
+    }
+
     /**
      * @inheritdoc
      */
