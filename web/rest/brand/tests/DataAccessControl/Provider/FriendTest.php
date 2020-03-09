8e69bc7a3de788e4c26487883d63fe0dff19d777
Mikel Madariaga <mikel@irontec.com>
2020-03-09 12:16:43 +0100

    web/rest: add friend and fax collections

---
+++ web/rest/brand/tests/DataAccessControl/Provider/FriendTest.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace Tests\DataAccessControl\Provider;
+
+use Ivoz\Api\Core\Security\DataAccessControlParser;
+use Ivoz\Provider\Domain\Model\Friend\Friend;
+use Symfony\Bundle\FrameworkBundle\Test\KernelTestCase;
+
+class FriendTest extends KernelTestCase
+{
+    use \Ivoz\Tests\AccessControlTestHelperTrait;
+
+    protected function getResourceClass()
+    {
+        return Friend::class;
+    }
+
+    protected function getAdminCriteria(): array
+    {
+        return ['username' => 'test_brand_admin'];
+    }
+
+    /**
+     * @test
+     */
+    function it_has_read_access_control()
+    {
+        $accessControl = $this
+            ->dataAccessControlParser
+            ->get(
+                DataAccessControlParser::READ_ACCESS_CONTROL_ATTRIBUTE
+            );
+
+        $this->assertEquals(
+            $accessControl,
+            [
+                [
+                    'company',
+                    'in',
+                    'companyRepository.getSupervisedCompanyIdsByAdmin(user)'
+                ]
+            ]
+        );
+    }
+
+    /**
+     * @test
+     */
+    function it_has_write_access_control()
+    {
+        $accessControl = $this
+            ->dataAccessControlParser
+            ->get(
+                DataAccessControlParser::WRITE_ACCESS_CONTROL_ATTRIBUTE
+            );
+
+        $this->assertEquals(
+            $accessControl,
+            [
+                'FALSE',
+            ]
+        );
+    }
+}
