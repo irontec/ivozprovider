d887c514f8f4930619fbbca7acd4122ab23b34f8
Mikel Madariaga <mikel@irontec.com>
2020-04-01 12:14:41 +0200

    tests: updated orm tests

--- schema/tests/Provider/Carrier/CarrierRepositoryTest.php
+++ schema/tests/Provider/Carrier/CarrierRepositoryTest.php
@@ -4,7 +4,11 @@ namespace Tests\Provider\Carrier;
 
 use Ivoz\Provider\Domain\Model\Administrator\Administrator;
 use Ivoz\Provider\Domain\Model\Administrator\AdministratorRepository;
+use Ivoz\Provider\Domain\Model\Brand\Brand;
+use Ivoz\Provider\Domain\Model\Brand\BrandRepository;
 use Ivoz\Provider\Domain\Model\Carrier\Carrier;
+use Ivoz\Provider\Domain\Model\ProxyTrunk\ProxyTrunk;
+use Ivoz\Provider\Domain\Model\ProxyTrunk\ProxyTrunkRepository;
 use Ivoz\Provider\Infrastructure\Persistence\Doctrine\CarrierDoctrineRepository;
 use Symfony\Bundle\FrameworkBundle\Test\KernelTestCase;
 use Tests\DbIntegrationTestHelperTrait;
@@ -19,6 +23,9 @@ class CarrierRepositoryTest extends KernelTestCase
     public function test_runner()
     {
         $this->it_finds_carrier_ids_group_by_brand();
+        $this->it_finds_ids_by_brand_admin();
+        $this->it_finds_ids_by_brand_and_proxyTrunk();
+        $this->it_finds_ids_by_proxyTrunk();
     }
 
     public function it_finds_carrier_ids_group_by_brand()
@@ -51,6 +58,60 @@ class CarrierRepositoryTest extends KernelTestCase
 
         $result = $repository->getCarrierIdsByBrandAdmin(
             $brandAdmin
+        );
+
+        $this->assertNotEmpty(
+            $result
+        );
+    }
+
+    public function it_finds_ids_by_brand_and_proxyTrunk()
+    {
+        /** @var BrandRepository $brandRepository */
+        $brandRepository = $this
+            ->em
+            ->getRepository(Brand::class);
+
+        $brand = $brandRepository->find(1);
+
+        /** @var ProxyTrunkRepository $repository */
+        $proxyTrunkRepository = $this
+            ->em
+            ->getRepository(ProxyTrunk::class);
+
+        $proxyTrunk = $proxyTrunkRepository->find(1);
+
+        /** @var ProxyTrunkRepository $repository */
+        $repository = $this
+            ->em
+            ->getRepository(Carrier::class);
+
+        $result = $repository->findByBrandAndProxyTrunks(
+            $brand,
+            $proxyTrunk
+        );
+
+        $this->assertNotEmpty(
+            $result
+        );
+    }
+
+    public function it_finds_ids_by_proxyTrunk()
+    {
+        /** @var ProxyTrunkRepository $repository */
+        $proxyTrunkRepository = $this
+            ->em
+            ->getRepository(ProxyTrunk::class);
+
+        $proxyTrunk = $proxyTrunkRepository->find(1);
+
+        /** @var ProxyTrunkRepository $repository */
+        $repository = $this
+            ->em
+            ->getRepository(Carrier::class);
+
+        $result = $repository->findByProxyTrunks(
+            $proxyTrunk
         );
 
         $this->assertNotEmpty(
