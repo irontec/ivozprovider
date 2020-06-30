9cccec9b7fa0b140594f0f35d1d595483f2b2805
Mikel Madariaga <mikel@irontec.com>
2020-06-30 10:23:11 +0200

    core: removed deprecated constants

--- web/admin/application/library/IvozProvider/Klear/Ghost/Companies.php
+++ web/admin/application/library/IvozProvider/Klear/Ghost/Companies.php
@@ -3,6 +3,7 @@
 use Ivoz\Core\Application\Service\DataGateway;
 use Ivoz\Core\Infrastructure\Domain\Service\Cgrates\AbstractBalanceService;
 use Ivoz\Provider\Domain\Model\Company\Company;
+use Ivoz\Provider\Domain\Model\Company\CompanyInterface;
 use Ivoz\Provider\Domain\Model\Company\CompanyDto;
 use Ivoz\Provider\Domain\Model\Ddi\Ddi;
 use Ivoz\Provider\Domain\Model\Ddi\DdiDto;
@@ -30,13 +31,13 @@ class IvozProvider_Klear_Ghost_Companies extends KlearMatrix_Model_Field_Ghost_A
     public function getTypeIcon($model)
     {
         switch ($model->getType()) {
-            case Company::VPBX:
+            case CompanyInterface::TYPE_VPBX:
                 return '<span class="ui-silk inline ui-silk-building" title="Company"></span>';
-            case Company::RETAIL:
+            case CompanyInterface::TYPE_RETAIL:
                 return '<span class="ui-silk inline ui-silk-basket" title="Retail"></span>';
-            case Company::WHOLESALE:
+            case CompanyInterface::TYPE_WHOLESALE:
                 return '<span class="ui-silk inline ui-silk-cart" title="Wholesale"></span>';
-            case Company::RESIDENTIAL:
+            case CompanyInterface::TYPE_RESIDENTIAL:
                 return '<span class="ui-silk inline ui-silk-house" title="Residential"></span>';
             default:
                 return $model->getType();
