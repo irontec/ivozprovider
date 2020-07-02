irontec/ivozprovider

commit d5d0b9023aab75986274dda4c895e38fa15db30c

core: removed duplicated code

library/Ivoz/Cgr/Infrastructure/Cgrates/Service/CarrierBalanceService.php
@@ -74,7 +74,8 @@ class CarrierBalanceService extends AbstractBalanceService implements CarrierBal
         );
 
         $brand = $entity->getBrand();
-        return self::TENANT_PREFIX . $brand->getId();
+
+        return $brand->getCgrTenant();
     }
 
     /**
@@ -88,6 +89,7 @@ class CarrierBalanceService extends AbstractBalanceService implements CarrierBal
             CarrierInterface::class
         );
 
-        return self::ACCOUNT_PREFIX . $entity->getId();
+        /** @var CarrierInterface $entity */
+        $entity->getCgrSubject();
     }
 }

library/Ivoz/Cgr/Infrastructure/Cgrates/Service/CompanyBalanceService.php
@@ -130,7 +130,8 @@ class CompanyBalanceService extends AbstractBalanceService implements CompanyBal
         );
 
         $brand = $entity->getBrand();
-        return self::TENANT_PREFIX . $brand->getId();
+
+        return $brand->getCgrTenant();
     }
 
     /**
@@ -144,6 +145,7 @@ class CompanyBalanceService extends AbstractBalanceService implements CompanyBal
             CompanyInterface::class
         );
 
-        return self::ACCOUNT_PREFIX . $entity->getId();
+        /** @var CompanyInterface $entity */
+        return $entity->getCgrSubject();
     }
 }

library/Ivoz/Cgr/Infrastructure/Cgrates/Service/FetchCallStatsService.php
@@ -28,7 +28,7 @@ class FetchCallStatsService implements FetchCallStatsServiceInterface
     public function execute(CarrierInterface $carrier)
     {
         $params = [
-            'StatsQueueId' => 'cr' . $carrier->getId()
+            'StatsQueueId' => $carrier->getCgrSubject()
         ];
         $response = $this->sendRequest(
             'CDRStatsV1.GetMetrics',

library/Ivoz/Cgr/Infrastructure/Cgrates/Service/ProcessExternalCdr.php
@@ -70,8 +70,8 @@ class ProcessExternalCdr
             'OriginID' => $callId,
             'ToR' => '*voice',
             'RequestType' => '*' . $company->getBillingMethod(),
-            'Tenant' => 'b' . $brand->getId(),
-            'Account' => 'c' . $company->getId(),
+            'Tenant' => $brand->getCgrTenant(),
+            'Account' => $company->getCgrSubject(),
             'Destination' => $trunksCdr->getCallee(),
             'SetupTime' => $time,
             'AnswerTime' => $time,
@@ -84,7 +84,7 @@ class ProcessExternalCdr
                 : '0';
 
             $payload['ExtraFields'] = [
-                'carrierId' => 'cr' . $carrier->getId(),
+                'carrierId' => $carrier->getCgrSubject(),
                 'calculateCost' => $calculateCost
             ];
         }

commit 813e600e431949ccb26a35e479b86700bb6ad17a

core: add missing return in getAccount
Fixes regression introduced in d5d0b9023

library/Ivoz/Cgr/Infrastructure/Cgrates/Service/CarrierBalanceService.php
@@ -90,6 +90,6 @@ class CarrierBalanceService extends AbstractBalanceService implements CarrierBal
         );
 
         /** @var CarrierInterface $entity */
-        $entity->getCgrSubject();
+        return $entity->getCgrSubject();
     }
 }