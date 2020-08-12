7490fe9d4e9076535a850f640105801b9d1acd43
Carlos Cruz <carlos@cruzccl.com>
2020-08-12 13:10:09 +0200

    schema: fix doctrine migration

--- schema/DoctrineMigrations/Version20200810142815.php
+++ schema/DoctrineMigrations/Version20200810142815.php
@@ -19,7 +19,9 @@ class Version20200810142815 extends LoggableMigration
         $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
 
         $this->addSql('DELETE FROM kam_trusted WHERE companyId IS NULL');
+        $this->addSql('ALTER TABLE kam_trusted DROP FOREIGN KEY FK_10A58A572480E723');
         $this->addSql('ALTER TABLE kam_trusted CHANGE companyId companyId INT UNSIGNED NOT NULL');
+        $this->addSql('ALTER TABLE kam_trusted ADD CONSTRAINT FK_10A58A572480E723 FOREIGN KEY (companyId) REFERENCES Companies (id) ON DELETE CASCADE');
     }
 
     /**
@@ -30,6 +32,8 @@ class Version20200810142815 extends LoggableMigration
         // this down() migration is auto-generated, please modify it to your needs
         $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
 
+        $this->addSql('ALTER TABLE kam_trusted DROP FOREIGN KEY FK_10A58A572480E723');
         $this->addSql('ALTER TABLE kam_trusted CHANGE companyId companyId INT UNSIGNED DEFAULT NULL');
+        $this->addSql('ALTER TABLE kam_trusted ADD CONSTRAINT FK_10A58A572480E723 FOREIGN KEY (companyId) REFERENCES Companies (id) ON DELETE CASCADE');
     }
 }
