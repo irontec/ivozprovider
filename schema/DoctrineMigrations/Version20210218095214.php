4a0aff298d0e1dd8f3f068eee1ee008a607a360b
Mikel Madariaga <mikel@irontec.com>
2021-02-18 16:28:55 +0100

    schema: changed sql_mode on the latest migration to avoid errors

--- schema/DoctrineMigrations/Version20210218095214.php
+++ schema/DoctrineMigrations/Version20210218095214.php
@@ -18,7 +18,9 @@ class Version20210218095214 extends LoggableMigration
         // this up() migration is auto-generated, please modify it to your needs
         $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
 
+        $this->addSql('set sql_mode = \'STRICT_TRANS_TABLES\'');
         $this->addSql('ALTER TABLE Companies CHANGE countryId countryId INT UNSIGNED NOT NULL');
+
         $this->addSql('ALTER TABLE DDIs CHANGE countryId countryId INT UNSIGNED DEFAULT NULL');
     }
 
