18563cfee1bd1a7810af4e5022ddaf7c016e92d2
Kaian <kaian@irontec.com>
2021-02-23 14:40:42 +0100

    iso: update iso templates for halliday

--- extra/simple-cdd/generate-iso.sh
+++ extra/simple-cdd/generate-iso.sh
@@ -1,23 +1,18 @@
 #!/bin/bash
-ARCH=$1
-echo Building ISO Image for $ARCH
+echo Building ISO Image for amd64
 
 # Add IvozProvider repositories
-echo deb http://packages.irontec.com/debian artemis main extra | sudo tee -a /etc/apt/sources.list
+echo deb http://packages.irontec.com/debian halliday main extra | sudo tee -a /etc/apt/sources.list
 echo deb http://packages.irontec.com/debian tayler main extra  | sudo tee -a /etc/apt/sources.list
 wget http://packages.irontec.com/public.key -q -O - | sudo apt-key add -
 
 # Update apt-cache
-sudo dpkg --add-architecture $ARCH
 sudo apt-get update
-
-# Set required architecture for binary packages
-sed -i "s/#ARCH#/$ARCH/g" package.list
 
 # Download ivozprovider packages for given rlease
 pushd local
-cat ../package.list | xargs apt-get download -t artemis
+cat ../package.list | xargs apt-get download -t halliday
 popd
 
 # Generate iso file
-ARCH=$ARCH build-simple-cdd --force-root --conf simple-cdd.conf --verbose --force-preseed --dist stretch
+build-simple-cdd --force-root --conf simple-cdd.conf --verbose --force-preseed --dist buster
