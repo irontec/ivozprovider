644dfb08cbe0380cf13d1fc4002c6a63c4413f4f
Mikel <mikel@irontec.com>
2021-04-16 13:36:17 +0200

    microservices: remove gearman from Worker/Rates

---
+++ library/Ivoz/Provider/Infrastructure/Redis/Job/RatesImporter.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace Ivoz\Provider\Infrastructure\Redis\Job;
+
+use Ivoz\Core\Infrastructure\Persistence\Redis\RedisMasterFactory;
+use Ivoz\Provider\Domain\Job\RatesImporterJobInterface;
+use Psr\Log\LoggerInterface;
+
+class RatesImporter implements RatesImporterJobInterface
+{
+    private $redisMasterFactory;
+    private $redisDb;
+    private $logger;
+
+    private $params = [];
+
+    public function __construct(
+        RedisMasterFactory $redisMasterFactory,
+        int $redisDb,
+        LoggerInterface $logger
+    ) {
+        $this->redisMasterFactory = $redisMasterFactory;
+        $this->redisDb = $redisDb;
+        $this->logger = $logger;
+    }
+
+    public function setParams(array $params): self
+    {
+        $this->params = $params;
+        return $this;
+    }
+
+    public function getParams(): array
+    {
+        return $this->params;
+    }
+
+    public function send(): void
+    {
+        try {
+            $redisClient = $this->redisMasterFactory->create(
+                $this->redisDb
+            );
+
+            $redisClient->rPush(
+                self::CHANNEL,
+                \json_encode($this->params)
+            );
+
+            $redisClient->close();
+        } catch (\Exception $e) {
+            $this
+                ->logger
+                ->error(
+                    $e->getMessage()
+                );
+        }
+    }
+}
