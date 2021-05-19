d308c3143dbf28c151a3c0e1d21ea3b923136479
Mikel Madariaga <mikel@irontec.com>
2021-05-19 12:28:40 +0200

    core: fixed BillableCall rotator
    Higher id's don't guarantee higher startTime, removed time based discrimination

--- library/Ivoz/Provider/Domain/Service/BillableCallHistoric/ImportFromBillableCalls.php
+++ library/Ivoz/Provider/Domain/Service/BillableCallHistoric/ImportFromBillableCalls.php
@@ -4,6 +4,7 @@ namespace Ivoz\Provider\Domain\Service\BillableCallHistoric;
 
 use Ivoz\Provider\Domain\Model\BillableCall\BillableCallRepository;
 use Ivoz\Provider\Domain\Model\BillableCallHistoric\BillableCallHistoricRepository;
+use Psr\Log\LoggerInterface;
 
 class ImportFromBillableCalls
 {
@@ -12,13 +13,16 @@ class ImportFromBillableCalls
 
     protected $billableCallRepository;
     protected $billableCallHistoricRepository;
+    protected $logger;
 
     public function __construct(
         BillableCallRepository $billableCallRepository,
-        BillableCallHistoricRepository $billableCallHistoricRepository
+        BillableCallHistoricRepository $billableCallHistoricRepository,
+        LoggerInterface $logger
     ) {
         $this->billableCallRepository = $billableCallRepository;
         $this->billableCallHistoricRepository = $billableCallHistoricRepository;
+        $this->logger = $logger;
     }
 
     /**
@@ -26,49 +30,63 @@ class ImportFromBillableCalls
      */
     public function execute(): int
     {
-        $affectedRows = 0;
+        $affectedRowsSum = 0;
 
         do {
             $fromId = $this->billableCallHistoricRepository->getMaxId();
-            $untilDate = $this->getRotateUntilDate($fromId);
+            if (!isset($untilId)) {
+                $untilId = $this->getRotateUntilId(
+                    $fromId
+                );
+            }
+
+            $infoMsg = sprintf(
+                'About migrate %d calls from #%d until #%d',
+                self::SYNC_CHUNK_SIZE,
+                $fromId,
+                $untilId
+            );
+            $this->logger->info($infoMsg);
+
             $ids = $this->billableCallRepository->getIdsInRange(
                 $fromId,
-                $untilDate,
+                $untilId,
                 self::SYNC_CHUNK_SIZE
             );
 
             if (empty($ids)) {
-                return $affectedRows;
+                return $affectedRowsSum;
             }
 
-            $affectedRows += $this
+            $affectedRows = $this
                 ->billableCallHistoricRepository
                 ->copyBillableCallsByIds(
                     $ids
                 );
 
+            $affectedRowsSum += $affectedRows;
+
             sleep(self::SLEEP_BETWEEN_LOOPS);
         } while ($affectedRows > 0);
 
-        return $affectedRows;
+        return $affectedRowsSum;
     }
 
-    private function getRotateUntilDate(int $fromId): \DateTime
+    private function getRotateUntilId(int $fromId): int
     {
         $utc =  new \DateTimeZone('UTC');
         $now = new \DateTime(null, $utc);
         $yearAgo = (clone $now)->modify('-1 year');
-        $fromDate = $this->billableCallRepository->getMinStartTime($fromId);
 
-        if ($fromDate >= $yearAgo) {
-            return $yearAgo;
-        }
+        $fromDate = $this->billableCallRepository->getMinStartTime($fromId);
 
         $rotateUntilDate = (clone $fromDate)->modify('+1 week');
         if ($rotateUntilDate > $yearAgo) {
-            return $yearAgo;
+            $rotateUntilDate = $yearAgo;
         }
 
-        return $rotateUntilDate;
+        $maxId = $this->billableCallRepository->getMaxIdUntilDate($fromId, $rotateUntilDate);
+
+        return $maxId;
     }
 }
