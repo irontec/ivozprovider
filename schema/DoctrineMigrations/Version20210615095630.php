598e061ba8b6de8326e84c926eccfea47680f008
Carlos Cruz <carlos@irontec.com>
2021-06-15 11:58:04 +0200

    schema: DDI unique per brand, not globally

---
+++ schema/DoctrineMigrations/Version20210615095630.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace Application\Migrations;
+
+use Ivoz\Core\Infrastructure\Persistence\Doctrine\LoggableMigration;
+use Doctrine\DBAL\Schema\Schema;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+class Version20210615095630 extends LoggableMigration
+{
+    /**
+     * @param Schema $schema
+     */
+    public function up(Schema $schema)
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('DROP INDEX DDIcountry ON DDIs');
+        $this->addSql('CREATE UNIQUE INDEX Ddicountrybrand ON DDIs (Ddi, brandId, countryId)');
+    }
+
+    /**
+     * @param Schema $schema
+     */
+    public function down(Schema $schema)
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('DROP INDEX Ddicountrybrand ON DDIs');
+        $this->addSql('CREATE UNIQUE INDEX DDIcountry ON DDIs (DDI, countryId)');
+    }
+}
