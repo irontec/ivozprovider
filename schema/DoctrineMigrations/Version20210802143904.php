af3d7cca3bed29033e99ce8dee2329bb62bb4858
Carlos Cruz <carlos@irontec.com>
2021-08-02 16:48:07 +0200

    schema: add kam_trunks_uacreg new field for Kamailio 5.5.1

---
+++ schema/DoctrineMigrations/Version20210802143904.php
@@ -0,0 +1,32 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Application\Migrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20210802143904 extends AbstractMigration
+{
+    public function isTransactional() : bool
+    {
+        return false;
+    }
+
+    public function up(Schema $schema) : void
+    {
+        // https://www.kamailio.org/wiki/install/upgrade/5.4.x-to-5.5.0
+        $this->addSql("ALTER TABLE kam_trunks_uacreg ADD contact_addr VARCHAR(255) NOT NULL DEFAULT ''");
+        $this->addSql("UPDATE kam_version SET table_version=5 WHERE TABLE_NAME='kam_trunks_uacreg'");
+    }
+
+    public function down(Schema $schema) : void
+    {
+        $this->addSql("UPDATE kam_version SET table_version=4 WHERE TABLE_NAME='kam_trunks_uacreg'");
+        $this->addSql("ALTER TABLE kam_trunks_uacreg DROP contact_addr");
+    }
+}
