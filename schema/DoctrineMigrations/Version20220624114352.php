d167611e70d90b83924ac4e1d47234f212e32271
Carlos Cruz <carlos@irontec.com>
2022-06-24 13:57:14 +0200

    schema: update kam_trunks_uacreg contact_addr values

---
+++ schema/DoctrineMigrations/Version20220624114352.php
@@ -0,0 +1,31 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Application\Migrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20220624114352 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return 'Set contactAddr value for uacreg entries';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('UPDATE kam_trunks_uacreg KTU JOIN DDIProviderRegistrations DPR ON DPR.id=KTU.ddiProviderRegistrationId JOIN DDIProviders DP ON DP.id=DPR.ddiProviderId LEFT JOIN ProxyTrunks PT ON PT.id=COALESCE(DP.proxyTrunkId, 1) SET contact_addr=CONCAT(PT.ip, ":5060")');
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql("UPDATE kam_trunks_uacreg SET contact_addr=''");
+    }
+}
