df0df5ed9265c881d2f43229228619b6cafcf1c4
Kaian <kaian@irontec.com>
2022-06-30 13:12:08 +0200

    schema: fix migration 20220627151626 class name

--- schema/DoctrineMigrations/Version20220627151626.php
+++ schema/DoctrineMigrations/Version20220627151626.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20220627155554 extends AbstractMigration
+final class Version20220627151626 extends AbstractMigration
 {
     public function getDescription(): string
     {
