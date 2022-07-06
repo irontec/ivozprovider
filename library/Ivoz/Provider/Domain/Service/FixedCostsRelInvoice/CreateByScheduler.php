3f0f816e141e5a0fba38e180012396f4d911110f
Kaian <kaian@irontec.com>
2022-07-06 12:08:59 +0200

    core: update FixedCosts services with new matching options

--- library/Ivoz/Provider/Domain/Service/FixedCostsRelInvoice/CreateByScheduler.php
+++ library/Ivoz/Provider/Domain/Service/FixedCostsRelInvoice/CreateByScheduler.php
@@ -3,7 +3,8 @@
 namespace Ivoz\Provider\Domain\Service\FixedCostsRelInvoice;
 
 use Ivoz\Core\Application\Service\EntityTools;
-use Ivoz\Provider\Domain\Model\FixedCostsRelInvoice\FixedCostsRelInvoice;
+use Ivoz\Provider\Domain\Model\Ddi\DdiRepository;
+use Ivoz\Provider\Domain\Model\FixedCostsRelInvoice\FixedCostsRelInvoiceDto;
 use Ivoz\Provider\Domain\Model\FixedCostsRelInvoiceScheduler\FixedCostsRelInvoiceSchedulerInterface;
 use Ivoz\Provider\Domain\Model\Invoice\InvoiceInterface;
 use Ivoz\Provider\Domain\Model\InvoiceScheduler\InvoiceSchedulerInterface;
@@ -11,28 +12,74 @@ use Ivoz\Provider\Domain\Model\InvoiceScheduler\InvoiceSchedulerInterface;
 class CreateByScheduler
 {
     public function __construct(
-        private EntityTools $entityTools
+        private EntityTools $entityTools,
+        private DdiRepository $ddiRepository
     ) {
     }
 
-    /**
-     * @param InvoiceSchedulerInterface $scheduler
-     * @param InvoiceInterface $invoice
-     *
-     * @return void
-     */
     public function execute(
         InvoiceSchedulerInterface $scheduler,
         InvoiceInterface $invoice
-    ) {
-        /** @var FixedCostsRelInvoiceSchedulerInterface[] $relFixedCosts */
+    ): void {
+        $company = $invoice->getCompany();
         $relFixedCosts = $scheduler->getRelFixedCosts();
         foreach ($relFixedCosts as $relFixedCost) {
-            $fixedCostRelInvoice = FixedCostsRelInvoice::fromFixedCostsRelInvoiceScheduler(
-                $invoice,
-                $relFixedCost
-            );
-            $this->entityTools->persist($fixedCostRelInvoice);
+            // Calculate Dynamic quantities
+            $quantity = $relFixedCost->getQuantity();
+            $type = $relFixedCost->getType();
+            if ($type === FixedCostsRelInvoiceSchedulerInterface::TYPE_MAXCALLS) {
+                // Quantity based on Client Max Calls setting
+                $quantity = $company->getMaxCalls();
+            } elseif ($type === FixedCostsRelInvoiceSchedulerInterface::TYPE_DDIS) {
+                // Quantity based on Client DDI Count
+                $ddisMatch = $relFixedCost->getDdisCountryMatch();
+                $companyCountry = $company->getCountry();
+                // DDI Match All Countries
+                if ($ddisMatch == FixedCostsRelInvoiceSchedulerInterface::DDISCOUNTRYMATCH_ALL) {
+                    $quantity = $this->ddiRepository->countByCompany(
+                        (int) $company->getId()
+                    );
+                }
+                // DDI Match from client country
+                if ($ddisMatch == FixedCostsRelInvoiceSchedulerInterface::DDISCOUNTRYMATCH_NATIONAL) {
+                    $quantity = $this->ddiRepository->countByCompanyAndCountry(
+                        (int) $company->getId(),
+                        (int) $companyCountry->getId()
+                    );
+                }
+                // DDI Match NOT from client country
+                if ($ddisMatch == FixedCostsRelInvoiceSchedulerInterface::DDISCOUNTRYMATCH_INTERNATIONAL) {
+                    $quantity = $this->ddiRepository->countByCompanyAndNotCountry(
+                        (int) $company->getId(),
+                        (int) $companyCountry->getId()
+                    );
+                }
+                // DDI Match from specific country
+                if ($ddisMatch == FixedCostsRelInvoiceSchedulerInterface::DDISCOUNTRYMATCH_SPECIFIC) {
+                    $ddisCountry = $relFixedCost->getDdisCountry();
+                    if (!$ddisCountry) {
+                        return;
+                    }
+                    $quantity = $this->ddiRepository->countByCompanyAndCountry(
+                        (int) $company->getId(),
+                        (int) $ddisCountry->getId()
+                    );
+                }
+            }
+
+            $fixedCostRelInvoiceDto = new FixedCostsRelInvoiceDto();
+            $fixedCostRelInvoiceDto
+                ->setQuantity(
+                    $quantity
+                )
+                ->setFixedCostId(
+                    $relFixedCost->getFixedCost()->getId()
+                )
+                ->setInvoiceId(
+                    $invoice->getId()
+                );
+
+            $this->entityTools->persistDto($fixedCostRelInvoiceDto);
         }
     }
 }
