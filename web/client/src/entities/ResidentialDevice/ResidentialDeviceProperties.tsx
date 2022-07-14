899a3b016f1850ecf646a855315e255103f60863
Mikel <mikel@irontec.com>
2022-07-14 12:41:14 +0200

    web/client: fixed residential device screen

--- web/client/src/entities/ResidentialDevice/ResidentialDeviceProperties.tsx
+++ web/client/src/entities/ResidentialDevice/ResidentialDeviceProperties.tsx
@@ -4,7 +4,41 @@ import {
   EntityValues,
 } from '@irontec/ivoz-ui/services/entity/EntityService';
 
-export type ResidentialDevicePropertyList<T> = Record<string, T>;
+export type ResidentialDevicePropertyList<T> = {
+  name: T;
+  domainName: T;
+  description: T;
+  transport: T;
+  ip: T;
+  port: T;
+  password: T;
+  outgoingDdi: T;
+  disallow: T;
+  allow: T;
+  directMediaMethod: T;
+  ddiIn: T;
+  language: T;
+  transformationRuleSet: T;
+  calleridUpdateHeader: T;
+  updateCallerid: T;
+  fromDomain: T;
+  maxCalls: T;
+  t38Passthrough: T;
+  rtpEncryption: T;
+  multiContact: T;
+  status: T;
+  statusIcon: T;
+  directConnectivity: T;
+};
+
+export type ResidentialDeviceStatus = {
+  contact: string;
+  publicContact: boolean;
+  expires: string;
+  publicReceived: boolean;
+  received: string;
+  userAgent: string;
+};
 
 export type ResidentialDeviceProperties = ResidentialDevicePropertyList<
   Partial<PropertySpec>
