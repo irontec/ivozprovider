16d662b5fb3472f1bd84417d487476dafc183c34
rbatista <rbatista@irontec.com>
2022-07-18 10:46:25 +0200

    web/user: Cleaned translations files

---
+++ web/portal/user/src/translations/index.js
@@ -0,0 +1,21 @@
+import es from './es';
+import en from './en';
+import ca from './ca';
+import it from './it';
+
+const translations = {
+  es: {
+    translation: es,
+  },
+  en: {
+    translation: en,
+  },
+  ca: {
+    translation: ca,
+  },
+  it: {
+    translation: it,
+  },
+};
+
+export default translations;
