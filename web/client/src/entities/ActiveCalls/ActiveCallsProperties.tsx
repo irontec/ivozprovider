5a4341ae81210dc3c9e0f1d29e8cfbc6e7cef549
Mikel <mikel@irontec.com>
2022-07-21 12:41:03 +0200

    web/client: added ActiveCalls entity

---
+++ web/client/src/entities/ActiveCalls/ActiveCallsProperties.tsx
@@ -0,0 +1,33 @@
+import { PropertySpec } from '@irontec/ivoz-ui/services/api/ParsedApiSpecInterface';
+import {
+  EntityValue,
+  EntityValues,
+} from '@irontec/ivoz-ui/services/entity/EntityService';
+
+export type BillableCallPropertyList<T> = {
+  attr?: T;
+  startTime?: T;
+  callid?: T;
+  caller?: T;
+  callee?: T;
+  destinationName?: T;
+  direction?: T;
+  invoice?: T;
+  price?: T;
+  duration?: T;
+  cost?: T;
+  carrierName?: T;
+  ratingPlanName?: T;
+  endpointType?: T;
+  endpointId?: T;
+  endpointName?: T;
+  ddiProvider?: T;
+};
+
+export type BillableCallProperties = BillableCallPropertyList<
+  Partial<PropertySpec>
+>;
+
+export type BillableCallPropertiesList = Array<
+  BillableCallPropertyList<EntityValue | EntityValues>
+>;
