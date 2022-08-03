0e2b531b6aa814f06e770edc835753981fe39921
Mikel <mikel@irontec.com>
2022-08-03 13:33:46 +0200

    web/user: replace react CRA dependencies by vite

--- web/portal/user/src/config.js
+++ web/portal/user/src/config.js
@@ -4,7 +4,6 @@ const defaults = {
 
 const dev = {
   ...defaults,
-  API_URL: `//${window.location.host}/api/user/dev.php`,
 };
 
 const prod = {
@@ -12,6 +11,6 @@ const prod = {
 };
 
 // Default to dev if not set
-const config = process.env.APP_ENV === 'dev' ? dev : prod;
+const config = import.meta.env.DEV === 'dev' ? dev : prod;
 
 export default config;
