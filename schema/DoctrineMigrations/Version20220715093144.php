3f3455a019a10603e50f79f248c5180c15d3c6b0
Mikel <mikel@irontec.com>
2022-08-11 10:24:06 +0200

    schema: updated migrations base class

--- schema/DoctrineMigrations/Version20220715093144.php
+++ schema/DoctrineMigrations/Version20220715093144.php
@@ -5,12 +5,12 @@ declare(strict_types=1);
 namespace Application\Migrations;
 
 use Doctrine\DBAL\Schema\Schema;
-use Doctrine\Migrations\AbstractMigration;
+use Ivoz\Core\Infrastructure\Persistence\Doctrine\LoggableMigration;
 
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20220715093144 extends AbstractMigration
+final class Version20220715093144 extends LoggableMigration
 {
     public function getDescription(): string
     {
