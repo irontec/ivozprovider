a6939f7fa25336e1014871e0198cadfb15a5d0bf
Kaian <kaian@irontec.com>
2022-10-07 11:14:03 +0200

    schema: update rtpengine control port to 2223

---
+++ schema/DoctrineMigrations/Version20221007090801.php
@@ -0,0 +1,26 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Application\Migrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+final class Version20221007090801 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return 'Update Media relay ports from 22223 to 2223';
+    }
+
+    public function up(Schema $schema): void
+    {
+		$this->addSql("UPDATE kam_rtpengine SET url = REPLACE(url, ':22223', ':2223');");
+    }
+
+    public function down(Schema $schema): void
+    {
+		$this->addSql("UPDATE kam_rtpengine SET url = REPLACE(url, ':2223', ':22223');");
+    }
+}
