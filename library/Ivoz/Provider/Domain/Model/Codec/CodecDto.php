14d53d1b165465f2c68e952659c9f596089df692
Mikel <mikel@irontec.com>
2022-11-15 12:37:10 +0100

    rest/brand: updated API properties & endpoints

--- library/Ivoz/Provider/Domain/Model/Codec/CodecDto.php
+++ library/Ivoz/Provider/Domain/Model/Codec/CodecDto.php
@@ -4,4 +4,18 @@ namespace Ivoz\Provider\Domain\Model\Codec;
 
 class CodecDto extends CodecDtoAbstract
 {
+    /**
+     * @inheritdoc
+     */
+    public static function getPropertyMap(string $context = '', string $role = null): array
+    {
+        if ($context === self::CONTEXT_COLLECTION) {
+            return [
+                'iden' => 'iden',
+                'id' => 'id'
+            ];
+        }
+
+        return parent::getPropertyMap($context, $role);
+    }
 }
