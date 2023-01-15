af7fff211f96b1fdcbeb750995371262f17b1554
Roger Batista <rbatista@irontec.com>
2023-01-15 19:25:24 +0100

    core: Added new endpoint for template preview.

---
+++ web/rest/brand/src/Controller/Provider/InvoiceTemplatePreviewAction.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Controller\Provider;
+
+use Service\InvoiceTemplate\InvoiceTemplateGenerator;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+class InvoiceTemplatePreviewAction
+{
+    public function __construct(
+        private InvoiceTemplateGenerator $invoiceTemplateGenerator
+    ) {
+    }
+
+    public function __invoke(Request $request): Response
+    {
+        $invoiceTemplateId = (int)$request->attributes->get('id');
+
+        $invoiceTemplate = $this
+            ->invoiceTemplateGenerator
+            ->execute($invoiceTemplateId);
+
+        if (is_null($invoiceTemplate)) {
+            return new Response('Invoice Template not found', 404);
+        }
+
+        return new Response($invoiceTemplate, 200);
+    }
+}
