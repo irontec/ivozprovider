f615dff27d7fd1e5f6d21d9f5a946983b057f106
Kaian <kaian@irontec.com>
2023-01-16 11:24:46 +0100

    core: add service to update user contacts

--- library/Ivoz/Provider/Domain/Model/Contact/Contact.php
+++ library/Ivoz/Provider/Domain/Model/Contact/Contact.php
@@ -11,7 +11,7 @@ class Contact extends ContactAbstract implements ContactInterface
 
     /**
      * @codeCoverageIgnore
-     * @return array
+     * @return array<string, mixed>
      */
     public function getChangeSet(): array
     {
@@ -26,5 +26,24 @@ class Contact extends ContactAbstract implements ContactInterface
     public function getId(): ?int
     {
         return $this->id;
+    }
+
+    protected function sanitizeValues(): void
+    {
+        // Set Work Phone in E.164 format
+        $workPhoneCountry = $this->getWorkPhoneCountry();
+        $workPhone = $this->getWorkPhone();
+        $workPhoneE164 = ($workPhoneCountry && $workPhone)
+            ? $workPhoneCountry->getCountryCode() . $workPhone
+            : null;
+        $this->setWorkPhoneE164($workPhoneE164);
+
+        // Set Mobile Phone in E.164 format
+        $mobilePhoneCountry = $this->getMobilePhoneCountry();
+        $mobilePhone = $this->getMobilePhone();
+        $mobilePhoneE164 = ($mobilePhoneCountry && $mobilePhone)
+            ? $mobilePhoneCountry->getCountryCode() . $mobilePhone
+            : null;
+        $this->setMobilePhoneE164($mobilePhoneE164);
     }
 }
