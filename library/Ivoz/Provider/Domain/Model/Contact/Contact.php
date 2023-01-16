47b8074459131912bbd7cc3dc48fd93db7c2eee3
Kaian <kaian@irontec.com>
2023-01-16 16:22:08 +0100

    core: set E.164 format numbers from Country and Number

--- library/Ivoz/Provider/Domain/Model/Contact/Contact.php
+++ library/Ivoz/Provider/Domain/Model/Contact/Contact.php
@@ -27,4 +27,27 @@ class Contact extends ContactAbstract implements ContactInterface
     {
         return $this->id;
     }
+
+    /**
+     * @return void
+     * @throws \Exception
+     */
+    protected function sanitizeValues()
+    {
+        // Set Work Phone in E.164 format
+        $workPhoneCountry = $this->getWorkPhoneCountry();
+        $workPhone = $this->getWorkPhone();
+        $workPhoneE164 = ($workPhoneCountry && $workPhone)
+            ? $workPhoneCountry->getCountryCode() . $workPhone
+            : null;
+        $this->setWorkPhoneE164($workPhoneE164);
+
+        // Set Mobile Phone in E.164 format
+        $mobilePhoneCountry = $this->getMobilePhoneCountry();
+        $mobilePhone = $this->getMobilePhone();
+        $mobilePhoneE164 = ($mobilePhoneCountry && $mobilePhone)
+            ? $mobilePhoneCountry->getCountryCode() . $mobilePhone
+            : null;
+        $this->setMobilePhoneE164($mobilePhoneE164);
+    }
 }
