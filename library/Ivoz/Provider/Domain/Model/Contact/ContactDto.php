f45dc6c9d645a61ad9f054db460aaa8228736e1b
Kaian <kaian@irontec.com>
2023-01-16 17:20:51 +0100

    core: updated exposed API fields in Contact entity

--- library/Ivoz/Provider/Domain/Model/Contact/ContactDto.php
+++ library/Ivoz/Provider/Domain/Model/Contact/ContactDto.php
@@ -4,4 +4,70 @@ namespace Ivoz\Provider\Domain\Model\Contact;
 
 class ContactDto extends ContactDtoAbstract
 {
+    /**
+     * @inheritdoc
+     * @codeCoverageIgnore
+     */
+    public static function getPropertyMap(string $context = '', string $role = null): array
+    {
+        if ($context === self::CONTEXT_COLLECTION) {
+            return [
+                'id' => 'id',
+                'name' => 'name',
+                'lastname' => 'lastname',
+                'email' => 'email',
+                'workPhoneE164' => 'workPhoneE164',
+                'mobilePhoneE164' => 'mobilePhoneE164',
+                'otherPhone' => 'otherPhone',
+                'userId' => 'user',
+            ];
+        }
+
+        $response = parent::getPropertyMap(...func_get_args());
+
+        if ($role === 'ROLE_COMPANY_ADMIN') {
+            unset($response['companyId']);
+        }
+
+        return $response;
+    }
+
+    private function filterUserContactReadOnlyFields(array $data): array
+    {
+        $readOnlyFlds = [
+            'name',
+            'lastname',
+            'email',
+            'otherPhone',
+        ];
+
+        if (!$data['user']) {
+            return $data;
+        }
+
+        foreach ($readOnlyFlds as $readOnlyFld) {
+            if (!isset($data[$readOnlyFld])) {
+                continue;
+            }
+
+            unset($data[$readOnlyFld]);
+        }
+
+        return $data;
+    }
+
+
+    public function denormalize(array $data, string $context, string $role = ''): void
+    {
+        $contextProperties = self::getPropertyMap($context, $role);
+        if ($role === 'ROLE_COMPANY_ADMIN') {
+            $contextProperties['companyId'] = 'company';
+            $data = $this->filterUserContactReadOnlyFields($data);
+        }
+
+        $this->setByContext(
+            $contextProperties,
+            $data
+        );
+    }
 }
