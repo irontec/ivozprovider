f8c2ff8d3dd6981a2724ac74f8149f562e4a1197
Mikel <mikel@irontec.com>
2023-01-30 17:42:25 +0100

    web/brand: fixed destination form error

--- web/portal/brand/src/entities/Destination/SelectOptions.ts
+++ web/portal/brand/src/entities/Destination/SelectOptions.ts
@@ -1,5 +1,7 @@
 import defaultEntityBehavior from '@irontec/ivoz-ui/entities/DefaultEntityBehavior';
 import { SelectOptionsType } from '@irontec/ivoz-ui/entities/EntityInterface';
+import { DropdownChoices } from '@irontec/ivoz-ui';
+import { getI18n } from 'react-i18next';
 import store from 'store';
 
 const DestinationSelectOptions: SelectOptionsType = ({
@@ -8,14 +10,18 @@ const DestinationSelectOptions: SelectOptionsType = ({
 }): Promise<unknown> => {
   const entities = store.getState().entities.entities;
   const Destination = entities.Destination;
+  const language = getI18n().language.substring(0, 2);
 
   return defaultEntityBehavior.fetchFks(
-    Destination.path,
-    ['id'],
+    Destination.path + `?_order[name.${[language]}]=ASC`,
+    ['id', 'name'],
     (data: any) => {
-      const options: any = {};
+      const options: DropdownChoices = [];
       for (const item of data) {
-        options[item.id] = item.id;
+        options.push({
+          id: item.id,
+          label: `${item.name[language]} (${item.countryCode})`,
+        });
       }
 
       callback(options);
