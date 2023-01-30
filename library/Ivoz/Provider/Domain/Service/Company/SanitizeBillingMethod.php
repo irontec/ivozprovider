076ba322768dd7dbea3fd0cc506fb72eea1a6b36
Kaian <kaian@irontec.com>
2023-01-30 14:11:13 +0100

    core: add Company service to validate billing method

---
+++ library/Ivoz/Provider/Domain/Service/Company/SanitizeBillingMethod.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace Ivoz\Provider\Domain\Service\Company;
+
+use Ivoz\Core\Application\Service\EntityTools;
+use Ivoz\Provider\Domain\Model\Company\CompanyDto;
+use Ivoz\Provider\Domain\Model\Company\CompanyInterface;
+use Ivoz\Provider\Domain\Model\Feature\Feature;
+
+class SanitizeBillingMethod implements CompanyLifecycleEventHandlerInterface
+{
+    public const PRE_PERSIST_PRIORITY = self::PRIORITY_NORMAL;
+
+    public function __construct(
+        private EntityTools $entityTools,
+    ) {
+    }
+
+    /** @return array<array-key, int> */
+    public static function getSubscribedEvents()
+    {
+        return [
+            self::EVENT_PRE_PERSIST => self::PRE_PERSIST_PRIORITY
+        ];
+    }
+
+    public function execute(CompanyInterface $company): void
+    {
+        // No Billing method, nothing to validate
+        if ($company->getBillingMethod() == CompanyInterface::BILLINGMETHOD_NONE) {
+            return;
+        }
+
+        $brand = $company->getBrand();
+        $hasBillingEnabled = $brand->hasFeature(Feature::BILLING);
+        // If Brand has no Billing feature enabled, disable Company Billing
+        if (!$hasBillingEnabled) {
+            /** @var CompanyDto $companyDto */
+            $companyDto = $this->entityTools->entityToDto(
+                $company
+            );
+            $companyDto->setBillingMethod(CompanyInterface::BILLINGMETHOD_NONE);
+            $this->entityTools->updateEntityByDto(
+                $company,
+                $companyDto
+            );
+        }
+    }
+}
