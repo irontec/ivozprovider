c6526b7d332ded5e7283b717bffafd33e4be468d
Fernando Mendoza <fmendoza@irontec.com>
2023-02-01 15:24:14 +0000

    web/portal: Added Banned Address to platform portal

---
+++ web/portal/platform/src/entities/BannedAddress/BannedAddressProperties.ts
@@ -0,0 +1,17 @@
+import { PropertySpec } from '@irontec/ivoz-ui/services/api/ParsedApiSpecInterface';
+import {
+  EntityValue,
+  EntityValues,
+} from '@irontec/ivoz-ui/services/entity/EntityService';
+
+export type BannedAddressPropertyList<T> = {
+  ip?: T;
+  lastTimeBanned?: T;
+};
+
+export type BannedAddressProperties = BannedAddressPropertyList<
+  Partial<PropertySpec>
+>;
+export type BannedAddressPropertiesList = Array<
+  BannedAddressPropertyList<EntityValue | EntityValues>
+>;
