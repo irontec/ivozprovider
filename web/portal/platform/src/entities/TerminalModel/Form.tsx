7aea33cb968ebfb417695774a8386a68e2e60a42
Fernando Mendoza <fmendoza@irontec.com>
2023-02-01 16:23:49 +0000

    web/portal: Added Teminal Manufacturer to platform portal

---
+++ web/portal/platform/src/entities/TerminalModel/Form.tsx
@@ -0,0 +1,43 @@
+import defaultEntityBehavior, {
+  EntityFormProps,
+  FieldsetGroups,
+} from '@irontec/ivoz-ui/entities/DefaultEntityBehavior';
+
+const Form = (props: EntityFormProps): JSX.Element => {
+  const DefaultEntityForm = defaultEntityBehavior.Form;
+
+  const groups: Array<FieldsetGroups | false> = [
+    {
+      legend: '',
+      fields: ['iden'],
+    },
+    {
+      legend: '',
+      fields: ['name'],
+    },
+    {
+      legend: '',
+      fields: ['description'],
+    },
+    {
+      legend: '',
+      fields: ['genericUrlPattern'],
+    },
+    {
+      legend: '',
+      fields: ['genericTemplate'],
+    },
+    {
+      legend: '',
+      fields: ['specificUrlPattern'],
+    },
+    {
+      legend: '',
+      fields: ['specificTemplate'],
+    },
+  ];
+
+  return <DefaultEntityForm {...props} groups={groups} />;
+};
+
+export default Form;
