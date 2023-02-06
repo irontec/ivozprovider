bc29d398522e0a3dfbb0e57edb025a75183fc1e9
Roger Batista <rbatista@irontec.com>
2023-02-06 10:16:53 +0100

    web/platform: Added currency entity to platform portal

---
+++ web/portal/platform/src/entities/Currency/Form.tsx
@@ -0,0 +1,20 @@
+import defaultEntityBehavior, {
+  EntityFormProps,
+  FieldsetGroups,
+} from '@irontec/ivoz-ui/entities/DefaultEntityBehavior';
+import _ from '@irontec/ivoz-ui/services/translations/translate';
+
+const Form = (props: EntityFormProps): JSX.Element => {
+  const DefaultEntityForm = defaultEntityBehavior.Form;
+
+  const groups: Array<FieldsetGroups | false> = [
+    {
+      legend: '',
+      fields: ['iden', 'symbol', 'name'],
+    },
+  ];
+
+  return <DefaultEntityForm {...props} groups={groups} />;
+};
+
+export default Form;
