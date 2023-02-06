60e87dd568cc19a22d74e158885d79f73d078d72
Fernando Mendoza <fmendoza@irontec.com>
2023-02-06 22:16:29 +0000

    web/portal: Added Special Number entity to portal platform

---
+++ web/portal/platform/src/entities/Country/Country.tsx
@@ -0,0 +1,37 @@
+import SettingsApplications from '@mui/icons-material/SettingsApplications';
+import EntityInterface from '@irontec/ivoz-ui/entities/EntityInterface';
+import _ from '@irontec/ivoz-ui/services/translations/translate';
+import defaultEntityBehavior from '@irontec/ivoz-ui/entities/DefaultEntityBehavior';
+import { getI18n } from 'react-i18next';
+import { CountryProperties, CountryPropertyList } from './CountryProperties';
+import selectOptions from './SelectOptions';
+import { EntityValue } from '@irontec/ivoz-ui';
+
+const properties: CountryProperties = {
+  name: {
+    label: _('Name'),
+  },
+  countryCode: {
+    label: _('Country code'),
+  },
+};
+
+const country: EntityInterface = {
+  ...defaultEntityBehavior,
+  icon: SettingsApplications,
+  iden: 'Country',
+  title: _('Country', { count: 2 }),
+  path: '/countries',
+  toStr: (row: CountryPropertyList<EntityValue>) => {
+    const language = getI18n().language.substring(0, 2);
+    return (row.name as string)[language as any];
+  },
+  properties,
+  acl: {
+    ...defaultEntityBehavior.acl,
+    iden: 'Countries',
+  },
+  selectOptions,
+};
+
+export default country;
---
+++ web/portal/platform/src/entities/Country/CountryNameSelectOptions.ts
@@ -0,0 +1,31 @@
+import { DropdownChoices, EntityValues } from '@irontec/ivoz-ui';
+import defaultEntityBehavior from '@irontec/ivoz-ui/entities/DefaultEntityBehavior';
+import { SelectOptionsType } from '@irontec/ivoz-ui/entities/EntityInterface';
+import store from 'store';
+
+const CountryNameSelectOptions: SelectOptionsType = ({
+  callback,
+  cancelToken,
+}): Promise<unknown> => {
+  const entities = store.getState().entities.entities;
+  const Country = entities.Country;
+
+  return defaultEntityBehavior.fetchFks(
+    Country.path,
+    ['id', 'name'],
+    (data: Array<EntityValues>) => {
+      const options: DropdownChoices = [];
+      for (const item of data) {
+        options.push({
+          id: item.id as string,
+          label: `${Country.toStr(item)}` as string,
+        });
+      }
+
+      callback(options);
+    },
+    cancelToken
+  );
+};
+
+export default CountryNameSelectOptions;
