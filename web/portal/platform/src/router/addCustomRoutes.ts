4691acfaed2b793cb12a819fe97746b622084dce
Fernando Mendoza <fmendoza@irontec.com>
2023-02-07 15:00:20 +0000

    web/portal: Added Active calls to platfom portal

--- web/portal/platform/src/router/addCustomRoutes.ts
+++ web/portal/platform/src/router/addCustomRoutes.ts
@@ -1,6 +1,15 @@
 import { RouteSpec } from '@irontec/ivoz-ui';
+import ActiveCallsComponent from '../components/ActiveCalls';
 
 const addCustomRoutes = (routes: Array<RouteSpec>): Array<RouteSpec> => {
+  const activeCallsRoute = routes.find(
+    (route) => route.key === 'ActiveCalls-list'
+  );
+
+  if (activeCallsRoute) {
+    activeCallsRoute.component = ActiveCallsComponent;
+  }
+
   return routes;
 };
 
