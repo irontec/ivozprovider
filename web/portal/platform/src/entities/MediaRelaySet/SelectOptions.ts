3180a0f38ef5a722f64b00f998592afca0e44243
Fernando Mendoza <fmendoza@irontec.com>
2023-02-08 23:12:20 +0000

    web/portal: Added Media relay set to platform portal

---
+++ web/portal/platform/src/entities/MediaRelaySet/SelectOptions.ts
@@ -0,0 +1,28 @@
+import { DropdownChoices, EntityValues } from '@irontec/ivoz-ui';
+import defaultEntityBehavior from '@irontec/ivoz-ui/entities/DefaultEntityBehavior';
+import { SelectOptionsType } from '@irontec/ivoz-ui/entities/EntityInterface';
+import store from 'store';
+
+const MediaRelaySetSelectOptions: SelectOptionsType = ({
+  callback,
+  cancelToken,
+}): Promise<unknown> => {
+  const entities = store.getState().entities.entities;
+  const MediaRelaySet = entities.MediaRelaySet;
+
+  return defaultEntityBehavior.fetchFks(
+    MediaRelaySet.path + `?_order[name]=ASC`,
+    ['id', 'name'],
+    (data: Array<EntityValues>) => {
+      const options: DropdownChoices = [];
+      for (const item of data) {
+        options.push({ id: item.id as number, label: item.name as string });
+      }
+
+      callback(options);
+    },
+    cancelToken
+  );
+};
+
+export default MediaRelaySetSelectOptions;
