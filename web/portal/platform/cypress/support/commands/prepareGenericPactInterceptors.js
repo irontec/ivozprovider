8db6eaa27224e06dd5c5bddde462b81cebed0e1b
Fernando Mendoza <fmendoza@irontec.com>
2023-02-15 11:26:26 +0000

    test: Added Administrator cypress test

--- web/portal/platform/cypress/support/commands/prepareGenericPactInterceptors.js
+++ web/portal/platform/cypress/support/commands/prepareGenericPactInterceptors.js
@@ -1,6 +1,7 @@
 import apiSpec from '../../fixtures/apiSpec.json';
 import TerminalManufacturerCollection from '../../fixtures/TerminalManufacturer/getCollection.json';
 import InvoiceTemplateCollection from '../../fixtures/InvoiceTemplate/getCollection.json';
+import AdministratorCollection from '../../fixtures/Administrator/getCollection.json';
 import UserCollection from '../../fixtures/Users/getCollection.json';
 
 Cypress.Commands.add('prepareGenericPactInterceptors', (pactContextName) => {
@@ -27,4 +28,8 @@ Cypress.Commands.add('prepareGenericPactInterceptors', (pactContextName) => {
   cy.intercept('GET', '**/api/platform/invoice_templates?*', {
     ...InvoiceTemplateCollection,
   }).as('getInvoiceTemplate');
+
+  cy.intercept('GET', '**/api/platform/administrators?*', {
+    ...AdministratorCollection,
+  }).as('getAdministrator');
 });
