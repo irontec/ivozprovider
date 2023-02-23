d68a8d89e02a4ef3993de2ac63ae536eea0466cd
Fernando Mendoza <fmendoza@irontec.com>
2023-02-23 16:27:02 +0000

    portal/brand: remove create, delete & update actons from CompanyBalance

--- web/portal/brand/src/entities/CompanyBalances/CompanyBalances.tsx
+++ web/portal/brand/src/entities/CompanyBalances/CompanyBalances.tsx
@@ -4,6 +4,14 @@ import _ from '@irontec/ivoz-ui/services/translations/translate';
 
 const CompanyBalances = {
   ...Company,
+  acl: {
+    read: true,
+    create: false,
+    update: false,
+    detail: false,
+    delete: false,
+    iden: 'Companies',
+  },
   icon: CurrencyYenIcon,
   title: _('Prepaid Balance', { count: 2 }),
   localPath: '/prepaid_balance',
