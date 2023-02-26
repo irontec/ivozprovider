e8d9678bd6b4e627319f2b3ca2d1c8f83a55180f
Roger Batista <rbatista@irontec.com>
2023-02-26 11:15:50 +0100

    portal/brand: Required fields, and unused field in form.

--- web/portal/brand/src/entities/BalanceNotification/Form.tsx
+++ web/portal/brand/src/entities/BalanceNotification/Form.tsx
@@ -19,7 +19,7 @@ const Form = (props: EntityFormProps): JSX.Element => {
   const groups: Array<FieldsetGroups | false> = [
     {
       legend: _('Notification options'),
-      fields: ['notificationTemplate', 'toAddress', 'threshold', 'lastSent'],
+      fields: ['notificationTemplate', 'toAddress', 'threshold'],
     },
   ];
 
