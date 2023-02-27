fe877643c25471994f6a45c0e70a987439476a4d
Mikel <mikel@irontec.com>
2023-02-27 12:57:45 +0100

    portal/client: review HuntGroupMember required fields

--- web/portal/client/src/entities/Country/SelectOptions.ts
+++ web/portal/client/src/entities/Country/SelectOptions.ts
@@ -1,5 +1,6 @@
 import defaultEntityBehavior from '@irontec/ivoz-ui/entities/DefaultEntityBehavior';
 import { SelectOptionsType } from '@irontec/ivoz-ui/entities/EntityInterface';
+import { getI18n } from 'react-i18next';
 import store from 'store';
 
 const CountrySelectOptions: SelectOptionsType = ({
@@ -8,6 +9,7 @@ const CountrySelectOptions: SelectOptionsType = ({
 }): Promise<unknown> => {
   const entities = store.getState().entities.entities;
   const Country = entities.Country;
+  const language = getI18n().language.substring(0, 2);
 
   return defaultEntityBehavior.fetchFks(
     Country.path,
@@ -15,7 +17,7 @@ const CountrySelectOptions: SelectOptionsType = ({
     (data: any) => {
       const options: any = {};
       for (const item of data) {
-        options[item.id] = `${Country.toStr(item)} (${item.countryCode})`;
+        options[item.id] = `${item.name[language]} (${item.countryCode})`;
       }
 
       callback(options);
