8ca371c6bd3d56e9bec719cc8716ac10a2cd9ce0
Mikel <mikel@irontec.com>
2023-02-28 11:12:32 +0100

    rest/brand: enhanced company and carrier billing feature access control

--- web/rest/brand/src/Controller/Provider/Carrier/ModifyCarrierBalanceAction.php
+++ web/rest/brand/src/Controller/Provider/Carrier/ModifyCarrierBalanceAction.php
@@ -2,17 +2,22 @@
 
 namespace Controller\Provider\Carrier;
 
+use Ivoz\Provider\Domain\Model\Administrator\AdministratorInterface;
 use Ivoz\Provider\Domain\Model\Carrier\CarrierInterface;
 use Ivoz\Provider\Domain\Model\Carrier\CarrierRepository;
 use Ivoz\Provider\Domain\Service\Carrier\DecrementBalance;
 use Ivoz\Provider\Domain\Service\Carrier\IncrementBalance;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpKernel\Exception\AccessDeniedHttpException;
+use Symfony\Component\Security\Core\Authentication\Token\Storage\TokenStorageInterface;
+use Symfony\Component\Security\Core\Authentication\Token\TokenInterface;
 
 class ModifyCarrierBalanceAction
 {
     const INCREMENT_OPERATION = 'increment';
     const DECREMENT_OPERATION = 'decrement';
     public function __construct(
+        private TokenStorageInterface $tokenStorage,
         private IncrementBalance $incrementBalance,
         private DecrementBalance $decrementBalance,
         private CarrierRepository $carrierRepository
@@ -24,6 +29,14 @@ class ModifyCarrierBalanceAction
         $carrierId = (int) $request->attributes->get('id');
         $operation = $request->request->get('operation');
         $amount = (float) $request->request->get('amount');
+
+        /** @var TokenInterface $token */
+        $token =  $this->tokenStorage->getToken();
+        /** @var AdministratorInterface $user */
+        $user = $token->getUser();
+        if (!$user->brandHasFeature('billing')) {
+            throw new AccessDeniedHttpException();
+        }
 
         $success = match ($operation) {
             self::INCREMENT_OPERATION => $this->incrementBalance->execute($carrierId, $amount),
