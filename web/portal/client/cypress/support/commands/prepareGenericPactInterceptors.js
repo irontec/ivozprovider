4b1a19e4dfef9f7f5f5f4eefd8579ec44ea39348
Fernando Mendoza <fmendoza@irontec.com>
2023-03-03 15:18:50 +0000

    test: Extension cypress test

--- web/portal/client/cypress/support/commands/prepareGenericPactInterceptors.js
+++ web/portal/client/cypress/support/commands/prepareGenericPactInterceptors.js
@@ -1,5 +1,6 @@
 import apiSpec from '../../fixtures/apiSpec.json';
 import TerminalCollection from '../../fixtures/Terminal/getCollection.json';
+import ExtensionCollection from '../../fixtures/Extension/getCollection.json';
 import UserCollection from '../../fixtures/Users/getCollection.json';
 
 Cypress.Commands.add('prepareGenericPactInterceptors', (pactContextName) => {
@@ -22,4 +23,8 @@ Cypress.Commands.add('prepareGenericPactInterceptors', (pactContextName) => {
   cy.intercept('GET', '**/api/client/terminals?*', {
     ...TerminalCollection,
   }).as('getTerminal');
+
+  cy.intercept('GET', '**/api/client/extensions?*', {
+    ...ExtensionCollection,
+  }).as('getExtension');
 });
