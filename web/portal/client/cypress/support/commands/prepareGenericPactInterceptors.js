7dd22b55dbbb5879ba5d03b204f87f66bc73ca36
Fernando Mendoza <fmendoza@irontec.com>
2023-03-03 10:24:38 +0000

    portal/client: user login cypress test

---
+++ web/portal/client/cypress/support/commands/prepareGenericPactInterceptors.js
@@ -0,0 +1,20 @@
+import apiSpec from '../../fixtures/apiSpec.json';
+import UserCollection from '../../fixtures/Users/getCollection.json';
+
+Cypress.Commands.add('prepareGenericPactInterceptors', (pactContextName) => {
+  cy.setupPact(
+    `client-consumer-${pactContextName}`,
+    `client-provider-${pactContextName}`
+  );
+
+  cy.intercept('GET', '**/api/client/docs.json', {
+    body: apiSpec,
+    headers: {
+      'content-type': 'application/json; charset=utf-8',
+    },
+  }).as('getApiSpec');
+
+  cy.intercept('GET', '**/api/client/users?*', {
+    ...UserCollection,
+  }).as('getUsers');
+});
