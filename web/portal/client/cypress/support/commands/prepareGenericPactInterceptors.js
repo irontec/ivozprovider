0b3ed98354f788c5c7c6657f05bdb3cb598279ef
Fernando Mendoza <fmendoza@irontec.com>
2023-03-07 11:51:18 +0000

    test: ConditionalRoute cypress test

--- web/portal/client/cypress/support/commands/prepareGenericPactInterceptors.js
+++ web/portal/client/cypress/support/commands/prepareGenericPactInterceptors.js
@@ -3,6 +3,7 @@ import TerminalCollection from '../../fixtures/Terminal/getCollection.json';
 import ExtensionCollection from '../../fixtures/Extension/getCollection.json';
 import FaxCollection from '../../fixtures/Fax/getCollection.json';
 import ContactCollection from '../../fixtures/Contact/getCollection.json';
+import ConditionalRouteCollection from '../../fixtures/ConditionalRoute/getCollection.json';
 import UserCollection from '../../fixtures/Users/getCollection.json';
 
 Cypress.Commands.add('prepareGenericPactInterceptors', (pactContextName) => {
@@ -37,4 +38,8 @@ Cypress.Commands.add('prepareGenericPactInterceptors', (pactContextName) => {
   cy.intercept('GET', '**/api/client/contacts?*', {
     ...ContactCollection,
   }).as('getContact');
+
+  cy.intercept('GET', '**/api/client/conditional_routes?*', {
+    ...ConditionalRouteCollection,
+  }).as('getConditionalRoute');
 });
