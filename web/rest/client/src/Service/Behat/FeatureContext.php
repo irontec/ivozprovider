dca89feaf814f8c64d5dbb79435c5870d909247d
Mikel <mikel@irontec.com>
2023-04-03 16:31:10 +0200

    tests: update API tests and fixtures

--- web/rest/client/src/Service/Behat/FeatureContext.php
+++ web/rest/client/src/Service/Behat/FeatureContext.php
@@ -2,12 +2,30 @@
 
 namespace Service\Behat;
 
-use Ivoz\Api\Behat\Context\FeatureContext as BaseFeatureContext;
 use Behat\Behat\Hook\Scope\BeforeScenarioScope;
 use Behat\MinkExtension\Context\RawMinkContext;
+use Ivoz\Api\Behat\Context\FeatureContext as BaseFeatureContext;
+use Symfony\Component\HttpKernel\KernelInterface;
 
 class FeatureContext extends BaseFeatureContext
 {
+    private string $storagePath;
+
+    public function __construct(
+        KernelInterface $kernel
+    ) {
+        $container = $kernel->getContainer();
+        /** @var string $storagePath */
+        $storagePath = $container->getParameter('local_storage_path');
+        $this->storagePath = $storagePath;
+
+        if (substr($this->storagePath, -1) !== '/') {
+            $this->storagePath .= '/';
+        }
+
+        parent::__construct($kernel);
+    }
+
     /**
      * @Given I add Company Authorization header
      */
@@ -70,6 +88,27 @@ class FeatureContext extends BaseFeatureContext
     public function setUserAuthorizationHeader()
     {
         return $this->setAuthorizationHeader('alice@democompany.com', 'user_login');
+    }
+
+    /**
+     * @Given storage file exists :filename
+     */
+    public function setFileExists(string $filename): void
+    {
+        $filePath = $this->storagePath . $filename;
+        $dir = dirname($filePath);
+
+        if (!file_exists($dir)) {
+            mkdir(
+                directory: $dir,
+                recursive: true,
+            );
+        }
+
+        file_put_contents(
+            $filePath,
+            'random data'
+        );
     }
 
     /**
