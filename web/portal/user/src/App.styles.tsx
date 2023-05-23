b9af7c644d43942d8b6d2679ab1149a1cd2762f8
Mikel <mikel@irontec.com>
2023-05-23 12:01:48 +0200

    updated styles

--- web/portal/user/src/App.styles.tsx
+++ web/portal/user/src/App.styles.tsx
@@ -1,11 +1,5 @@
 import { styled } from '@mui/styles';
 
-export const StyledAppFlexDiv = styled('div')(() => {
-  return {
-    maxWidth: '1920px',
-  };
-});
-
 export const StyledAppApiLoading = styled('div')({
   'text-align': 'center',
 });
