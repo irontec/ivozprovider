6d0cd91280f2763b32587a4f96dee9adbf7868af
Mikel <mikel@irontec.com>
2023-08-16 10:50:27 +0200

    portal/platform: use selected language in AdministatorRelPublicEntities lists

--- web/portal/platform/src/entities/PublicEntity/PublicEntity.tsx
+++ web/portal/platform/src/entities/PublicEntity/PublicEntity.tsx
@@ -3,6 +3,7 @@ import defaultEntityBehavior from '@irontec/ivoz-ui/entities/DefaultEntityBehavi
 import EntityInterface from '@irontec/ivoz-ui/entities/EntityInterface';
 import _ from '@irontec/ivoz-ui/services/translations/translate';
 import AccountTreeIcon from '@mui/icons-material/AccountTree';
+import { getI18n } from 'react-i18next';
 
 import {
   PublicEntityProperties,
@@ -27,6 +28,7 @@ const properties: PublicEntityProperties = {
   },
   name: {
     label: _('Name'),
+    multilang: true,
   },
 };
 
@@ -36,8 +38,11 @@ const PublicEntity: EntityInterface = {
   iden: 'PublicEntity',
   title: _('Entity', { count: 2 }),
   path: '/public_entities',
-  toStr: (row: PublicEntityPropertyList<EntityValues>) =>
-    row.name?.en as string,
+  toStr: (row: PublicEntityPropertyList<EntityValues>) => {
+    const language = getI18n().language.substring(0, 2);
+
+    return row.name?.[language] as string;
+  },
   properties,
   selectOptions: async () => {
     const module = await import('./SelectOptions');
