15f30fffc6c4511f2c5013b2156bab60977710bc
Mikel <mikel@irontec.com>
2023-09-19 13:17:47 +0200

    tests: updated orm tests

--- schema/tests/Provider/WebPortal/WebPortalRepositoryTest.php
+++ schema/tests/Provider/WebPortal/WebPortalRepositoryTest.php
@@ -18,6 +18,7 @@ class WebPortalRepositoryTest extends KernelTestCase
     {
         $this->its_instantiable();
         $this->its_finds_user_web_portal_by_server_name();
+        $this->its_finds_user_web_portal_by_server_name_and_type();
     }
 
     public function its_instantiable()
@@ -34,6 +35,23 @@ class WebPortalRepositoryTest extends KernelTestCase
     }
 
     public function its_finds_user_web_portal_by_server_name()
+    {
+        /** @var WebPortalRepository $repository */
+        $repository = $this
+            ->em
+            ->getRepository(WebPortal::class);
+
+        $webPortal = $repository->findByServerName(
+            'users-ivozprovider.irontec.com'
+        );
+
+        $this->assertInstanceOf(
+            WebPortal::class,
+            $webPortal
+        );
+    }
+
+    public function its_finds_user_web_portal_by_server_name_and_type()
     {
         /** @var WebPortalRepository $repository */
         $repository = $this
