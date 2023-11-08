8f876fc3566ddaf938137eecef2b908d572f39f0
Kaian <kaian@irontec.com>
2023-11-08 10:30:33 +0100

    schema: add announceUserCount field to ConferenceRooms table

---
+++ schema/DoctrineMigrations/Version20231108092228.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Application\Migrations;
+
+use Ivoz\Core\Infrastructure\Persistence\Doctrine\LoggableMigration;
+use Doctrine\DBAL\Schema\Schema;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+class Version20231108092228 extends LoggableMigration
+{
+    /**
+     * @param Schema $schema
+     */
+    public function up(Schema $schema)
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('ALTER TABLE ConferenceRooms ADD announceUserCount VARCHAR(10) DEFAULT \'first\' NOT NULL COMMENT \'[enum:always|first]\'');
+    }
+
+    /**
+     * @param Schema $schema
+     */
+    public function down(Schema $schema)
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('ALTER TABLE ConferenceRooms DROP announceUserCount');
+    }
+}
