0e7e0ad679cf299886f59417e816fe0c72b88798
Mikel <mikel@irontec.com>
2024-01-22 18:53:31 +0100

    schema: make domains accessible on client API for restricted admins

---
+++ schema/DoctrineMigrations/Version20240122174421.php
@@ -0,0 +1,36 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Application\Migrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Ivoz\Core\Infrastructure\Persistence\Doctrine\LoggableMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20240122174421 extends LoggableMigration
+{
+    public function getDescription(): string
+    {
+        return 'Make domains accesible on client API';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('update PublicEntities set client = 1 where iden = \'Domains\'');
+        $this->addSql(
+            'INSERT IGNORE INTO AdministratorRelPublicEntities (administratorId, publicEntityId, `create`, `read`, `update`, `delete`) '
+            . 'SELECT A.id, P.id, 0, 1, 0, 0 FROM Administrators A INNER JOIN PublicEntities P '
+            . 'WHERE A.restricted = 1 AND A.brandId IS NOT NULL AND A.companyId IS NOT NULL AND P.iden in ("Domains")'
+        );
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('update PublicEntities set client = 0 where iden = \'Domains\'');
+    }
+}
