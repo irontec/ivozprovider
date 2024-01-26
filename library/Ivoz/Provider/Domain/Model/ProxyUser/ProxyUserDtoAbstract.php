997709e76194f981a673e110860a1cb5e63b97bc
Mikel <mikel@irontec.com>
2024-01-26 11:40:14 +0100

    core: regenerated entities

--- library/Ivoz/Provider/Domain/Model/ProxyUser/ProxyUserDtoAbstract.php
+++ library/Ivoz/Provider/Domain/Model/ProxyUser/ProxyUserDtoAbstract.php
@@ -24,6 +24,11 @@ abstract class ProxyUserDtoAbstract implements DataTransferObjectInterface
     private $ip = null;
 
     /**
+     * @var string|null
+     */
+    private $advertisedIp = null;
+
+    /**
      * @var int|null
      */
     private $id = null;
@@ -45,6 +50,7 @@ abstract class ProxyUserDtoAbstract implements DataTransferObjectInterface
         return [
             'name' => 'name',
             'ip' => 'ip',
+            'advertisedIp' => 'advertisedIp',
             'id' => 'id'
         ];
     }
@@ -57,6 +63,7 @@ abstract class ProxyUserDtoAbstract implements DataTransferObjectInterface
         $response = [
             'name' => $this->getName(),
             'ip' => $this->getIp(),
+            'advertisedIp' => $this->getAdvertisedIp(),
             'id' => $this->getId()
         ];
 
@@ -96,6 +103,18 @@ abstract class ProxyUserDtoAbstract implements DataTransferObjectInterface
     public function getIp(): ?string
     {
         return $this->ip;
+    }
+
+    public function setAdvertisedIp(?string $advertisedIp): static
+    {
+        $this->advertisedIp = $advertisedIp;
+
+        return $this;
+    }
+
+    public function getAdvertisedIp(): ?string
+    {
+        return $this->advertisedIp;
     }
 
     /**
