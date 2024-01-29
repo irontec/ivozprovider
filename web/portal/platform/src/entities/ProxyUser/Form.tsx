85b536f2a6dde286fed4f8bbff1255f7f7a86792
Mikel <mikel@irontec.com>
2024-01-29 12:48:02 +0100

    portal/platform: handle many proxies

--- web/portal/platform/src/entities/ProxyUser/Form.tsx
+++ web/portal/platform/src/entities/ProxyUser/Form.tsx
@@ -5,6 +5,12 @@ import {
 import { Form as DefaultEntityForm } from '@irontec/ivoz-ui/entities/DefaultEntityBehavior/Form';
 
 const Form = (props: EntityFormProps): JSX.Element => {
+  const { row } = props;
+
+  const readOnlyProperties = {
+    name: row?.id === 1,
+  };
+
   const groups: Array<FieldsetGroups | false> = [
     {
       legend: '',
@@ -16,7 +22,13 @@ const Form = (props: EntityFormProps): JSX.Element => {
     },
   ];
 
-  return <DefaultEntityForm {...props} groups={groups} />;
+  return (
+    <DefaultEntityForm
+      {...props}
+      readOnlyProperties={readOnlyProperties}
+      groups={groups}
+    />
+  );
 };
 
 export default Form;
