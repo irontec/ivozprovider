5cb9a70b4bb7438551968b479b85e53c732308e5
Kaian <kaian@irontec.com>
2024-02-06 14:13:04 +0100

    iso: update ISO scripts for tempest release

--- extra/simple-cdd/generate-iso.sh
+++ extra/simple-cdd/generate-iso.sh
@@ -2,17 +2,17 @@
 echo Building ISO Image for amd64
 
 # Add IvozProvider repositories
-echo deb http://packages.irontec.com/debian halliday main extra | sudo tee -a /etc/apt/sources.list
+echo deb http://packages.irontec.com/debian tempest main extra | sudo tee -a /etc/apt/sources.list
 echo deb http://packages.irontec.com/debian tayler main extra  | sudo tee -a /etc/apt/sources.list
-wget http://packages.irontec.com/public.key -q -O - | sudo apt-key add -
+sudo wget http://packages.irontec.com/public.key -q -O /etc/apt/trusted.gpg.d/irontec-debian-repository.asc
 
 # Update apt-cache
 sudo apt-get update
 
 # Download ivozprovider packages for given release
 pushd local
-cat ../package.list | xargs apt-get download -t halliday
+cat ../package.list | xargs apt-get download -t tempest
 popd
 
 # Generate iso file
-build-simple-cdd --dvd --force-root --conf simple-cdd.conf --verbose --force-preseed --dist bullseye
+build-simple-cdd --dvd --force-root --conf simple-cdd.conf --verbose --force-preseed --dist bookworm
