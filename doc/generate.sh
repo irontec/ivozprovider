2f85dab83993ccc6da38bdc981c5a44e49e60b52
Kaian <kaian@irontec.com>
2024-02-06 13:30:10 +0100

    doc: generate documentation from tempest branch templates

--- doc/generate.sh
+++ doc/generate.sh
@@ -4,17 +4,17 @@
 mkdir -p /opt/irontec/ghpages/
 
 # Regenerate sphinx documentation
-sphinx-versioning build -r artemis doc/sphinx /opt/irontec/ghpages/en -wartemis -woasis -wbleeding -whalliday -Wx -- -D"language=en"
-sphinx-versioning build -r artemis doc/sphinx /opt/irontec/ghpages/es -wartemis -woasis -wbleeding -whalliday -Wx -- -D"language=es"
-sphinx-versioning build -r artemis doc/sphinx /opt/irontec/ghpages/ensingle -wartemis -woasis -wbleeding -whalliday -Wx -- -D"language=en" -b singlehtml
-sphinx-versioning build -r artemis doc/sphinx /opt/irontec/ghpages/essingle -wartemis -woasis -wbleeding -whalliday -Wx -- -D"language=es" -b singlehtml
-sphinx-versioning build -r artemis doc/sphinx /opt/irontec/ghpages/enlatex -wartemis -woasis -whalliday -Wx -- -D"language=en" -b latex
-sphinx-versioning build -r artemis doc/sphinx /opt/irontec/ghpages/eslatex -wartemis -woasis -whalliday -Wx -- -D"language=es" -b latex
-sphinx-versioning build -r artemis doc/sphinx /opt/irontec/ghpages/enepub -wartemis -woasis -whalliday -Wx -- -D"language=en" -b epub
-sphinx-versioning build -r artemis doc/sphinx /opt/irontec/ghpages/esepub -wartemis -woasis -whalliday -Wx -- -D"language=es" -b epub
+sphinx-versioning build -r tempest doc/sphinx /opt/irontec/ghpages/en -woasis -wartemis -whalliday -wtempest -wbleeding -Wx -- -D"language=en"
+sphinx-versioning build -r tempest doc/sphinx /opt/irontec/ghpages/es -woasis -wartemis -whalliday -wtempest -wbleeding -Wx -- -D"language=es"
+sphinx-versioning build -r tempest doc/sphinx /opt/irontec/ghpages/ensingle -woasis -wartemis -whalliday -wtempest -wbleeding -Wx -- -D"language=en" -b singlehtml
+sphinx-versioning build -r tempest doc/sphinx /opt/irontec/ghpages/essingle -woasis -wartemis -whalliday -wtempest -wbleeding -Wx -- -D"language=es" -b singlehtml
+sphinx-versioning build -r tempest doc/sphinx /opt/irontec/ghpages/enlatex -woasis -wartemis -whalliday -wtempest -wbleeding -Wx -- -D"language=en" -b latex
+sphinx-versioning build -r tempest doc/sphinx /opt/irontec/ghpages/eslatex -woasis -wartemis -whalliday -wtempest -wbleeding -Wx -- -D"language=es" -b latex
+sphinx-versioning build -r tempest doc/sphinx /opt/irontec/ghpages/enepub -woasis -wartemis -whalliday -wtempest -wbleeding -Wx -- -D"language=en" -b epub
+sphinx-versioning build -r tempest doc/sphinx /opt/irontec/ghpages/esepub -woasis -wartemis -whalliday -wtempest -wbleeding -Wx -- -D"language=es" -b epub
 
 # Generate PDF from latex build
-for RELEASE in oasis artemis halliday; do
+for RELEASE in oasis artemis halliday tempest; do
     for LANG in es en; do
         pushd /opt/irontec/ghpages/${LANG}latex/${RELEASE}
             pdflatex -interaction nonstopmode IvozProvider.tex > /dev/null || true
