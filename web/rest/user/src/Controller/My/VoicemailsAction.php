4d7dd520339161aa6655711f9f1d8130f8bfb473
Dani Garcia <dgarcia@irontec.com>
2024-03-05 18:31:14 +0100

    rest/user: added endpoints for voicemail and voicemail_messages

---
+++ web/rest/user/src/Controller/My/VoicemailsAction.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Controller\My;
+
+use ApiPlatform\Core\Exception\ResourceClassNotFoundException;
+use Ivoz\Provider\Domain\Model\Voicemail\VoicemailRepository;
+use Symfony\Component\Security\Core\Authentication\Token\Storage\TokenStorageInterface;
+use Ivoz\Provider\Domain\Model\User\UserInterface;
+
+class VoicemailsAction
+{
+    public function __construct(
+        private TokenStorageInterface $tokenStorage,
+        private VoicemailRepository $voicemailRepository
+    ) {
+    }
+
+    public function __invoke()
+    {
+        $token =  $this->tokenStorage->getToken();
+
+        if (!$token || !$token->getUser()) {
+            throw new ResourceClassNotFoundException('User not found');
+        }
+
+        /** @var UserInterface $user */
+        $user = $token->getUser();
+
+        return $this
+            ->voicemailRepository
+            ->getVoicemailsByUser($user);
+    }
+}
