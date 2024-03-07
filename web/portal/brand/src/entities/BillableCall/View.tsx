26aa9ce7fd6357487e5b29decbdd881eba7edd48
Mikel <mikel@irontec.com>
2024-03-07 11:56:50 +0100

    portal/brand: fixed billable call detail view

---
+++ web/portal/brand/src/entities/BillableCall/View.tsx
@@ -0,0 +1,40 @@
+import { FieldsetGroups } from '@irontec/ivoz-ui/entities/DefaultEntityBehavior';
+import DefaultView from '@irontec/ivoz-ui/entities/DefaultEntityBehavior/View';
+import { ViewProps } from '@irontec/ivoz-ui/entities/EntityInterface';
+import _ from '@irontec/ivoz-ui/services/translations/translate';
+
+const View = (props: ViewProps): JSX.Element | null => {
+  const groups: Array<FieldsetGroups | false> = [
+    {
+      legend: _('Basic Information'),
+      fields: [
+        'company',
+        'startTime',
+        'duration',
+        'direction',
+        'caller',
+        'callee',
+      ],
+    },
+    {
+      legend: _('Billing Information'),
+      fields: [
+        'price',
+        'cost',
+        'ratingPlanName',
+        'destinationName',
+        'carrier',
+        'ddiProvider',
+        'invoice',
+      ],
+    },
+    {
+      legend: _('Extra Information'),
+      fields: ['callid', 'endpointType', 'endpointId', 'endpointName'],
+    },
+  ];
+
+  return <DefaultView {...props} groups={groups} />;
+};
+
+export default View;
