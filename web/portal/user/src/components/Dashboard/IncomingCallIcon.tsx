1a7762fed386cc8072778528b07c205f522bdc34
Soraya Ben <sben@irontec.com>
2024-03-11 14:37:25 +0100

    web/portal: Center position icons dashboard

--- web/portal/user/src/components/Dashboard/IncomingCallIcon.tsx
+++ web/portal/user/src/components/Dashboard/IncomingCallIcon.tsx
@@ -5,11 +5,11 @@ export default function IncomingCallIcon(): JSX.Element {
       <g fill='var(--color-primary)'>
         <path
           d='M14.566 19.152A18.466 18.466 0 0 1 5 9.86v-.002l-.519-1.14a3.5 3.5 0 0 1 1.237-4.355l1.333-.894a1 1 0 0 1 1.335.203l2.43 3.012a1 1 0 0 1-.183 1.431l-1.257.932a12.14 12.14 0 0 0 5.511 5.51l.932-1.256a1 1 0 0 1 1.431-.183l3.012 2.43a1 1 0 0 1 .203 1.335l-.888 1.324a3.5 3.5 0 0 1-4.331 1.247l-.68-.303Z'
-          transform='translate(7, 9)'
+          transform='translate(8, 9)'
         />
         <path
           d='M13.75 9a.75.75 0 0 0 .75.75h3.828a.75.75 0 0 0 0-1.5h-2.017l3.159-3.159a.75.75 0 1 0-1.061-1.06L15.25 7.188V5.172a.75.75 0 0 0-1.5 0V9Z'
-          transform='translate(7, 9)'
+          transform='translate(8, 9)'
         />
       </g>
     </svg>
