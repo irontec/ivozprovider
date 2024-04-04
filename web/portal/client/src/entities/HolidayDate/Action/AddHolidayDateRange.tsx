fb924bcdd05edf29437e84ac89f321c5b5de5d9b
rgomez <rgomez@irontec.com>
2024-04-04 12:32:18 +0200

    portal/client: hide Add Holiday date range option if user does not have permissions

--- web/portal/client/src/entities/HolidayDate/Action/AddHolidayDateRange.tsx
+++ web/portal/client/src/entities/HolidayDate/Action/AddHolidayDateRange.tsx
@@ -8,12 +8,25 @@ import {
 import _ from '@irontec/ivoz-ui/services/translations/translate';
 import AddCircleIcon from '@mui/icons-material/AddCircle';
 import { Tooltip } from '@mui/material';
+import { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
+import { useStoreState } from 'store';
 
 const AddHolidayDateRange: ActionFunctionComponent = (
   props: MultiSelectActionItemProps
 ) => {
   const { variant = 'icon' } = props;
+  const [disabled, setDisabled] = useState(false);
+  const aboutMe = useStoreState((state) => state.clientSession.aboutMe.profile);
+  const holidayDateRangeAcl = aboutMe?.acls?.find(
+    (item) => item.iden === 'HolidayDateRange'
+  );
+
+  useEffect(() => {
+    if (aboutMe?.restricted === true && holidayDateRangeAcl?.create !== true) {
+      setDisabled(true);
+    }
+  }, [aboutMe, holidayDateRangeAcl?.create]);
 
   if (isSingleRowAction(props)) {
     return <span className='display-none'></span>;
@@ -24,16 +37,29 @@ const AddHolidayDateRange: ActionFunctionComponent = (
     '/holiday_dates_range'
   );
 
-  const linkStyles = {
+  const linkStyles: React.CSSProperties = {
     textDecoration: 'none',
     color: 'inherit',
   };
 
   return (
     <>
-      <Link to={targetUrl} style={linkStyles}>
+      <Link
+        to={targetUrl}
+        style={linkStyles}
+        onClick={(e) => {
+          if (!disabled) {
+            return;
+          }
+
+          e.preventDefault();
+          e.stopPropagation();
+        }}
+      >
         {variant === 'text' && (
-          <MoreMenuItem>{_('Add Holiday date range')}</MoreMenuItem>
+          <MoreMenuItem className={disabled ? 'disabled' : ''}>
+            {_('Add Holiday date range')}
+          </MoreMenuItem>
         )}
         {variant === 'icon' && (
           <Tooltip
