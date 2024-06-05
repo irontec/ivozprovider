912e37288b7da1d04d43130a888edafafed4da6a
Dani Garcia <dgarcia@irontec.com>
2024-06-05 14:11:59 +0200

    core: updated fixtures

--- library/DataFixtures/ORM/ProviderFriend.php
+++ library/DataFixtures/ORM/ProviderFriend.php
@@ -43,13 +43,26 @@ class ProviderFriend extends Fixture implements DependentFixtureInterface
             $this->setName("testFriend2");
             $this->setDirectConnectivity("intervpbx");
             $this->setPriority(2);
-            $this->setCompany($fixture->getReference('_reference_ProviderCompany2'));
+            $this->setCompany($fixture->getReference('_reference_ProviderCompany3'));
             $this->setInterCompany($fixture->getReference('_reference_ProviderCompany1'));
         })->call($item2);
 
         $this->addReference('_reference_ProviderFriend2', $item2);
         $this->sanitizeEntityValues($item2);
         $manager->persist($item2);
+
+        $item3 = $this->createEntityInstance(Friend::class);
+        (function () use ($fixture) {
+            $this->setName("testFriend3");
+            $this->setDirectConnectivity("intervpbx");
+            $this->setPriority(2);
+            $this->setCompany($fixture->getReference('_reference_ProviderCompany1'));
+            $this->setInterCompany($fixture->getReference('_reference_ProviderCompany3'));
+        })->call($item3);
+
+        $this->addReference('_reference_ProviderFriend3', $item3);
+        $this->sanitizeEntityValues($item3);
+        $manager->persist($item3);
 
         $manager->flush();
     }
