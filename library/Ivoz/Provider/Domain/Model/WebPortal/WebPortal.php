09f8e8150a97ec669a07c4fef579ef0162fd261a
Dani Garcia <dgarcia@irontec.com>
2024-06-27 16:18:42 +0200

    core: sanitized WebPortal company-urlType-Brand

--- library/Ivoz/Provider/Domain/Model/WebPortal/WebPortal.php
+++ library/Ivoz/Provider/Domain/Model/WebPortal/WebPortal.php
@@ -5,6 +5,8 @@ namespace Ivoz\Provider\Domain\Model\WebPortal;
 use Assert\Assertion;
 use Ivoz\Core\Domain\Model\TempFileContainnerTrait;
 use Ivoz\Core\Domain\Service\FileContainerInterface;
+use Ivoz\Provider\Domain\Model\Company\Company;
+use Ivoz\Provider\Domain\Model\Company\CompanyInterface;
 
 /**
  * WebPortal
@@ -35,6 +37,24 @@ class WebPortal extends WebPortalAbstract implements FileContainerInterface, Web
                 $this->getUrlType()
             );
             throw new \DomainException($errorMsg);
+        }
+
+        $isUserType = $this->getUrlType() === self::URLTYPE_USER;
+        $isCompanyValidForUser =
+            $this->getCompany() === null
+            || $this->getCompany()->getType() === CompanyInterface::TYPE_VPBX;
+
+        if ($isUserType && !$isCompanyValidForUser) {
+            throw new \DomainException("Company must be vpbx in user urls");
+        }
+
+        $isAdminType = $this->getUrlType() === self::URLTYPE_ADMIN;
+        $clientMatchesBrand =
+            $this->getCompany() === null
+            || $this->getBrand()?->getId() === $this->getCompany()->getId();
+
+        if ($isAdminType && !$clientMatchesBrand) {
+            throw new \DomainException("Company must belong to brand");
         }
     }
 
