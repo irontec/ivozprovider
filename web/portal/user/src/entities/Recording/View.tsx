f66b72e107d6f7470afa23fb9e5a9a84378ad128
Dani Garcia <dgarcia@irontec.com>
2024-08-12 09:10:38 +0200

    portal/user: added recordings section

---
+++ web/portal/user/src/entities/Recording/View.tsx
@@ -0,0 +1,18 @@
+import {
+  FieldsetGroups,
+  View as DefaultEntityView,
+} from '@irontec/ivoz-ui/entities/DefaultEntityBehavior';
+import { ViewProps } from '@irontec/ivoz-ui/entities/EntityInterface';
+
+const View = (props: ViewProps): JSX.Element | null => {
+  const groups: Array<FieldsetGroups | false> = [
+    {
+      legend: '',
+      fields: ['caller', 'callee', 'duration', 'recordedFile'],
+    },
+  ];
+
+  return <DefaultEntityView {...props} groups={groups} />;
+};
+
+export default View;
