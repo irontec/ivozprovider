f399131f3cb36a5d6b2133f87559678757f91e9a
Dani Garcia <dgarcia@irontec.com>
2024-08-20 11:33:32 +0200

    portal/client: hide column on ddis/recordings

--- web/portal/client/src/entities/Recording/Recording.tsx
+++ web/portal/client/src/entities/Recording/Recording.tsx
@@ -1,8 +1,11 @@
 import defaultEntityBehavior from '@irontec/ivoz-ui/entities/DefaultEntityBehavior';
-import EntityInterface from '@irontec/ivoz-ui/entities/EntityInterface';
+import EntityInterface, {
+  EntityColumnsFuncType,
+} from '@irontec/ivoz-ui/entities/EntityInterface';
 import _ from '@irontec/ivoz-ui/services/translations/translate';
 import SettingsVoiceIcon from '@mui/icons-material/SettingsVoice';
 
+import Ddi from '../Ddi/Ddi';
 import Type from './Field/Type';
 import { RecordingProperties } from './RecordingProperties';
 
@@ -39,7 +42,18 @@ const properties: RecordingProperties = {
   },
 };
 
-const columns = ['calldate', 'typeGhost', 'caller', 'callee', 'duration'];
+const columnsFunc: EntityColumnsFuncType = () => {
+  const showType = !location.pathname.includes(Ddi.path);
+  const columns = [
+    'calldate',
+    showType && 'typeGhost',
+    'caller',
+    'callee',
+    'duration',
+  ];
+
+  return columns.filter((row) => row !== false) as Array<string>;
+};
 
 const recording: EntityInterface = {
   ...defaultEntityBehavior,
@@ -49,7 +63,8 @@ const recording: EntityInterface = {
   title: _('Recording', { count: 2 }),
   path: '/recordings',
   properties,
-  columns,
+  toStr: (row) => row.calldate,
+  columns: columnsFunc,
   defaultOrderBy: '',
   acl: {
     ...defaultEntityBehavior.acl,
