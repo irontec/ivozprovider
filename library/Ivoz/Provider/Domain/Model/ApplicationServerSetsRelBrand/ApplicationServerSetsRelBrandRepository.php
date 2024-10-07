541d47886b745bd190fd33af0eed62addd4730dd
Dani Garcia <dgarcia@irontec.com>
2024-10-07 22:48:04 +0200

    core: set default values for required fields

--- library/Ivoz/Provider/Domain/Model/ApplicationServerSetsRelBrand/ApplicationServerSetsRelBrandRepository.php
+++ library/Ivoz/Provider/Domain/Model/ApplicationServerSetsRelBrand/ApplicationServerSetsRelBrandRepository.php
@@ -3,6 +3,7 @@
 namespace Ivoz\Provider\Domain\Model\ApplicationServerSetsRelBrand;
 
 use Ivoz\Core\Domain\Service\Repository\RepositoryInterface;
+use Ivoz\Provider\Domain\Model\Administrator\AdministratorInterface;
 
 /**
  * @extends RepositoryInterface<ApplicationServerSetsRelBrandInterface, ApplicationServerSetsRelBrandDto>
@@ -13,4 +14,9 @@ interface ApplicationServerSetsRelBrandRepository extends RepositoryInterface
      * @return ApplicationServerSetsRelBrandInterface[]
      */
     public function findByBrandId(int $brandId): array;
+
+    /**
+     * @return int[]
+     */
+    public function getApplicationServerSetIdsByBrandAdmin(AdministratorInterface $admin): array;
 }
