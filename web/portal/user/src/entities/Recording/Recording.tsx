b27d9d0359dcf299f626f4cfe9eaeb4b9f319825
Roger Batista <rbatista@irontec.com>
2024-10-23 10:42:40 +0200

    portal/user: Added bulk recording download

--- web/portal/user/src/entities/Recording/Recording.tsx
+++ web/portal/user/src/entities/Recording/Recording.tsx
@@ -3,6 +3,7 @@ import EntityInterface from '@irontec/ivoz-ui/entities/EntityInterface';
 import _ from '@irontec/ivoz-ui/services/translations/translate';
 import SettingsVoiceIcon from '@mui/icons-material/SettingsVoice';
 
+import Actions from './Action';
 import { RecordingProperties } from './RecordingProperties';
 
 const properties: RecordingProperties = {
@@ -37,13 +38,14 @@ const recording: EntityInterface = {
   title: _('Recording', { count: 2 }),
   path: '/recordings',
   properties,
-  toStr: (row) => row.calldate,
+  toStr: (row) => row.calldate as string,
   columns,
   defaultOrderBy: '',
   acl: {
     ...defaultEntityBehavior.acl,
     iden: 'Recordings',
   },
+  customActions: Actions,
   View: async () => {
     const module = await import('./View');
 
