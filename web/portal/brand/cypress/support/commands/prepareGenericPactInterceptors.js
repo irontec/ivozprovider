156bda07227d6611948189b7454b550fe04461a3
Roger Batista <rbatista@irontec.com>
2024-11-28 15:47:12 +0100

    portal/brand: Updated cypress tests

--- web/portal/brand/cypress/support/commands/prepareGenericPactInterceptors.js
+++ web/portal/brand/cypress/support/commands/prepareGenericPactInterceptors.js
@@ -19,6 +19,7 @@ import CarriersCollection from '../../fixtures/Provider/Carriers/getCollection.j
 import CarrierServerCollection from '../../fixtures/Provider/CarrierServer/getCollection.json';
 import CodecsCollection from '../../fixtures/Provider/Codecs/getCollection.json';
 import CompaniesCollection from '../../fixtures/Provider/Companies/getCollection.json';
+import CompaniesUnassignedCollection from '../../fixtures/Provider/Companies/getUnassignedCollection.json';
 import CorporationsCollection from '../../fixtures/Provider/Corporations/getCollection.json';
 import CountriesCollection from '../../fixtures/Provider/Countries/getCollection.json';
 import CurrenciesCollection from '../../fixtures/Provider/Currencies/getCollection.json';
@@ -26,7 +27,9 @@ import DdiProvidersCollection from '../../fixtures/Provider/DdiProviders/getColl
 import DdiProvidersAddressesCollection from '../../fixtures/Provider/DdiProviders/getProviderAddressesCollection.json';
 import DdiProvidersRegistrationsCollection from '../../fixtures/Provider/DdiProviders/getProviderRegistrationsCollection.json';
 import DdisCollection from '../../fixtures/Provider/Ddis/getCollection.json';
+import DomainsCollection from '../../fixtures/Provider/Domains/getCollection.json';
 import FeaturesCollection from '../../fixtures/Provider/Features/getCollection.json';
+import FriendsCollection from '../../fixtures/Provider/Friends/getCollection.json';
 import LanguagesCollection from '../../fixtures/Provider/Languages/getCollection.json';
 import MediaRelaySetsColection from '../../fixtures/Provider/MediaRelaySets/getCollection.json';
 import NotificationTemplateContentsCollection from '../../fixtures/Provider/NotificationTemplateContents/getCollection.json';
@@ -34,8 +37,11 @@ import NotificationTemplatesCollection from '../../fixtures/Provider/Notificatio
 import OutgoingDdiRulesCollection from '../../fixtures/Provider/OutgoingDdiRules/getCollection.json';
 import OutgoingRoutingCollection from '../../fixtures/Provider/OutgoingRoutings/getCollection.json';
 import ProxyTrunksCollection from '../../fixtures/Provider/ProxyTrunks/getCollection.json';
+import ProxyUsersCollection from '../../fixtures/Provider/ProxyUsers/getCollection.json';
 import RatingPlanGroupCollection from '../../fixtures/Provider/RatingPlanGroups/getCollections.json';
 import RatingProfilesCollection from '../../fixtures/Provider/RatingProfile/getCollection.json';
+import ResidentialDevicesCollection from '../../fixtures/Provider/ResidentialDevices/getCollection.json';
+import RetailAccountsCollection from '../../fixtures/Provider/RetailAccounts/getCollection.json';
 import RoutingPatternGroupsCollection from '../../fixtures/Provider/RoutingPatternGroups/getCollection.json';
 import RoutingPatternsCollection from '../../fixtures/Provider/RoutingPatterns/getCollection.json';
 import RoutingTagsCollection from '../../fixtures/Provider/RoutingTags/getCollection.json';
@@ -241,5 +247,28 @@ Cypress.Commands.add(
     cy.intercept('GET', '**/api/brand/routing_patterns?*', {
       ...RoutingPatternsCollection,
     }).as('getRoutingPatterns');
+    cy.intercept('GET', '**/api/brand/residential_devices?*', {
+      ...ResidentialDevicesCollection,
+    }).as('getResidentialDevices');
+
+    cy.intercept('GET', '**/api/brand/domains?*', {
+      ...DomainsCollection,
+    }).as('getDomains');
+
+    cy.intercept('GET', '**/api/brand/proxy_users?*', {
+      ...ProxyUsersCollection,
+    }).as('getProxyUsers');
+
+    cy.intercept('GET', '**/api/brand/retail_accounts?*', {
+      ...RetailAccountsCollection,
+    }).as('getRetailAccounts');
+
+    cy.intercept('GET', '**/api/brand/friends?*', {
+      ...FriendsCollection,
+    }).as('getFriends');
+
+    cy.intercept('GET', '**/api/brand/companies/corporate/unassigned*', {
+      ...CompaniesUnassignedCollection,
+    }).as('getFriends');
   }
 );
