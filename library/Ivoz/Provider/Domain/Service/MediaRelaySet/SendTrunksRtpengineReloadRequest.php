3f169f109a74fa0bbc2b9538ee0171538ecd9639
Dani Garcia <dgarcia@irontec.com>
2024-12-13 11:00:43 +0100

    core: reload rtpengine on MediaRelaySet deletion

---
+++ library/Ivoz/Provider/Domain/Service/MediaRelaySet/SendTrunksRtpengineReloadRequest.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Ivoz\Provider\Domain\Service\MediaRelaySet;
+
+use Ivoz\Kam\Domain\Service\TrunksClientInterface;
+use Ivoz\Provider\Domain\Model\MediaRelaySet\MediaRelaySetInterface;
+
+class SendTrunksRtpengineReloadRequest implements MediaRelaySetEventHandlerInterface
+{
+    public const ON_COMMIT_PRIORITY = self::PRIORITY_LOW;
+
+    public function __construct(
+        private TrunksClientInterface $trunksClient
+    ) {
+    }
+
+    /** @return array<string, int> */
+    public static function getSubscribedEvents(): array
+    {
+        return [
+            self::EVENT_ON_COMMIT => self::ON_COMMIT_PRIORITY,
+        ];
+    }
+
+    public function execute(MediaRelaySetInterface $mediaRelaySet): void
+    {
+        $mustReloadRtpengine = $mediaRelaySet->hasBeenDeleted();
+        if (!$mustReloadRtpengine) {
+            return;
+        }
+
+        $this->trunksClient->reloadRtpengine();
+    }
+}
