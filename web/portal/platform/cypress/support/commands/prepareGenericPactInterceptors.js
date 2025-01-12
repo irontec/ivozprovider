f29f6a93595a023d213bb30582ed148c21f2c28d
rgomez <rgomez@irontec.com>
2025-01-12 22:24:40 +0100

    portal/platform: add cypress testing for proxies user

--- web/portal/platform/cypress/support/commands/prepareGenericPactInterceptors.js
+++ web/portal/platform/cypress/support/commands/prepareGenericPactInterceptors.js
@@ -7,6 +7,7 @@ import ActiveCallsItem from '../../fixtures/My/ActiveCalls/getActiveCalls.json';
 import DashboardItem from '../../fixtures/My/Dashboard/getDashboard.json';
 import ProfileItem from '../../fixtures/My/Profile/getProfile.json';
 import ThemeItem from '../../fixtures/My/Theme/getTheme.json';
+import ProxyUserCollection from '../../fixtures/ProxyUser/getCollection.json';
 import TerminalManufacturerCollection from '../../fixtures/TerminalManufacturer/getCollection.json';
 import TimezonesItem from '../../fixtures/Timezones/getTimezones.json';
 import UserCollection from '../../fixtures/Users/getCollection.json';
@@ -68,4 +69,8 @@ Cypress.Commands.add('prepareGenericPactInterceptors', (pactContextName) => {
   cy.intercept('GET', '**/api/platform/application_servers*', {
     ...ApplicationServer,
   }).as('getApplicationServers');
+
+  cy.intercept('GET', '**/api/platform/proxy_users*', {
+    ...ProxyUserCollection,
+  }).as('getProxyUser');
 });
