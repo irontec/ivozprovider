f7c8c57eb591d0343d40d593a717cd339bb35c39
rgomez <rgomez@irontec.com>
2025-07-08 10:38:34 +0200

    portal/brand: showed all companies in selectors for foreign key filters

--- web/portal/brand/src/entities/Company/SelectOptions/SelectOptions.ts
+++ web/portal/brand/src/entities/Company/SelectOptions/SelectOptions.ts
@@ -1,6 +1,6 @@
 import { DropdownChoices } from '@irontec/ivoz-ui';
-import defaultEntityBehavior from '@irontec/ivoz-ui/entities/DefaultEntityBehavior';
 import { SelectOptionsType } from '@irontec/ivoz-ui/entities/EntityInterface';
+import { fetchAllPages } from '@irontec/ivoz-ui/helpers/fechAllPages';
 import store from 'store';
 
 const CompanySelectOptions: SelectOptionsType = ({
@@ -10,19 +10,21 @@ const CompanySelectOptions: SelectOptionsType = ({
   const entities = store.getState().entities.entities;
   const Company = entities.Company;
 
-  return defaultEntityBehavior.fetchFks(
-    `${Company.path}?_order[name]=ASC`,
-    ['id', 'name'],
-    (data) => {
-      const options: DropdownChoices = [];
+  return fetchAllPages({
+    endpoint: `${Company.path}?_order[name]=ASC`,
+    params: {
+      _properties: ['id', 'name'],
+    },
+    setter: async (data) => {
+      const options: DropdownChoices = {};
       for (const item of data) {
-        options.push({ id: item.id, label: item.name });
+        options[item.id] = Company.toStr(item);
       }
 
       callback(options);
     },
-    cancelToken
-  );
+    cancelToken,
+  });
 };
 
 export default CompanySelectOptions;
