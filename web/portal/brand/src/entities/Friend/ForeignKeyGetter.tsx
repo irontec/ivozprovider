58c540c6b21654938b6c9137a0d09fc6881e87b8
rgomez <rgomez@irontec.com>
2025-09-22 11:45:53 +0200

    portal/brand: changed Friends company selector to use VpbxSelectOptions instead of   general company selector

--- web/portal/brand/src/entities/Friend/ForeignKeyGetter.tsx
+++ web/portal/brand/src/entities/Friend/ForeignKeyGetter.tsx
@@ -1,6 +1,7 @@
 import { autoSelectOptions } from '@irontec/ivoz-ui/entities/DefaultEntityBehavior';
 import { ForeignKeyGetterType } from '@irontec/ivoz-ui/entities/EntityInterface';
 
+import { VpbxSelectOptions } from '../Company/SelectOptions';
 import { FriendPropertyList } from './FriendProperties';
 
 /** TODO remove this file unless you need to change default behaviour **/
@@ -14,7 +15,14 @@ export const foreignKeyGetter: ForeignKeyGetterType = async ({
     entityService,
     cancelToken,
     response,
-    skip: ['interCompany'],
+    skip: ['interCompany', 'company'],
+  });
+
+  promises[promises.length] = VpbxSelectOptions({
+    callback: (options) => {
+      response.company = options;
+    },
+    cancelToken,
   });
 
   await Promise.all(promises);
