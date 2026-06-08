cc20b9a22b05741d56de45c2636ca8242ea441fc
Dani Garcia <dgarcia@irontec.com>
2026-06-08 16:11:31 +0200

    core: updated webhook validations

--- library/Ivoz/Provider/Domain/Model/Webhook/Webhook.php
+++ library/Ivoz/Provider/Domain/Model/Webhook/Webhook.php
@@ -34,12 +34,15 @@ class Webhook extends WebhookAbstract implements WebhookInterface
         $company = $this->getCompany();
         $brand = $this->getBrand();
         $ddi = $this->getDdi();
+        $user = $this->getUser();
 
         if (!is_null($company)) {
             $companyBrand = $company->getBrand();
 
             if ($brand->getId() !== $companyBrand->getId()) {
-                throw new \DomainException('Company does not belong to the specified brand');
+                throw new \DomainException(
+                    'Company does not belong to the specified brand'
+                );
             }
 
             $this->setBrand($companyBrand);
@@ -51,8 +54,24 @@ class Webhook extends WebhookAbstract implements WebhookInterface
             if (is_null($company)) {
                 $this->setCompany($ddiCompany);
             } elseif ($company->getId() !== $ddiCompany?->getId()) {
-                throw new \DomainException('DDI does not belong to the specified company');
+                throw new \DomainException(
+                    'DDI does not belong to the specified company'
+                );
             }
+        }
+
+        if (!is_null($user)) {
+            $userCompany = $user->getCompany();
+
+            if (is_null($company)) {
+                $this->setCompany($userCompany);
+            } elseif ($company->getId() !== $user->getCompany()->getId()) {
+                throw new \DomainException('User does not belong to the specified company');
+            }
+        }
+
+        if (!is_null($user) && !is_null($ddi)) {
+            throw new \DomainException('Webhook can not be bound to both ddi and user');
         }
 
         $this->sanitizeWebhookConfiguration();
@@ -65,10 +84,14 @@ class Webhook extends WebhookAbstract implements WebhookInterface
         return parent::setUri($uri);
     }
 
-    private const VALID_TEMPLATE_PLACEHOLDERS = [
+    private const VALID_TEMPLATE_PLACEHOLDERS_BRAND = [
         'event', 'time', 'callId', 'company', 'companyId',
         'ddiId', 'crId', 'dpId', 'direction',
-        'caller', 'callee', 'carrier', 'ddiProvider',
+        'caller', 'callee', 'carrier', 'ddiProvider', 'iden',
+    ];
+
+    private const VALID_TEMPLATE_PLACEHOLDERS_USER = [
+        'event', 'callId', 'direction', 'owner', 'party', 'userId', 'time', 'iden',
     ];
 
     protected function sanitizeWebhookConfiguration(): void
@@ -76,14 +99,19 @@ class Webhook extends WebhookAbstract implements WebhookInterface
         $hasAnyEvent = $this->getEventStart() ||
                       $this->getEventRing() ||
                       $this->getEventAnswer() ||
-                      $this->getEventEnd();
+                      $this->getEventEnd() ||
+                      $this->getEventUpdateClid();
 
         if (!$hasAnyEvent) {
             throw new \DomainException('At least one event (Start, Ring, Answer, or End) must be enabled');
         }
 
+        $validPlaceholders = !is_null($this->getUser())
+            ? self::VALID_TEMPLATE_PLACEHOLDERS_USER
+            : self::VALID_TEMPLATE_PLACEHOLDERS_BRAND;
+
         preg_match_all('/\{\{(\w+)\}\}/', $this->getTemplate(), $matches);
-        $invalid = array_diff($matches[1], self::VALID_TEMPLATE_PLACEHOLDERS);
+        $invalid = array_diff($matches[1], $validPlaceholders);
         if (!empty($invalid)) {
             throw new \DomainException('Invalid template placeholders: ' . implode(', ', $invalid));
         }
--- library/Ivoz/Provider/Domain/Service/FeaturesRelCompany/CheckValidity.php
+++ library/Ivoz/Provider/Domain/Service/FeaturesRelCompany/CheckValidity.php
@@ -5,6 +5,7 @@ namespace Ivoz\Provider\Domain\Service\FeaturesRelCompany;
 use Ivoz\Core\Domain\Service\AvoidEntityUpdatesAbstract;
 use Ivoz\Provider\Domain\Model\Company\CompanyInterface;
 use Ivoz\Provider\Domain\Model\Feature\Feature;
+use Ivoz\Provider\Domain\Model\Feature\FeatureInterface;
 use Ivoz\Provider\Domain\Model\FeaturesRelCompany\FeaturesRelCompanyInterface;
 
 class CheckValidity extends AvoidEntityUpdatesAbstract implements FeaturesRelCompanyLifecycleEventHandlerInterface
@@ -24,17 +25,19 @@ class CheckValidity extends AvoidEntityUpdatesAbstract implements FeaturesRelCom
         $feature = $relCompany->getFeature();
         $featureIden = $feature->getIden();
 
-        if ($featureIden === Feature::WEBHOOKS_IDEN) {
-            throw new \DomainException('Webhooks feature can only be assigned to brands');
-        }
-
         $operatorPanelFeature = $featureIden === Feature::OPERATOR_PANEL_IDEN;
 
         $client = $relCompany->getCompany();
         $vpbxClient = $client?->getType() === CompanyInterface::TYPE_VPBX;
 
-        if ($operatorPanelFeature && ! $vpbxClient) {
+        if ($operatorPanelFeature && !$vpbxClient) {
             throw new \DomainException('Operator panel feature can only be assigned to vPBX clients');
+        }
+
+        $webhookFeature = $featureIden === Feature::WEBHOOKS_IDEN;
+
+        if ($webhookFeature && !$vpbxClient) {
+            throw new \DomainException('Webhooks feature can only be assigned to vPBX clients');
         }
     }
 }
